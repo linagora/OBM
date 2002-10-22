208d0b8e1a80d4dfe58e2d0c289b9e95509a122d
Madhi <medhi@linagora.com>
2002-10-22 12:27:53 +0000

    Correction dans deal ( XHTML )  ---------------------------------------------------------------------- Modified Files:  	obminclude/phplib/auth.inc obminclude/phplib/session4.inc  	php/agenda/agenda_index.php php/deal/deal_display.inc  ----------------------------------------------------------------------

--- ui/php/agenda/agenda_index.php
+++ ui/php/agenda/agenda_index.php
@@ -6,6 +6,16 @@
 ///////////////////////////////////////////////////////////////////////////////
 // $Id$ //
 ///////////////////////////////////////////////////////////////////////////////
+$www ="   <p class=\"messageInfo\">
+    	<a href=\"http://validator.w3.org/check/referer\"><img
+        src=\"http://www.w3.org/Icons/valid-xhtml10\"
+        alt=\"Valid XHTML 1.0!\" height=\"31\" width=\"88\" /></a>
+	<a href=\"http://jigsaw.w3.org/css-validator/\">
+ 	 <img style=\"border:0;width:88px;height:31px\"
+       src=\"http://jigsaw.w3.org/css-validator/images/vcss\" 
+       alt=\"Valid CSS!\" />
+	 </a>
+  	</p>";
 
 ///////////////////////////////////////////////////////////////////////////////
 // Session,Auth,Perms Management                                             //
