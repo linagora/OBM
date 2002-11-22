7609d8cda013bda7db5695a5c66e214edd65d9a3
pierre <pierre@localhost>
2002-11-22 21:25:40 +0000

    Modified Files: 	obminclude/global.inc obminclude/global_display.inc 	obminclude/themes/standard/style.css php/obm.php

--- ui/php/obm.php
+++ ui/php/obm.php
@@ -56,7 +56,7 @@ echo "   <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
     \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
    <html xmlns=\"http://www.w3.org/1999/xhtml\"> 
     <head>
-     <meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\"/>
+     <meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />
      <title>$l_title - OBM Version $obm_version</title>
      <link rel=\"stylesheet\" type=\"text/css\" href=\"/images/$set_theme/style.css\" />
     </head>
