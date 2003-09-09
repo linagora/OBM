7cbeefb0f9eea04f2947956596483e282d2e1739
Madhi <medhi@linagora.com>
2003-09-09 15:47:25 +0000

    Message forcé pour eviter le probleme du SVN log

--- ui/www/screenshots.php
+++ ui/www/screenshots.php
@@ -4,6 +4,13 @@
 <head>
 <title>OBM Screenshots</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features)
+}
+//-->
+</script>
 <link href="style01.css" rel="stylesheet" type="text/css">
 </head>
 
@@ -14,40 +21,141 @@
     <h1>Screenshots</h1>
     <p>&nbsp;</p>
   </div>
+<!-- Contenu de la page ------------------------------------------------------------------------------------------------------------------->
+<div class="center">
+   <h2>Company, contact, and deal views<a name="4"></a></h2>
+  <table class="picture">
+    <tr> 
+      <td><a href="#4"><img src="images/vignettes/Affaires01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_affaires01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Search a deal</td>
+      <td><a href="#4"><img src="images/vignettes/Affaires02.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_affaires02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Deal view</td>
+      <td><a href="#4"><img src="images/vignettes/Contact01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_contact01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Search a contact</td>
+      <td><a href="#4"><img src="images/vignettes/Contact02.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_contact02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Contact view</td>
+    </tr>
+  </table>
+  <table class="picture">
+    <tr> 
+      <td><a href="#4"><img src="images/vignettes/Societe01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_societe01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Search an company</td>
+      <td><a href="#4"><img src="images/vignettes/Societe02.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_societe02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+       Company view</td>
+      <td></td>
+      <td></td>
 
-      <table>
-      <tr>
-      <td>
-      <blockquote>
-        <div align="left">New default theme
-          <br /><a href="images/snap1_big.png"><img src="images/snap1.png" alt="new theme" /></a>
-          <br />OBM 0.6.0
-        </div>
-      </blockquote>
+    </tr>
+  </table>
+  <h2>contract and incident views<a name="5"></a></h2>
 
-      </td><td>
-
-      <blockquote>
-        <div align="right">New calendar in action
-          <br /><a href="images/snap2_big.png"><img src="images/snap2.png" alt="calendar" /></a>
-          <br />OBM 0.6.4
-        </div>
-      </blockquote>
-
+    <table class="picture">
+     <tr> 
+      <td><a href="#4"><img src="images/vignettes/Contrats01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_contracts01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Contract view</td>
+      <td><a href="#4"><img src="images/vignettes/Incident01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_incident01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Search an incident</td>      
+      <td><a href="#4"><img src="images/vignettes/Incident02.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_incident02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Incident view</td>
+      <td><a href="#4"><img src="images/vignettes/Incident03.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_incident03.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Create an incident</td>
+      <td><br>
       </td>
-      </tr><tr>
-      <td colspan="2">
+      <td>&nbsp;</td>
+    </tr>
+   </table> 
 
-      <blockquote>
-        <div align="center">Main admin screen
-          <br /><a href="images/snap3_big.png"><img src="images/snap3.png" alt="new theme" /></a>
-          <br />OBM 0.6.0
-        </div>
-      </blockquote>
-      </td>
 
+  <h2>agenda views<a name="2"></a></h2>
+    <table class="picture">
+      <tr> 
+        
+      <td><a href="#2"><img src="images/vignettes/agenda01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_agenda01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+          Annual view</td>
+        
+      <td><a href="#2"><img src="images/vignettes/agenda02.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_agenda02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+          Month view</td>
+        
+      <td><a href="#2"><img src="images/vignettes/agenda03.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_agenda03.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+          Week view </td>
+        
+      <td><a href="#2"><img src="images/vignettes/agenda04.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_agenda04.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+          Day view</td>
       </tr>
-      </table>
+    </table>
+
+    <table class="picture">
+      <tr> 
+        
+      <td width="48%"><a href="#2"><img src="images/vignettes/agenda05.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_agenda05.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Create a meeting</td>
+        
+      <td width="48%"><a href="#2"><img src="images/vignettes/agenda06.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_agenda06.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+          user's Authorisation</td>
+        <td width="2%">&nbsp;</td>
+        <td width="2%">&nbsp;</td>
+      </tr>
+     </table>
+     <br>
+
+    <h2>File management views<a name="3"></a></h2>
+  <table class="picture">
+    <tr> 
+      <td><a href="#3"><img src="images/vignettes/documents01.png" alt="Zoom" width="150" height="110" border="0" onClick="MM_openBrWindow('zoom_documents01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Search a file</td>
+      <td><a href="#3"><img src="images/vignettes/documents02.png" alt="Zoom" width="150" height="110" border="0" onClick="MM_openBrWindow('zoom_documents02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        File view</td>
+      <td><a href="#3"><img src="images/vignettes/documents03.png" alt="Zoom" width="150" height="110" border="0" onClick="MM_openBrWindow('zoom_documents03.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Tree view </td>
+      <td><a href="#3"><img src="images/vignettes/documents04.png" alt="Zoom" width="150" height="110" border="0" onClick="MM_openBrWindow('zoom_documents04.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Create a file</td>
+    </tr>
+  </table>
+  <br>
+  <h2>Time management views<a name="3"></a></h2>
+  <table class="picture">
+    <tr> 
+      <td><a href="#3"><img src="images/vignettes/gestionTemps01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_gestionTemps01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Manageing your week</td>
+      <td><a href="#3"><img src="images/vignettes/gestionTemps02.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_gestionTemps02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        W eek view</td>
+      <td><a href="#3"><img src="images/vignettes/gestionTemps03.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_gestionTemps03.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Month view </td>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+  <br>
+  
+  <h2>Settings views<a name="5"></a></h2>
+  <table class="picture">
+    <tr> 
+      <td><a href="#5"><img src="images/vignettes/setting01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_setting01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Personnal setting</td>
+      <td><a href="#5"><img src="images/vignettes/utilisateurs01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_utilisateurs01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        user management</td>
+      <td><a href="#5"><img src="images/vignettes/utilisateurs02.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_utilisateurs02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        User view</td>
+      <td><a href="#5"><img src="images/vignettes/Admin01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_Admin01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Administration</td>
+    </tr>
+  </table>
+  <br>
+  <table class="picture">
+    <tr> 
+      <td><a href="#5"><img src="images/vignettes/Admin02.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_admin02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        Language management</td>
+      <td><a href="#5"><img src="images/vignettes/langue01.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_langue01.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        English translation</td>
+      <td><a href="#5"><img src="images/vignettes/langue02.png" alt="Zoom" border="0" onClick="MM_openBrWindow('zoom_langue02.html','ScreenCaptured','toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=820,height=660')"></a><br>
+        English translation</td>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+  <br>
+</div>
+</div>
+
+
 
 <?php include("footer.html"); ?>
 
