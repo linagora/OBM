44f1973f3b06890a9dc61de7861571a14f0ccc6a
pierre <pierre@localhost>
2004-03-24 16:57:59 +0000

    Bookmarks and Todo titles in lang !, contact subscription on deletion, sort,.. Modified Files: 	doc/html/changelog.html obminclude/global_display.inc 	obminclude/global_query.inc obminclude/obm_conf.inc 	obminclude/lang/en/contact.inc obminclude/lang/en/global.inc 	obminclude/lang/fr/contact.inc obminclude/lang/fr/global.inc 	obminclude/lang/it/contact.inc 	obminclude/themes/grey/display.inc 	obminclude/themes/standard/display.inc 	php/company/company_query.inc php/contact/contact_display.inc 	php/contact/contact_query.inc 	scripts/0.8/obmdb_default_values_0.8.sql

--- ui/scripts/0.8/obmdb_default_values_0.8.sql
+++ ui/scripts/0.8/obmdb_default_values_0.8.sql
@@ -92,11 +92,12 @@ INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,displa
 INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_firstname',2,1);
 INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_archive',3,1);
 INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_function',4,1);
-INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_company_name',5,2);
-INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_phone',6,1);
-INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_homephone',7,1);
-INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_mobilephone',8,1);
-INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_email',9,1);
+INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_title',5,1);
+INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_company_name',6,2);
+INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_phone',7,1);
+INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_homephone',8,1);
+INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_mobilephone',9,1);
+INSERT INTO DisplayPref (display_user_id,display_entity,display_fieldname,display_fieldorder,display_display) values (0,'contact','contact_email',10,1);
 
 -- module 'company'
 
