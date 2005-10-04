c718d295b1984d503cf6df43b9a544acdbf5e26d
pierre <pierre@localhost>
2005-10-04 09:52:58 +0000

    fixes for preferences and exportcsv Modified Files: 	obminclude/of/of_display.inc php/exportcsv/exportcsv_index.php

--- ui/php/exportcsv/exportcsv_index.php
+++ ui/php/exportcsv/exportcsv_index.php
@@ -39,6 +39,13 @@ if (($action == "index") || ($action == "")) {
 
 } elseif ($action == "export_page")  {
 ///////////////////////////////////////////////////////////////////////////////
+  $emodule = $params["module"];
+  if ($emodule != "") {
+    require_once("$obminclude/lang/$set_lang/${emodule}.inc");
+    require_once("$path/$emodule/${emodule}_display.inc");
+  } else {
+    $emodule = "obm";
+  }
   $first_row = $params["first_row"];
   $nb_rows = $params["nb_rows"];
   $query = stripslashes($params["query"]);
@@ -46,22 +53,12 @@ if (($action == "index") || ($action == "")) {
     // We remove the limit clause
     $query = preg_replace("/(limit .*)$/i", "", $query);
   }
-  $query_pref = stripslashes($params["query_pref"]);
-  $module = $params["module"];
-  if ($module != "") {
-    require_once("$obminclude/lang/$set_lang/${module}.inc");
-    require_once("$path/$module/${module}_display.inc");
-  } else {
-    $module = "obm";
-  }
+  
+  $prefs = get_display_pref($auth->auth["uid"], $emodule); // XXXX entity here?
   
   display_debug_msg($query, $cdg_sql);
   $obm_q = new DB_OBM;
   $obm_q->query($query);
-
-  display_debug_msg($query_pref, $cdg_sql);
-  $pref_q = new DB_OBM;
-  $pref_q->query($query_pref);
 
   // Set separator (if not set in setting => ;)
   if (($set_csv_sep != $ccsvd_sc) && ($set_csv_sep != $ccsvd_tab)) {
@@ -72,11 +69,11 @@ if (($action == "index") || ($action == "")) {
     $sep = $set_csv_sep;
   }
 
-  $export_d = new OBM_DISPLAY("DATA", $pref_q);
+  $export_d = new OBM_DISPLAY("DATA", $prefs, $emodule);
   $export_d->data_set = $obm_q;
-  $export_d->display_module = $module;
+  //  $export_d->display_module = $module;
   header("Content-Type: text/comma-separated-values");
-  header("Content-Disposition: attachment; filename=\"$module.csv\"");
+  header("Content-Disposition: attachment; filename=\"$emodule.csv\"");
   $export_d->dis_data_file($first_row, $nb_rows, $sep, $params["function"]);
 }
 
@@ -86,13 +83,12 @@ if (($action == "index") || ($action == "")) {
 // returns : $params hash with parameters set
 ///////////////////////////////////////////////////////////////////////////////
 function get_param_export() {
-  global $first_row, $nb_rows, $query, $query_pref, $call_module, $func_data;
+  global $first_row, $nb_rows, $query, $call_module, $func_data;
   global $cdg_param;
 
   if (isset ($first_row)) $params["first_row"] = $first_row;
   if (isset ($nb_rows)) $params["nb_rows"] = $nb_rows;
   if (isset ($query)) $params["query"] = $query;
-  if (isset ($query_pref)) $params["query_pref"] = $query_pref;
   if (isset ($call_module)) $params["module"] = $call_module;
   if (isset ($func_data)) $params["function"] = $func_data;
 
