019b8869005d9f6926947f027b60f467fb14f257
Nicolas <nicolas@linagora.com>
2007-06-07 10:27:15 +0000

    script for msyql

--- funambol/src/sql/mysql/init_schema.sql
+++ funambol/src/sql/mysql/init_schema.sql
@@ -1,28 +1,35 @@
 --
--- Initialization data for the Dummy module
--- @version $Id: init_schema.sql,v 1.1 2006/01/18 16:04:56 luigiafassina Exp $
+-- Initialization data for the Funambol-OBM module
 --
 
 --
 -- Module structure registration
 --
 
-delete from fnbl_sync_source_type where id='dummy';
+delete from fnbl_sync_source_type where id='obm-contact';
 insert into fnbl_sync_source_type(id, description, class, admin_class)
-values('dummy','Dummy SyncSource','com.funambol.examples.engine.source.DummySyncSource','com.funambol.examples.admin.DummySyncSourceConfigPanel');
+values('obm-contact','OBM Contact SyncSource','fr.aliasource.funambol.engine.source.ContactSyncSource','fr.aliasource.funambol.admin.ObmSyncSourceConfigPanel');
 
-delete from fnbl_module where id='dummy';
+delete from fnbl_sync_source_type where id='obm-calendar';
+insert into fnbl_sync_source_type(id, description, class, admin_class)
+values('obm-calendar','OBM Calendar SyncSource','fr.aliasource.funambol.engine.source.CalendarSyncSource','fr.aliasource.funambol.admin.ObmSyncSourceConfigPanel');
+
+delete from fnbl_module where id='obm';
 insert into fnbl_module (id, name, description)
-values('dummy','dummy','Dummy');
+values('obm','obm','Funambol OBM Connector');
 
-delete from fnbl_connector where id='dummy';
+delete from fnbl_connector where id='obm';
 insert into fnbl_connector(id, name, description, admin_class)
-values('dummy','FunambolDummyConnector','Funambol Dummy Connector','');
+values('obm','Funambol OBM Connector','Funambol OBM Connector','');
 
-delete from fnbl_connector_source_type where connector='dummy' and sourcetype='dummy';
+delete from fnbl_connector_source_type where connector='obm' and sourcetype='obm-contact';
 insert into fnbl_connector_source_type(connector, sourcetype)
-values('dummy','dummy');
+values('obm','obm-contact');
 
-delete from fnbl_module_connector where module='dummy' and connector='dummy';
+delete from fnbl_connector_source_type where connector='obm' and sourcetype='obm-calendar';
+insert into fnbl_connector_source_type(connector, sourcetype)
+values('obm','obm-calendar');
+
+delete from fnbl_module_connector where module='obm' and connector='obm';
 insert into fnbl_module_connector(module, connector)
-values('dummy','dummy');
+values('obm','obm');
