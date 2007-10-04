87c8fc80ec7f658bdfc2fc20e6ee67e4033478a2
Sylvain Garcia <sgarcia@linagora.com>
2007-10-04 16:57:17 +0000

    mise en place d'OBM dan obm-core

--- ui/debian/misc/upgrade2.0-2.1.sh
+++ ui/debian/misc/upgrade2.0-2.1.sh
@@ -1,29 +1,16 @@
 #!/bin/sh
 #Shell script to upgrade OBM 2.0 to 2.1
 
-loginBdObm ()
-{
-	grep "dbuser" /etc/obm/debian-db-obm.conf | cut -d"=" -f2
-}
+OBM_USER=`grep "dbuser" /etc/obm/debian-db-obm.conf | cut -d"=" -f2`
+OBM_PASSWD=`grep "dbpass" /etc/obm/debian-db-obm.conf | cut -d"=" -f2`	
+OBM_DBNAME=`grep "dbname" /etc/obm/debian-db-obm.conf | cut -d"=" -f2`	
+OBM_HOST=`grep "dbserver" /etc/obm/debian-db-obm.conf | cut -d"=" -f2`
 
-passwdBdObm ()
-{
-	grep "dbpass" /etc/obm/debian-db-obm.conf | cut -d"=" -f2	
-}
+cd /usr/share/obm/www/scripts/2.1/
+/usr/bin/mysql -u ${OBM_USER} -p${OBM_PASSWD} ${OBM_DBNAME} < /usr/share/obm/www/scripts/2.1/update-2.0-2.1.mysql.sql || true
+/usr/bin/php -d include_path="/usr/share/obm/www"  /usr/share/obm/www/scripts/2.1/update-2.0-2.1.php || true
 
-nameBdObm ()
-{
-	grep "dbname" /etc/obm/debian-db-obm.conf | cut -d"=" -f2	
-}
-
-LOGIN=loginBdObm
-PASSWD=passwdBdObm
-BD=nameBdObm
-
-#/usr/bin/mysql -u $LOGIN -p$PASSWD $BD < /usr/share/obm/scripts/2.1/update-2.0-2.1.mysql.sql
-#/usr/bin/php -d 
-
-touch /tmp/upgra20to21
+#touch /tmp/upgra20to21
 
 
 exit 0
