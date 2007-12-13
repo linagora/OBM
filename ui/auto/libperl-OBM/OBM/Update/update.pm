1853ccd3d3a155a5de1fc54ac728d7f6d4566133
Anthony <anthony@linagora.com>
2007-12-13 14:00:40 +0000

    Ignore les groupes privés dans la suppression différentielle en mode global

--- ui/auto/libperl-OBM/OBM/Update/update.pm
+++ ui/auto/libperl-OBM/OBM/Update/update.pm
@@ -394,7 +394,7 @@ sub _doGlobalDelete {
 
 
     # Traitement des entités de type 'groupe'
-    $query = "SELECT group_id FROM P_UGroup WHERE group_domain_id=".$self->{"domain"}." AND group_id NOT IN (SELECT group_id FROM UGroup WHERE group_domain_id=".$self->{"domain"}.")";
+    $query = "SELECT group_id FROM P_UGroup WHERE group_domain_id=".$self->{"domain"}." AND group_privacy=0 AND group_id NOT IN (SELECT group_id FROM UGroup WHERE group_domain_id=".$self->{"domain"}." AND group_privacy=0)";
     if( !&OBM::dbUtils::execQuery( $query, $self->{"dbHandler"}, \$queryResult ) ) {
         &OBM::toolBox::write_log( "[Update::update]: probleme lors de l'execution d'une requete SQL : ".$self->{"dbHandler"}->err, "W" );
         return 0;
