dc9b9862eabd156730b579c3650d3786acd569ed
pierre <pierre@localhost>
2008-01-10 00:56:45 +0000

    minor fixes in lang, backup, regexp

--- ui/php/backup/backup_index.php
+++ ui/php/backup/backup_index.php
@@ -17,16 +17,16 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 
-$path = "..";
-$module = "backup";
-$obminclude = getenv("OBM_INCLUDE_VAR");
-if ($obminclude == "") $obminclude = "obminclude";
+$path = '..';
+$module = 'backup';
+$obminclude = getenv('OBM_INCLUDE_VAR');
+if ($obminclude == '') $obminclude = 'obminclude';
 include("$obminclude/global.inc");
-$backup = get_param_backup();
-page_open(array("sess" => "OBM_Session", "auth" => $auth_class_name, "perm" => "OBM_Perm"));
+$params = get_param_backup();
+page_open(array('sess' => 'OBM_Session', 'auth' => $auth_class_name, 'perm' => 'OBM_Perm'));
 include("$obminclude/global_pref.inc");
-require("backup_query.inc");
-require("backup_display.inc");
+require('backup_query.inc');
+require('backup_display.inc');
 
 get_backup_action();
 $perm->check_permissions($module, $action);
@@ -36,67 +36,67 @@ page_close();
 ///////////////////////////////////////////////////////////////////////////////
 // Main Program                                                              //
 ///////////////////////////////////////////////////////////////////////////////
-if ($action == "index") {
+if ($action == 'index') {
 ///////////////////////////////////////////////////////////////////////////////
-  $display["search"] = dis_backup_search_form($backup);
-  if ($_SESSION['set_display'] == "yes") {
-    $display["result"] = dis_backup_search_list($backup);
+  $display['search'] = dis_backup_search_form($params);
+  if ($_SESSION['set_display'] == 'yes') {
+    $display['result'] = dis_backup_search_list($params);
   } else {
-    $display["msg"] .= display_info_msg($l_no_display);
+    $display['msg'] .= display_info_msg($l_no_display);
   }
 
-} elseif ($action == "search") {
+} elseif ($action == 'search') {
 ///////////////////////////////////////////////////////////////////////////////
-  $display["search"] = dis_backup_search_form($backup);
-  $display["result"] = dis_backup_search_list($backup);
+  $display['search'] = dis_backup_search_form($params);
+  $display['result'] = dis_backup_search_list($params);
   
-} elseif ($action == "new") {
+} elseif ($action == 'new') {
 ///////////////////////////////////////////////////////////////////////////////
-  $display["detail"] = dis_backup_form($backup);
+  $display['detail'] = dis_backup_form($params);
   
-} elseif ($action == "insert") {
+} elseif ($action == 'insert') {
 ///////////////////////////////////////////////////////////////////////////////
   $ret = run_query_backup_create();
   if ($ret) {
-    $display["msg"] .= display_ok_msg("$l_backup : $l_insert_ok");
+    $display['msg'] .= display_ok_msg("$l_backup : $l_insert_ok");
   } else {
-    $display["msg"] .= display_err_msg("$l_backup : $l_insert_error $err[msg]");
+    $display['msg'] .= display_err_msg("$l_backup : $l_insert_error $err[msg]");
   }
   dis_backup_index();
 
-} elseif ($action == "restore") {
+} elseif ($action == 'restore') {
 ///////////////////////////////////////////////////////////////////////////////
-  $ret = run_query_backup_restore($backup["filename"]);
+  $ret = run_query_backup_restore($params['filename']);
   if ($ret) {
-    $display["msg"] .= display_ok_msg("$l_backup : $l_restore_ok");
+    $display['msg'] .= display_ok_msg("$l_backup : $l_restore_ok");
   } else {
-    $display["msg"] .= display_err_msg("$l_backup : $l_restore_error $err[msg]");
+    $display['msg'] .= display_err_msg("$l_backup : $l_restore_error $err[msg]");
   }
   dis_backup_index();
 
-} elseif ($action == "check_delete") {
+} elseif ($action == 'check_delete') {
 ///////////////////////////////////////////////////////////////////////////////
-  if (check_backup_can_delete($backup["filename"])) {
-    $display["msg"] .= display_info_msg($err["msg"]);
-    $display["detail"] = dis_can_delete_backup($backup["filename"]);
+  if (check_backup_can_delete($params['filename'])) {
+    $display['msg'] .= display_info_msg($err['msg']);
+    $display['detail'] = dis_can_delete_backup($params['filename']);
   } else {
-    $display["msg"] .= display_warn_msg($err["msg"]);
-    $display["detail"] = dis_backup_index($backup);
+    $display['msg'] .= display_warn_msg($err['msg']);
+    $display['detail'] = dis_backup_index($params);
   }
 
-} elseif ($action == "delete") {
+} elseif ($action == 'delete') {
 ///////////////////////////////////////////////////////////////////////////////
-  if (check_backup_can_delete($backup["filename"])) {
-    $retour = run_query_backup_delete($backup["filename"]);
+  if (check_backup_can_delete($params['filename'])) {
+    $retour = run_query_backup_delete($params['filename']);
     if ($retour) {
-      $display["msg"] .= display_ok_msg("$l_backup : $l_delete_ok");
+      $display['msg'] .= display_ok_msg("$l_backup : $l_delete_ok");
     } else {
-      $display["msg"] .= display_err_msg("$l_backup : $err[msg] : $l_delete_error");
+      $display['msg'] .= display_err_msg("$l_backup : $err[msg] : $l_delete_error");
     }
-    dis_backup_index($backup);
+    dis_backup_index($params);
   } else {
-    $display["msg"] .= display_warn_msg("$err[msg] $l_cant_delete");
-    dis_backup_index($backup);
+    $display['msg'] .= display_warn_msg("$err[msg] $l_cant_delete");
+    dis_backup_index($params);
   }
 }
 
@@ -104,29 +104,23 @@ if ($action == "index") {
 ///////////////////////////////////////////////////////////////////////////////
 // Display
 ///////////////////////////////////////////////////////////////////////////////
-$display["head"] = display_head($l_backup);
-if (! $backup["popup"]) {
-  $display["header"] = display_menu($module);
+$display['head'] = display_head($l_backup);
+if (! $params['popup']) {
+  $display['header'] = display_menu($module);
 }
-$display["end"] = display_end();
+$display['end'] = display_end();
 
 display_page($display);
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// Stores Backup parameters transmited in $backup hash
-// returns : $backup hash with parameters set
+// Stores Backup parameters transmited in $params hash
+// returns : $params hash with parameters set
 ///////////////////////////////////////////////////////////////////////////////
 function get_param_backup() {
-  global $tf_filename, $tf_version, $tf_date, $param_backup, $popup; 
 
   // Get global params
-  $params = get_global_params("Backup");
-
-  if (isset ($param_backup)) $params["filename"] = $param_backup;
-  if (isset ($tf_filename)) $params["filename"] = $tf_filename;
-  if (isset ($tf_version)) $params["version"] = $tf_version;
-  if (isset ($tf_date)) $params["date"] = $tf_date;
+  $params = get_global_params('Backup');
 
   return $params;
 }
@@ -136,12 +130,12 @@ function get_param_backup() {
 //  Backup Action 
 ///////////////////////////////////////////////////////////////////////////////
 function get_backup_action() {
-  global $dbackup, $actions, $path;
+  global $$params, $actions, $path;
   global $l_header_list, $l_header_find, $l_header_new, $l_header_delete;
   global $cright_read, $cright_write, $cright_read_admin, $cright_write_admin;
 
 // Index  
-  $actions["backup"]["index"] = array (
+  $actions['backup']['index'] = array (
     'Name'     => $l_header_list,
     'Url'      => "$path/backup/backup_index.php?action=index",
     'Right'    => $cright_read_admin,
@@ -149,14 +143,14 @@ function get_backup_action() {
                                     	 );
 
 // Search
-  $actions["backup"]["search"] = array (
+  $actions['backup']['search'] = array (
     'Url'      => "$path/backup/backup_index.php?action=search",
     'Right'    => $cright_read_admin,
     'Condition'=> array ('None') 
                                     	 );
 
 // New
-  $actions["backup"]["new"] = array (
+  $actions['backup']['new'] = array (
     'Name'     => $l_header_new,
     'Url'      => "$path/backup/backup_index.php?action=new",
     'Right'    => $cright_write,
@@ -164,15 +158,15 @@ function get_backup_action() {
                                      );
 
 // CheckDelete
-  $actions["backup"]["check_delete"] = array (
-    'Url'      => "$path/backup/backup_index.php?action=check_delete&amp;backup_id=".$backup["filename"]."",
+  $actions['backup']['check_delete'] = array (
+    'Url'      => "$path/backup/backup_index.php?action=check_delete&amp;backup_id=".$backup['filename'],
     'Right'    => $cright_write,
     'Privacy'  => true,
     'Condition'=> array ('None') 
                                      	      );
 
 // Delete
-  $actions["backup"]["delete"] = array (
+  $actions['backup']['delete'] = array (
     'Url'      => "$path/backup/backup_index.php?action=delete",
     'Right'    => $cright_write,
     'Privacy'  => true,
@@ -180,14 +174,14 @@ function get_backup_action() {
                                      	      );
 
 // Insert
-  $actions["backup"]["insert"] = array (
+  $actions['backup']['insert'] = array (
     'Url'      => "$path/backup/backup_index.php?action=insert",
     'Right'    => $cright_write,
     'Condition'=> array ('None') 
                                      	 );  
 
 // Restore
-  $actions["backup"]["restore"] = array (
+  $actions['backup']['restore'] = array (
     'Url'      => "$path/backup/backup_index.php?action=restore",
     'Right'    => $cright_write,
     'Condition'=> array ('None') 
