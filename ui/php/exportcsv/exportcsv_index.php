21f3492b419cbb291fb82ef207443eb963464dd3
pierre <pierre@localhost>
2008-02-04 00:34:09 +0000

    security fix in exportcsv, fix project and invoice csv export, user nomad sort

--- ui/php/exportcsv/exportcsv_index.php
+++ ui/php/exportcsv/exportcsv_index.php
@@ -11,13 +11,13 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Session, Auth, Perms Management                                           //
 ///////////////////////////////////////////////////////////////////////////////
-$path = "..";
-$module = "exportcsv";
-$obminclude = getenv("OBM_INCLUDE_VAR");
-if ($obminclude == "") $obminclude = "obminclude";
+$path = '..';
+$module = 'exportcsv';
+$obminclude = getenv('OBM_INCLUDE_VAR');
+if ($obminclude == '') $obminclude = 'obminclude';
 include("$obminclude/global.inc");
 $params = get_export_params();
-page_open(array("sess" => "OBM_Session", "auth" => $auth_class_name, "perm" => "OBM_Perm"));
+page_open(array('sess' => 'OBM_Session', 'auth' => $auth_class_name, 'perm' => 'OBM_Perm'));
 include("$obminclude/global_pref.inc");
 page_close();
 
@@ -25,61 +25,71 @@ page_close();
 // Main program                                                              //
 //    Action : (index, document)                                             //
 ///////////////////////////////////////////////////////////////////////////////
-if (($action == "index") || ($action == "")) {
-  echo "Action incorrecte";
+if (($action == 'index') || ($action == '')) {
+  echo $l_invalid_action;
   dis_end();
 
-} elseif ($action == "message")  {
+} elseif ($action == 'message')  {
 ///////////////////////////////////////////////////////////////////////////////
-  dis_head("Téléchargement");        // Head & Body
-  echo "Téléchargement en cours";
+  dis_head($l_download);        // Head & Body
+  echo $l_download_active;
   dis_end();
 
-} elseif ($action == "export_page")  {
+} elseif ($action == 'export_page')  {
 ///////////////////////////////////////////////////////////////////////////////
-  $emodule = $params["module"];
-  if ($emodule != "") {
+  $emodule = $params['module'];
+  if ($emodule != '') {
     require_once("$obminclude/lang/".$_SESSION['set_lang']."/${emodule}.inc");
     require_once("$path/$emodule/${emodule}_display.inc");
   } else {
-    $emodule = "obm";
+    $emodule = 'obm';
   }
-  $entity = $params["entity"];
-  if ($entity == "") {
+  $entity = $params['entity'];
+  if ($entity == '') {
     $entity = $emodule;
   }
-  $first_row = $params["first_row"];
-  $nb_rows = $params["nb_rows"];
-  $query = stripslashes($params["query"]);
+  $first_row = $params['first_row'];
+  $nb_rows = $params['nb_rows'];
+  $query = stripslashes($params['query']);
 
   if ($ctu_sql_limit && ($nb_rows > $_SESSION['set_rows'])) {
     // We remove the limit clause
-    $query = preg_replace("/(limit .*)$/i", "", $query);
+    $query = preg_replace("/(limit .*)$/i", '', $query);
   }
   
-  $prefs = get_display_pref($obm["uid"], $entity);
+  // Check if banned words are included
+  $excluded_words = array ('insert', 'update', 'delete', 'create', 'alter', 'drop', 'userobm_password');
+  while ( list($key, $value) = each($excluded_words) ) {
+    if (preg_match("/\b$value\b/i", $query)) {
+      $err['msg'] = "$l_err_query_banned $l_banned_word : $value";
+      echo $err['msg'];
+      return false;
+    }
+  }
+
+  $prefs = get_display_pref($obm['uid'], $entity);
   
-  display_debug_msg($query, $cdg_sql, "export()");
+  display_debug_msg($query, $cdg_sql, 'export()');
   $obm_q = new DB_OBM;
   $obm_q->query($query);
 
   // Set separator (if not set in setting => ;)
   if (($_SESSION['set_csv_sep'] != $ccsvd_sc) && ($_SESSION['set_csv_sep'] != $ccsvd_tab)) {
-    $sep = ";";
+    $sep = ';';
   } else if ($_SESSION['set_csv_sep'] == $ccsvd_tab) {
     $sep = "\t";
   } else {
     $sep = $_SESSION['set_csv_sep'];
   }
 
-  $export_d = new OBM_DISPLAY("DATA", $prefs, $emodule);
+  $export_d = new OBM_DISPLAY('DATA', $prefs, $emodule);
   $export_d->display_entity = "$entity";
   $export_d->data_set = $obm_q;
-  header("Content-Type: text/comma-separated-values");
+  header('Content-Type: text/comma-separated-values');
   header("Content-Disposition: attachment; filename=\"$entity.csv\"");
   header('Cache-Control: maxage=3600'); 
   header('Pragma: public');
-  $export_d->dis_data_file($first_row, $nb_rows, $sep, $params["function"]);
+  $export_d->dis_data_file($first_row, $nb_rows, $sep, $params['function']);
 }
 
 
@@ -89,10 +99,10 @@ if (($action == "index") || ($action == "")) {
 ///////////////////////////////////////////////////////////////////////////////
 function get_export_params() {
 
-  $params = get_global_params("ExportCSV");
+  $params = get_global_params('ExportCSV');
 
-  if (isset ($params["call_module"])) $params["module"] = $params["call_module"];
-  if (isset ($params["func_data"])) $params["function"] = $params["func_data"];
+  if (isset ($params['call_module'])) $params['module'] = $params['call_module'];
+  if (isset ($params['func_data'])) $params['function'] = $params['func_data'];
 
   return $params;
 }
