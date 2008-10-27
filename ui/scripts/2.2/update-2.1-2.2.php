9e0c9dd730381cdbb12836a768cb996ffb04cce2
Madhi <medhi@linagora.com>
2008-10-27 18:58:27 +0000

    fixing php update script : bad request for global domain

--- ui/scripts/2.2/update-2.1-2.2.php
+++ ui/scripts/2.2/update-2.1-2.2.php
@@ -25,19 +25,21 @@ function make_profiles() {
   global $profiles, $cdg_sql;
   global $obm;
 
-  $query = "SELECT domain_id from Domain where domain_name='global.virt'";
+  $query = "SELECT domain_id from Domain where domain_global=TRUE";
   $obm_q = new DB_OBM;
   $obm_q->query($query);
   $obm_q->next_record();
   $global_domain_id = $obm_q->f('domain_id');
   
+  echo "Deleting profiles in database (multiple script execution)\n";
   if(!isset($profiles['admin'])) {
     $profiles['admin'] =  array ('section' => array ('default' => 1),'module' => array ('default' => $perm_admin,'domain' => 0));
   }
   if(count($profiles) > 0) {
     $query = "DELETE FROM Profile WHERE profile_name IN ('".implode("','",$profiles)."')";
   }
-
+  
+  echo "Parsing configuration files : ".count($profiles)." profiles found\n";
   $query = "SELECT *  FROM ProfileProperty";
   $obm_q->query($query);
   while($obm_q->next_record()) {
@@ -45,7 +47,7 @@ function make_profiles() {
   }
   foreach ($profiles as $name => $data) {
     $name = addslashes($name);
-    
+    echo "** Inserting profile : $name \n"; 
     $query = "INSERT INTO Profile (
         	profile_timecreate,
         	profile_domain_id,
@@ -61,6 +63,7 @@ function make_profiles() {
     $obm_q->next_record();
     $profile_id = $obm_q->f('profile_id');
     foreach ($data['section'] as $section_name => $section) {
+      echo "**** Right on section $section_name : $section \n";
       $query = "INSERT INTO ProfileSection (
             profilesection_section_name,
             profilesection_domain_id,
@@ -78,6 +81,7 @@ function make_profiles() {
     // Insert Modules
     foreach ($data['module'] as $module_name => $module) {
       $module_value = hexdec($module_value);
+      echo "**** Right on module $module_name : $module \n";
       $query = "INSERT INTO ProfileModule (
             profilemodule_module_name,
             profilemodule_domain_id,
@@ -101,6 +105,7 @@ function make_profiles() {
       } else {
         $property = $property_data['default'];
       }
+      echo "**** Setting property $property_name : $property\n";
       $query = "INSERT INTO ProfilePropertyValue (
               	profilepropertyvalue_profile_id,
               	profilepropertyvalue_property_id,
