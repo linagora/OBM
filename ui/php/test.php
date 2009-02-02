b30295936d5871bd07a29896022e4b8fba26598b
Madhi <medhi@linagora.com>
2009-02-02 17:41:40 +0000

    Fixing database query, calendar day planning view onclick event to create a new event

--- ui/php/test.php
+++ ui/php/test.php
@@ -1,1 +1,25 @@
-<?php phpinfo(); ?>
+<?php 
+/*Only one profiled function within*/
+function oneDoThousandArray() {
+  doThousandArray();
+}
+
+function doThousandArray() {
+  for($i=0; $i<1000;$i++) {
+    $test = array(); 
+  }
+}
+/*1000 profiled function within*/
+function oneThoushandDoArray() {
+  for($i=0; $i<1000;$i++) {
+    doArray();
+  }
+}
+
+function doArray() {
+  $test = array(); 
+}
+
+oneDoThousandArray();
+oneThoushandDoArray();
+?>
