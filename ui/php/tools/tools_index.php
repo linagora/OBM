1bd37f70283a30d897848757ae44307c0197acd9
Madhi <medhi@linagora.com>
2009-02-06 13:54:49 +0000

    removing debug message

--- ui/php/tools/tools_index.php
+++ ui/php/tools/tools_index.php
@@ -121,7 +121,6 @@ if ($action == 'cancel_update') {
     set_update_state($params['domain_id']);
     store_update_data($params);
     $res = exec_tools_update_update($params);
-    var_dump($res);
     if ($res === 0) {
       $display['msg'] .= display_ok_msg($l_upd_running);
       $display['detail'] = dis_tools_update_detail();
