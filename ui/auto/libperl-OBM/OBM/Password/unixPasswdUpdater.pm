c2e0a5d3e353c8288b90170bf6cf3a077df97333
Anthony <anthony@linagora.com>
2009-02-07 22:02:56 +0000

    Adding Unix password change support with 'changePasswd.pl' script

---
+++ ui/auto/libperl-OBM/OBM/Password/unixPasswdUpdater.pm
@@ -0,0 +1,66 @@
+package OBM::Password::unixPasswdUpdater;
+
+$VERSION = '1.0';
+
+use OBM::Ldap::ldapEngine;
+use OBM::Password::passwd;
+use OBM::Ldap::utils;
+@ISA = ('OBM::Ldap::ldapEngine', 'OBM::Password::passwd', 'OBM::Ldap::utils');
+
+$debug = 1;
+
+
+sub update {
+    my $self = shift;
+    my( $entity, $passwd ) = @_;
+
+
+    if( !defined($entity) ) {
+        $self->_log( 'entité non définie', 3 );
+        return 1;
+    }elsif( !ref($entity) ) {
+        $self->_log( 'entité incorrecte', 3 );
+        return 1;
+    }elsif( ref($entity) ne 'OBM::Entities::obmUser' ) {
+        $self->_log( 'type d\'entité \''.ref($entity).' non supporté', 0 );
+        return 1;
+    }
+    $self->{'currentEntity'} = $entity;
+
+
+    if( !defined($passwd) ) {
+        $self->_log( 'pas de nouveau mot de passe', 4 );
+        return 0;
+    }
+
+
+    # Obtention des DNs de l'entité mise à jour
+    my $currentEntityDNs = $entity->getCurrentDnPrefix();
+
+    for( my $i=0; $i<=$#{$currentEntityDNs}; $i++ ) {
+        my $updateLdapEntity = $self->_searchLdapEntityByDN( $currentEntityDNs->[$i] );
+
+        if( defined($updateLdapEntity) && !ref($updateLdapEntity) ) {
+            return 1;
+        }
+
+        if( $passwd = $self->_convertPasswd( 'PLAIN', $passwd) ) {
+            if( $self->_modifyAttr( $passwd , $updateLdapEntity, 'userPassword' ) ) {
+                if( $self->_ldapUpdateEntity($updateLdapEntity) ) {
+                    $self->_log( 'échec de mise à jour de l\'entréé LDAP', 3 );
+                    return 1;
+                }
+            }else {
+                $self->_log( 'échec de mise à jour du mot de passe Unix', 3 );
+                return 1;
+            }
+        }else {
+            $self->_log( 'echec de conversion du mot de passe Unix', 3 );
+            return 1;
+        }
+    }
+
+    $self->_log( 'mot de passe Unix mis à jour', 2 );
+
+    return 0;
+}
--- ui/auto/libperl-OBM/OBM/Update/updatePassword.pm
+++ ui/auto/libperl-OBM/OBM/Update/updatePassword.pm
@@ -1,6 +1,6 @@
 package OBM::Update::updatePassword;
 
-$VERSION = "1.0";
+$VERSION = '1.0';
 
 $debug = 1;
 
@@ -8,126 +8,189 @@ use 5.006_001;
 require Exporter;
 use strict;
 
-
-#require OBM::toolBox;
-require OBM::Update::utils;
-require OBM::Ldap::ldapEngine;
-require OBM::Entities::obmUser;
+use OBM::Tools::commonMethods qw(_log dump);
+use OBM::Update::commonUpdate qw(   
+            _getUserIdFromUserLoginDomain
+            );
 
 
 sub new {
-    my $self = shift;
+    my $class = shift;
     my( $parameters ) = @_;
 
-    # Définition des attributs de l'objet
-    my %updatePasswdAttr = (
-        oldPassword => undef,
-        newPasswordDesc => undef,
-        userLogin => undef,
-        userId => undef,
-        userObject => undef,
-        domainId => undef,
-        domainList => undef,
-        engine => undef
-    );
+    my $self = bless { }, $class;
+
 
     if( !defined($parameters) ) {
-        croak( "[Update::updatePassword]: Usage: PACKAGE->new(DBHANDLER, PARAMLIST)" );
-    }
-
-
-    if( !$parameters->{"no-old"} ) {
-        $updatePasswdAttr{oldPassword} = $parameters->{"old-passwd"};
-    }
-
-    $updatePasswdAttr{newPasswordDesc}->{newPassword} = $parameters->{passwd};
-    $updatePasswdAttr{newPasswordDesc}->{newPasswordType} = $parameters->{type};
-    $updatePasswdAttr{newPasswordDesc}->{unix} = $parameters->{unix};
-    $updatePasswdAttr{newPasswordDesc}->{samba} = $parameters->{samba};
-    $updatePasswdAttr{newPasswordDesc}->{sql} = $parameters->{sql};
-
-    $updatePasswdAttr{userLogin} = $parameters->{login};
-    $updatePasswdAttr{domainId} = $parameters->{domain};
-
-
-    # Obtention du userId BD
-    $updatePasswdAttr{userId} = &OBM::Update::utils::getUserIdFromUserLoginDomain( $updatePasswdAttr{userLogin}, $updatePasswdAttr{domainId} );
-    if( !defined($updatePasswdAttr{userId}) ) {
-        &OBM::toolBox::write_log( "[Update::updatePassword]: utilisateur '".$updatePasswdAttr{userLogin}."' inconnu", "W" );
+        $self->_log( 'paramètres d\'initialisation non définis', 0 );
         return undef;
     }
 
 
-    # Obtention des informations sur les domaines nécessaires
-    $updatePasswdAttr{domainList} = &OBM::Update::utils::getDomains( $updatePasswdAttr{domainId} );
+    if( !$parameters->{'no-old'} ) {
+        $self->{'oldPassword'} = $parameters->{'old-passwd'};
+    }
 
-    # Obtention des serveurs LDAP par domaines
-    &OBM::Update::utils::getLdapServer( $updatePasswdAttr{domainList} );
+    $self->{'newPasswordDesc'}->{'newPassword'} = $parameters->{'passwd'};
+    $self->{'newPasswordDesc'}->{'unix'} = $parameters->{'unix'};
+    $self->{'newPasswordDesc'}->{'samba'} = $parameters->{'samba'};
+    $self->{'newPasswordDesc'}->{'sql'} = $parameters->{'sql'};
 
-    # Initialisation du moteur LDAP
-    $updatePasswdAttr{engine}->{ldapEngine} = OBM::Ldap::ldapEngine->new( $updatePasswdAttr{domainList} );
-    if( !defined($updatePasswdAttr{engine}->{ldapEngine}) ) {
-        &OBM::toolBox::write_log( "[Update::updatePassword]: probleme a l'initialisation du moteur LDAP.", "W" );
+    $self->{'userLogin'} = $parameters->{'login'};
+    $self->{'domainId'} = $parameters->{'domain'};
+
+    require OBM::Ldap::ldapServers;
+    if( !($self->{'ldapservers'} = OBM::Ldap::ldapServers->instance()) ) {
+        $self->_log( 'initialisation du gestionnaire de serveur LDAP impossible', 3 );
         return undef;
     }
 
-    if( !$updatePasswdAttr{"engine"}->{"ldapEngine"}->init( 0 ) ) {
-        &OBM::toolBox::write_log( "[Update::updatePassword]: probleme a l'initialisation du moteur LDAP.", "W" );
+    if( $self->_getEntity() ) {
         return undef;
     }
 
+    return $self;
+}
 
-    # Création de l'objet de l'utilisateur
-    $updatePasswdAttr{userObject} = OBM::Entities::obmUser->new( 0, 0, $updatePasswdAttr{userId} );
-    if( !defined($updatePasswdAttr{userObject}) ) {
-        &OBM::toolBox::write_log( "[Update::updatePassword]: erreur a l'initialisation de l'utilisateur.", "W" );
-        return undef;
+
+sub _getEntity {
+    my $self = shift;
+
+    # Getting BD user ID
+    my $userId = $self->_getUserIdFromUserLoginDomain( $self->{'userLogin'}, $self->{'domainId'} );
+    if( !defined($userId) ) {
+        $self->_log( 'utilisateur \''.$self->{'userLogin'}.'\', domaine d\'ID '.$self->{'domainId'}.' inconnu', 1 );
+        return 1;
     }
 
-    if( !$updatePasswdAttr{userObject}->getEntity( &OBM::Update::utils::findDomainbyId( $updatePasswdAttr{domainList}, $updatePasswdAttr{domainId} ) ) ) {
-        &OBM::toolBox::write_log( "[Update::updatePassword]: erreur a l'initialisation de l'utilisateur.", "W" );
-        return undef;
+    # Getting user entity
+    require OBM::EntitiesFactory::factoryProgramming;
+    my $programmingObj = OBM::EntitiesFactory::factoryProgramming->new();
+    if( !defined($programmingObj) ) {
+        $self->_log( 'probleme lors de la programmation de la factory d\'entités', 3 );
+        return 1;
+    }
+    $programmingObj->setEntitiesType( 'USER' );
+    $programmingObj->setUpdateType( 'SYSTEM_ENTITY' );
+    $programmingObj->setEntitiesIds( [$userId] );
+
+    require OBM::entitiesFactory;
+    my $entitiesFactory = OBM::entitiesFactory->new( 'PROGRAMMABLE', 1 );
+    if( !defined($entitiesFactory) ) {
+        $self->_log( 'probleme lors de la programmation de la factory d\'entités', 3 );
+        return 1;
+    }
+    if( $entitiesFactory->loadEntities($programmingObj) ) {
+        $self->_log( 'probleme lors de la programmation de la factory d\'entités', 3 );
+        return 1;
     }
 
+    while( my $userEntity = $entitiesFactory->next() ) {
+        $self->{'userEntity'} = $userEntity;
+    }
 
-    bless( \%updatePasswdAttr, $self );
+    if( !defined($self->{'userEntity'}) ) {
+        $self->_log( 'problème lors de la récupération de la description de l\'utilisateur', 0 );
+        return 1;
+    }
+
+    return 0;
 }
 
 
 sub update {
     my $self = shift;
-    my $ldapEngine = $self->{engine}->{ldapEngine};
-    my $userObject = $self->{userObject};
 
-    
+    my $entityDns = $self->{'userEntity'}->getCurrentDnPrefix();
+
     if( $self->{oldPassword} ) {
-        if( !$ldapEngine->checkPasswd( $userObject, $self->{oldPassword} ) ) {
-            return 0;
+        if( $self->_checkPasswd() ) {
+            $self->_log( 'erreur lors de la vérification de l\'ancien mot de passe de '.$self->{'userEntity'}->getDescription(), 0 );
+            return 1;
+        }else {
+            $self->_log( 'ancien mot de passe correct', 0 );
         }
     }
 
-
-    if( !$ldapEngine->updatePassword( $userObject, $self->{newPasswordDesc} ) ) {
-        return 0;
+    if( $self->_updatePassword() ) {
+        $self->_log( 'erreur lors de la mise a jour du mot de passe de '.$self->{'userEntity'}->getDescription(), 0 );
+        return 1;
     }
+#    if( !$ldapEngine->updatePassword( $userObject, $self->{newPasswordDesc} ) ) {
+#        return 0;
+#    }
+#
+#
+#    if( !$userObject->updateDbEntityPassword( $self->{newPasswordDesc} ) ) {
+#        return 0;
+#    }
 
-
-    if( !$userObject->updateDbEntityPassword( $self->{newPasswordDesc} ) ) {
-        return 0;
-    }
-
-    return 1;
+    return 0;
 }
 
 
-sub dump {
+sub _checkPasswd {
     my $self = shift;
-    my @desc;
 
-    push( @desc, $self );
+    my $entityLdapServer = $self->{'ldapservers'}->getLdapServer($self->{'userEntity'}->getLdapServerId());
+    if( ref($entityLdapServer) ne 'OBM::Ldap::ldapServer' ) {
+        $self->_log( 'erreur lors de l\'obtention du serveur LDAP de '.$self->{'userEntity'}->getDescription(), 3 );
+        return 1;
+    }
 
-    require Data::Dumper;
-    print Data::Dumper->Dump( \@desc );
+    my $currentEntityDNs = $self->{'userEntity'}->getCurrentDnPrefix();
+    if( !defined($currentEntityDNs) ) {
+        $self->_log( 'erreur lors de l\'obtention du DN de '.$self->{'userEntity'}->getDescription(), 3 );
+        return 1;
+    }
 
-    return 1;
+    for( my $i=0; $i<=$#{$currentEntityDNs}; $i++ ) {
+        $entityLdapServer->resetConn();
+
+        $entityLdapServer->setDnLogin( $currentEntityDNs->[$i] );
+        $entityLdapServer->setPasswd( $self->{oldPassword} );
+
+        if( !$entityLdapServer->getLdapConn() ) {
+            $entityLdapServer->resetConn();
+            return 1;
+        }
+    }
+
+    $entityLdapServer->resetConn();
+    return 0;
+}
+
+
+sub _updatePassword {
+    my $self = shift;
+
+    if( $self->{'newPasswordDesc'}->{'unix'} ) {
+        $self->_log( 'mise à jour du mot de passe Unix', 4 );
+
+        if( $self->_updateUnixPasswd() ) {
+            $self->_log( 'probleme lors de la mise à jour du mot de passe Unix', 3 );
+            return 1;
+        }else {
+            $self->_log( 'mise à jour du mot de passe Unix', 2 );
+        }
+    }
+#    $self->{'newPasswordDesc'}->{'samba'} = $parameters->{'samba'};
+#    $self->{'newPasswordDesc'}->{'sql'} = $parameters->{'sql'};
+
+
+    return 0;
+}
+
+
+sub _updateUnixPasswd {
+    my $self = shift;
+
+    require OBM::Password::unixPasswdUpdater;
+    my $passwordUpdater = OBM::Password::unixPasswdUpdater->new();
+    if( $passwordUpdater->update( $self->{'userEntity'}, $self->{'newPasswordDesc'}->{'newPassword'} ) ) {
+        $self->_log( 'problème a la mise à jour du mot de passe Unix', 0 );
+        return 1;
+    }
+
+    return 0;
 }
