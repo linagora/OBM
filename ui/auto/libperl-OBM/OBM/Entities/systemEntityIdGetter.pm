a6022e6de555ae459101da125400185643bf466d
Anthony <anthony@linagora.com>
2009-02-12 22:43:19 +0000

    Adding perl Package to get ID BD for an entity

---
+++ ui/auto/libperl-OBM/OBM/Entities/systemEntityIdGetter.pm
@@ -0,0 +1,15 @@
+package OBM::Entities::systemEntityIdGetter;
+
+$VERSION = '1.0';
+
+use OBM::Entities::entityIdGetter;
+@ISA = ('OBM::Entities::entityIdGetter');
+
+$debug = 1;
+
+use 5.006_001;
+use strict;
+
+
+my $userObmTable = 'P_UserObm';
+my $mailShareTable = 'P_MailShare';
