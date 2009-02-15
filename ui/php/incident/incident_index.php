49063419c74e6f52dad30e27a9a39d1b69a1633b
Sylvain Garcia <sgarcia@linagora.com>
2009-02-15 17:54:39 +0000

    add licence to all exetuted files

--- ui/php/incident/incident_index.php
+++ ui/php/incident/incident_index.php
@@ -1,4 +1,23 @@
 <?php
+/*
+ +-------------------------------------------------------------------------+
+ |  Copyright (c) 1997-2009 OBM.org project members team                   |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; version 2                 |
+ | of the License.                                                         |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | http://www.obm.org                                                      |
+ +-------------------------------------------------------------------------+
+*/
+?>
+<?php
 ///////////////////////////////////////////////////////////////////////////////
 // OBM - File : incident_index.php                                           //
 //     - Desc : Incident Index File                                          //
--- ui/scripts/2.0/update-1.2-2.0.php
+++ ui/scripts/2.0/update-1.2-2.0.php
@@ -1,3 +1,22 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ |  Copyright (c) 1997-2009 OBM.org project members team                   |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; version 2                 |
+ | of the License.                                                         |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | http://www.obm.org                                                      |
+ +-------------------------------------------------------------------------+
+*/
+?>
 <script language="php">
 ///////////////////////////////////////////////////////////////////////////////
 // OBM - File : update-1.2-2.0.php                                           //
