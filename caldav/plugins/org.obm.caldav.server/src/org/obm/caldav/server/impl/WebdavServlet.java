d6a24396685659c11983f13b12f89aa74df3d41b
Thomas <Thomas@linagora.com>
2009-04-07 15:40:17 +0000

    add dav servlet

---
+++ caldav/plugins/org.obm.caldav.server/src/org/obm/caldav/server/impl/WebdavServlet.java
@@ -0,0 +1,19 @@
+package org.obm.caldav.server.impl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WebdavServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(req, resp);
+	}
+
+}
