071848e7f4d5726e6a8be2def323b7f4884ce93f
Thomas <Thomas@linagora.com>
2009-04-07 15:44:11 +0000

    serial uid

--- caldav/plugins/org.obm.caldav.server/src/org/obm/caldav/server/impl/WebdavServlet.java
+++ caldav/plugins/org.obm.caldav.server/src/org/obm/caldav/server/impl/WebdavServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class WebdavServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 1410911584964336424L;
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
