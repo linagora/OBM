14877304598860055e06ca248c564d6888ce1a84
Thomas <Thomas@linagora.com>
2009-04-08 10:03:34 +0000

    add missing handlers stubs step 3

---
+++ caldav/plugins/org.obm.caldav.server/src/org/obm/caldav/server/impl/LockHandler.java
@@ -0,0 +1,13 @@
+package org.obm.caldav.server.impl;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LockHandler extends DavMethodHandler {
+
+	@Override
+	public void process(HttpServletRequest req, HttpServletResponse resp) {
+		logger.info("process(req, resp)");
+	}
+
+}
