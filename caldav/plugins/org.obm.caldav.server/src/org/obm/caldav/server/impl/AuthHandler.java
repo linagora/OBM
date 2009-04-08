9937195e1bb4bdbf87b3d760b886020251a260c3
Thomas <Thomas@linagora.com>
2009-04-08 12:20:28 +0000

    add authentification APIs step 2

---
+++ caldav/plugins/org.obm.caldav.server/src/org/obm/caldav/server/impl/AuthHandler.java
@@ -0,0 +1,12 @@
+package org.obm.caldav.server.impl;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AuthHandler {
+
+	public Token doAuth(HttpServletRequest req) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
