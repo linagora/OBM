c53008de566cef8dfe7e57915a8c44272893af7e
Thomas <Thomas@linagora.com>
2009-04-10 14:35:25 +0000

    server -> lightning work step 9

--- caldav/plugins/org.obm.caldav.server/src/org/obm/caldav/server/impl/PropFindHandler.java
+++ caldav/plugins/org.obm.caldav.server/src/org/obm/caldav/server/impl/PropFindHandler.java
@@ -1,13 +1,52 @@
 package org.obm.caldav.server.impl;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletResponse;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import fr.aliasource.utils.DOMUtils;
 
 public class PropFindHandler extends DavMethodHandler {
 
 	@Override
-	public void process(Token t, HttpServletRequest req, HttpServletResponse resp) {
+	public void process(Token t, DavRequest req, HttpServletResponse resp) {
 		logger.info("process(req, resp)");
+
+		String depth = req.getHeader("Depth");
+		logger.info("depth: " + depth);
+
+		Document doc = req.getDocument();
+		Element root = doc.getDocumentElement();
+		Element prop = DOMUtils.getUniqueElement(root, "D:prop");
+		NodeList propsToLoad = prop.getChildNodes();
+		Set<String> toLoad = new HashSet<String>();
+
+		for (int i = 0; i < propsToLoad.getLength(); i++) {
+			Node n = propsToLoad.item(i);
+			if (n.getNodeType() == Node.ELEMENT_NODE) {
+				String name = n.getNodeName();
+				toLoad.add(name);
+				logger.info(name);
+			}
+		}
+
+		Document ret = new PropertyListBuilder().build(t, req, toLoad);
+
+		try {
+			DOMUtils.logDom(ret);
+
+			resp.setStatus(207); // multi status webdav
+			resp.setContentType("text/xml; charset=utf-8");
+			DOMUtils.serialise(ret, resp.getOutputStream());
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
 	}
 
 }
