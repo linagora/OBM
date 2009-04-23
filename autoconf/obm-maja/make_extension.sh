0c57da738dfd0e48cad46be77ed06ab9abb541c0
Christophe <christophe@linagora.com>
2009-04-23 12:41:42 +0000

    make_extension.sh to automatically update version in install.rdf according to VERSION file content

--- autoconf/obm-maja/make_extension.sh
+++ autoconf/obm-maja/make_extension.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 pushd extension
+sed 's/\(<em:version>\).\+\(<\/em:version>\)/\1'$(cat ../VERSION)'\2/' install.rdf > install.rdf.sed
+mv install.rdf.sed install.rdf
 rm -f ../obm-maja_*.xpi
 zip -r ../obm-maja_`cat ../VERSION`.xpi * -x "*~" "*svn*"
 popd
