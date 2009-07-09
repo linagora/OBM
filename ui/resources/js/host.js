64c1652e8e686fcdf6f42ac5d0cc43a651456a1f
Sylvain Garcia <sgarcia@linagora.com>
2009-07-09 09:16:31 +0000

    host monitor: add lang, move code to host_query.inc

--- ui/resources/js/host.js
+++ ui/resources/js/host.js
@@ -21,27 +21,31 @@
 //
 obm.initialize.chain(function () {
     //add event on day,week,month,day
-    $('menuDay').getElements('a').each(function(el){
-      el.addEvent('click', function(){
-        interval = el.getProperty('title');
-        $('stat_img').getElements('img').each(function(el){
-          img = el.getProperty('src');
-          regex = new RegExp("-day.png|-week.png|-month.png|-year.png");
-          img = img.replace(regex,'-'+interval+'.png');
-          el.setProperty('src', img);
+    $try(function(){
+      $('menuDay').getElements('a').each(function(el){
+        el.addEvent('click', function(){
+          interval = el.getProperty('title');
+          $('stat_img').getElements('img').each(function(el){
+            img = el.getProperty('src');
+            regex = new RegExp("-day.png|-week.png|-month.png|-year.png");
+            img = img.replace(regex,'-'+interval+'.png');
+            el.setProperty('src', img);
           });
+        });
       });
     });
     //add event on categories
-    $('stat_menu').getElements('a').each(function(el){
-      el.addEvent('click', function(){
-        category = el.getProperty('title');
-        $('stat_img').getElements('img').each(function(el){
-          img = el.getProperty('src');
-          regex = new RegExp("(.*-)(.*)(-[a-z]*.png)");
-          img = img.replace(regex,'$1'+category+'$3');
-          el.setProperty('src', img);
+    $try(function(){
+      $('stat_menu').getElements('a').each(function(el){
+        el.addEvent('click', function(){
+          category = el.getProperty('title');
+          $('stat_img').getElements('img').each(function(el){
+            img = el.getProperty('src');
+            regex = new RegExp("(.*-)(.*)(-[a-z]*.png)");
+            img = img.replace(regex,'$1'+category+'$3');
+            el.setProperty('src', img);
           });
+        });
       });
     });
 
