e7ab3b73afe4b2023ebf2eca8c82a972f803198e
Sylvain Garcia <sgarcia@linagora.com>
2009-07-09 09:16:22 +0000

    fix css and js, stat are ok

--- ui/resources/js/host.js
+++ ui/resources/js/host.js
@@ -1,19 +1,48 @@
-function modify_stat_interval(interval) {
-    $('stat_img').getElements('img').each(function(el){
-        img = el.getProperty('src');
-        regex = new RegExp("-day.png|-week.png|-month.png|-year.png");
-        img = img.replace(regex,'-'+interval+'.png');
-        el.setProperty('src', img);
-        });
-}
 
-function change_stat_cat(cat) {
-  $('stat_img').getElements('img').each(function(el){
-      img = el.getProperty('src');
-      regex = new RegExp("(.*-)(.*)(-[a-z]*.png)");
-      img = img.replace(regex,'$1'+cat+'$3');
-      el.setProperty('src', img);
-
+//
+//function modify_stat_interval(interval) {
+//  $('stat_img').getElements('img').each(function(el){
+//      img = el.getProperty('src');
+//      regex = new RegExp("-day.png|-week.png|-month.png|-year.png");
+//      img = img.replace(regex,'-'+interval+'.png');
+//      el.setProperty('src', img);
+//      });
+//}
+//
+//function change_stat_cat(cat) {
+//  $('stat_img').getElements('img').each(function(el){
+//      img = el.getProperty('src');
+//      regex = new RegExp("(.*-)(.*)(-[a-z]*.png)");
+//      img = img.replace(regex,'$1'+cat+'$3');
+//      el.setProperty('src', img);
+//
+//      });
+//}
+//
+obm.initialize.chain(function () {
+    //add event on day,week,month,day
+    $('menuDay').getElements('a').each(function(el){
+      el.addEvent('click', function(){
+        interval = el.getProperty('title');
+        $('stat_img').getElements('img').each(function(el){
+          img = el.getProperty('src');
+          regex = new RegExp("-day.png|-week.png|-month.png|-year.png");
+          img = img.replace(regex,'-'+interval+'.png');
+          el.setProperty('src', img);
+          });
       });
-}
+    });
+    //add event on categories
+    $('stat_menu').getElements('a').each(function(el){
+      el.addEvent('click', function(){
+        category = el.getProperty('title');
+        $('stat_img').getElements('img').each(function(el){
+          img = el.getProperty('src');
+          regex = new RegExp("(.*-)(.*)(-[a-z]*.png)");
+          img = img.replace(regex,'$1'+category+'$3');
+          el.setProperty('src', img);
+          });
+      });
+    });
 
+});
