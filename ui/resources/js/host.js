7efbe1a7a2485442bc154418db7017562271045b
Sylvain Garcia <sgarcia@linagora.com>
2009-07-09 09:16:36 +0000

    remove comment code of monitoring

--- ui/resources/js/host.js
+++ ui/resources/js/host.js
@@ -1,24 +1,3 @@
-
-//
-//function modify_stat_interval(interval) {
-//  $('stat_img').getElements('img').each(function(el){
-//      img = el.getProperty('src');
-//      regex = new RegExp("-day.png|-week.png|-month.png|-year.png");
-//      img = img.replace(regex,'-'+interval+'.png');
-//      el.setProperty('src', img);
-//      });
-//}
-//
-//function change_stat_cat(cat) {
-//  $('stat_img').getElements('img').each(function(el){
-//      img = el.getProperty('src');
-//      regex = new RegExp("(.*-)(.*)(-[a-z]*.png)");
-//      img = img.replace(regex,'$1'+cat+'$3');
-//      el.setProperty('src', img);
-//
-//      });
-//}
-//
 obm.initialize.chain(function () {
     //add event on day,week,month,day
     $try(function(){
