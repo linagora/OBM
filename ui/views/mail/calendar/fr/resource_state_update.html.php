488a48eb4ccce4df1587b9572f24321e9cca4bea
Sylvain Garcia <sgarcia@linagora.com>
2009-08-13 12:34:30 +0000

    add copyright

--- ui/views/mail/calendar/fr/resource_state_update.html.php
+++ ui/views/mail/calendar/fr/resource_state_update.html.php
@@ -1,3 +1,22 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ |  Copyright (c) 1997-2009 OBM.org project members team                   |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; version 2                 |
+ | of the License.                                                         |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | http://www.obm.org                                                      |
+ +-------------------------------------------------------------------------+
+*/
+?>
 <table style="width:100%; border:3px solid #000;">
     <tr>
         <th style="text-align:left; background-color: blue; color:#fff; font-size:16px" colspan="2">
--- ui/views/mail/user/fr/validate_confirmation.plain.php
+++ ui/views/mail/user/fr/validate_confirmation.plain.php
@@ -1,3 +1,22 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ |  Copyright (c) 1997-2009 OBM.org project members team                   |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; version 2                 |
+ | of the License.                                                         |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | http://www.obm.org                                                      |
+ +-------------------------------------------------------------------------+
+*/
+?>
 Madame, Monsieur,
 
 Nous avons le plaisir de vous informer que l'utilisateur <?php echo $user_label ?> a été validé par <?php echo $admin_label ?>.
