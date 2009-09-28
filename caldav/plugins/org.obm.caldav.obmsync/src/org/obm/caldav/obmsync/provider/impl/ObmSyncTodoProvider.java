6113a54849046e52ae106139e076eb64f44a42fe
Thomas <Thomas@linagora.com>
2009-09-28 09:49:23 +0000

    Revert "rm unused plugin"
    This reverts commit fe617355c25dfd5799a08619615e9684c8154d7d.

---
+++ caldav/plugins/org.obm.caldav.obmsync/src/org/obm/caldav/obmsync/provider/impl/ObmSyncTodoProvider.java
@@ -0,0 +1,77 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: GPL 2.0
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License Version 2 or later (the "GPL").
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Initial Developer of the Original Code is
+ *   MiniG.org project members
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.obm.caldav.obmsync.provider.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.obm.caldav.obmsync.provider.ICalendarProvider;
+import org.obm.sync.auth.AccessToken;
+import org.obm.sync.auth.AuthFault;
+import org.obm.sync.auth.ServerFault;
+import org.obm.sync.calendar.Event;
+import org.obm.sync.calendar.EventTimeUpdate;
+import org.obm.sync.calendar.EventType;
+import org.obm.sync.client.calendar.AbstractEventSyncClient;
+import org.obm.sync.client.calendar.TodoClient;
+import org.obm.sync.items.EventChanges;
+
+public class ObmSyncTodoProvider extends AbstractObmSyncProvider  {
+
+	protected static final Log logger = LogFactory.getLog(ObmSyncTodoProvider.class);
+	
+	private static ICalendarProvider instance;
+	
+	public static ICalendarProvider getInstance(){
+		if(instance == null){
+			instance = new ObmSyncTodoProvider();
+		}
+		return instance;
+	}
+	
+	protected ObmSyncTodoProvider() {
+		super(); 
+	}
+	
+	@Override
+	protected AbstractEventSyncClient getObmSyncClient(String url) {
+		return new TodoClient(url);
+	}
+	
+	@Override
+	public EventChanges getSync(AccessToken token, String userId, Date lastSync)
+	throws AuthFault, ServerFault {
+		logger.info("Get sync["+lastSync+"] from obm-sync");
+		return client.getSync(token, userId, lastSync);
+	}
+
+	@Override
+	public List<Event> getAll(AccessToken token, String calendar)
+			throws ServerFault, AuthFault {
+		logger.info("Get all Event from obm-sync");
+		return super.getAll(token, calendar, EventType.VTODO);
+	}
+
+	@Override
+	public List<EventTimeUpdate> getAllEventTimeUpdate(AccessToken token,
+			String calendar) throws ServerFault, AuthFault {
+		logger.info("Get all EventTimeUpdate from obm-sync");
+		return super.getAllEventTimeUpdate(token, calendar, EventType.VTODO);
+	}
+}
