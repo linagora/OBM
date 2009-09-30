ef8741117d5cc2aafaf6622383c011a21cd50013
Thomas <Thomas@linagora.com>
2009-09-30 09:28:49 +0000

    allow running pg script without modifiying debian's pg_hba.conf

--- ui/scripts/2.3/install_obmdb_pgsql_2.3.sh
+++ ui/scripts/2.3/install_obmdb_pgsql_2.3.sh
@@ -9,7 +9,9 @@ db=$1
 user=$2
 pw=$3
 obm_lang=$4
+host=localhost
 
+export PGPASSWORD=$pw
 
 echo "  Delete old database"
 dropdb ${db}
@@ -35,21 +37,21 @@ ALTER DATABASE ${db} SET TIMEZONE='GMT';
 \q
 EOF
 
-psql -U ${user} ${db} -f \
+psql -U ${user} -h ${host} ${db} -f \
 create_obmdb_2.3.pgsql.sql > /tmp/data_insert.log 2>&1
 grep -i error /tmp/data_insert.log && {
     echo "error in pg script"
     exit 1
 }
 
-psql -U ${user} ${db} -f \
+psql -U ${user} -h ${host} ${db} -f \
 obmdb_default_values_2.3.sql >> /tmp/data_insert.log 2>&1
 grep -i error /tmp/data_insert.log && {
     echo "error in pg script"
     exit 1
 }
 
-psql -U ${user} ${db} -f \
+psql -U ${user} -h ${host} ${db} -f \
 obmdb_triggers_2.3.pgsql.sql >> /tmp/data_insert.log 2>&1
 grep -i error /tmp/data_insert.log && {
     echo "error in pg script"
@@ -57,7 +59,7 @@ grep -i error /tmp/data_insert.log && {
 }
 
 echo "  Dictionnary data insertion"
-psql -U ${user} ${db} -f \
+psql -U ${user} -h ${host} ${db} -f \
 data-${obm_lang}/obmdb_ref_2.3.sql >> /tmp/data_insert.log 2>&1
 grep -i error /tmp/data_insert.log && {
     echo "error in pg script"
@@ -65,7 +67,7 @@ grep -i error /tmp/data_insert.log && {
 }
 
 echo "  Company Naf Code data insertion"
-psql -U ${user} ${db} -f \
+psql -U ${user} -h ${host} ${db} -f \
 data-${obm_lang}/obmdb_nafcode_2.3.sql >> /tmp/data_insert.log 2>&1
 grep -i error /tmp/data_insert.log && {
     echo "error in pg script"
@@ -73,14 +75,14 @@ grep -i error /tmp/data_insert.log && {
 }
 
 echo "  Default preferences data insertion"
-psql -U ${user} ${db} -f \
+psql -U ${user} -h ${host} ${db} -f \
 obmdb_prefs_values_2.3.sql >> /tmp/data_insert.log 2>&1
 grep -i error /tmp/data_insert.log && {
     echo "error in pg script"
     exit 1
 }
 
-psql -U ${user} -q ${db} <<EOF
+psql -U ${user} -h ${host} -q ${db} <<EOF
 UPDATE UserObmPref SET userobmpref_value='${obm_lang}' WHERE userobmpref_option='set_lang'
 \q
 EOF
