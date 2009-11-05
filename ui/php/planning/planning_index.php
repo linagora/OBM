c225c9aff1bb49d73a7d72fed9b267b36b4bba49
guillaumel <guillaumel@localhost>
2009-11-05 10:35:15 +0000

    Changes in the planning module : - added tasktypegroup management interface - the group listbox is now in the configuration - some code cleaning

--- ui/php/planning/planning_index.php
+++ ui/php/planning/planning_index.php
@@ -4,16 +4,21 @@
 //     - Desc : Planning Index File                                          //
 // 2008-10-14 Guillaume Lardon                                               //
 ///////////////////////////////////////////////////////////////////////////////
-// $Id: planning_index.php 16 2009-09-24 07:15:46Z glardon $
+// $Id$
 ///////////////////////////////////////////////////////////////////////////////
 // Actions :
 // - index (default) --                -- show the planning for a month
-// - import          --                -- import a planning from a spreadsheet
 // - detailconsult   -- plannedtask_id -- show the planned task detail
 // - detailupdate    -- plannedtask_id -- show the planned task detail form
 // - insert          -- form fields    -- insert the planned task
 // - update          -- form fields    -- update the planned task
 // - delete          -- plannedtask    -- delete the planned task
+// - admin           --                -- admin index (type)
+// - tasktypegroup_insert -- form fields    -- insert the tasktype group
+// - tasktypegroup_update -- form fields    -- update the tasktype group
+// - tasktypegroup_checklink -- $sel_ttg    -- check if ttg is used
+// - tasktypegroup_delete -- $sel_ttg    -- delete the ttg
+// - tasktype_update -- form fields    -- update the tasktype
 ///////////////////////////////////////////////////////////////////////////////
 
 $path = '..';
@@ -27,6 +32,8 @@ update_planning_session_params();
 include("$obminclude/global_pref.inc");
 include('planning_display.inc');
 include('planning_query.inc');
+#require('obminclude/javascript/check_js.inc');
+include("$obminclude/of/of_category.inc");
 
 if ($action == '') $action = 'index';
 get_planning_action();
@@ -39,29 +46,10 @@ page_close();
 ///////////////////////////////////////////////////////////////////////////////
  if (($action == 'index') || ($action == '')) {
   $display['detail'] .= dis_planning_view($params);
-  //if ($perm->check_right('planning', $cright_read_admin)) 
   $display['features'] .= dis_group_select($params, run_query_planning_groups(), 1);
-}
-else if ($action == 'import_fileselect') {
-///////////////////////////////////////////////////////////////////////////////
-  $display['title'] = $l_import;
-  $display['detail'] = html_planning_import_fileselect_form($params);
-} else if ($action == 'import_tabselect') {
-///////////////////////////////////////////////////////////////////////////////
-  $tabs = run_query_planning_extract_ods($params['file']);
-  $display['title'] = $l_import;
-  $display['detail'] = html_planning_import_tabselect_form($params,$tabs);
-} else if ($action == 'import') {
-///////////////////////////////////////////////////////////////////////////////
-  $result = run_query_import_planning($params);
-  $display['detail'] = html_planning_import_report($result);
 } else if ($action == 'new') {
 ///////////////////////////////////////////////////////////////////////////////
   $display['detail'] = html_plannedtask_form($action, '', $params);
-
-} else if ($action == 'newform') {
-///////////////////////////////////////////////////////////////////////////////
-  $display['detail'] = html_planning_newmonth_form($action, '', $params);
 
 } else if ($action == 'detailconsult') {
 ///////////////////////////////////////////////////////////////////////////////
@@ -117,14 +105,6 @@ else if ($action == 'import_fileselect') {
     $display['detail'] = html_plannedtask_form($action, $obm_q, $params, $err['field'],$conflict_id);
   }
 
-} else if ($action == 'detailuserupdate') {
-///////////////////////////////////////////////////////////////////////////////
-  if (check_planning_update_rights($params)) {
-    $obm_q = run_query_plannedtask_user_detail($params['user_id'],$params['month']);
-    $display['detail'] = html_plannedtask_user_form($action, $obm_q, $params);
-  } else {
-    $display['msg'] .= display_warn_msg($err['msg']);
-  }
 }
 elseif ($action == 'delete') {
 ///////////////////////////////////////////////////////////////////////////////
@@ -138,7 +118,54 @@ elseif ($action == 'delete') {
   $display['detail'] = dis_planning_view($params);
   $display['features'] .= dis_group_select($params, run_query_planning_groups(), 1);
 
-} 
+}
+elseif ($action == 'admin') {
+///////////////////////////////////////////////////////////////////////////////
+  $display['detail'] = dis_planning_admin_index();
+}
+elseif ($action == 'tasktypegroup_insert') {
+///////////////////////////////////////////////////////////////////////////////
+  $retour = run_query_tasktypegroup_insert($params);
+  if ($retour) {
+    $display['msg'] .= display_ok_msg("$l_tasktypegroup : $l_insert_ok");
+  } else {
+    $display['msg'] .= display_err_msg("$l_tasktypegroup : $l_insert_error");
+  }
+  $display['detail'] = dis_planning_admin_index();
+
+} elseif ($action == 'tasktypegroup_update') {
+///////////////////////////////////////////////////////////////////////////////
+  $retour = run_query_tasktypegroup_update($params);
+  if ($retour) {
+    $display['msg'] .= display_ok_msg("$l_tasktypegroup : $l_update_ok");
+  } else {
+    $display['msg'] .= display_err_msg("$l_tasktypegroup : $l_update_error");
+  }
+  $display['detail'] = dis_planning_admin_index();
+
+} elseif ($action == 'tasktypegroup_checklink') {
+///////////////////////////////////////////////////////////////////////////////
+  $display['detail'] = dis_tasktypegroup_links($params['tasktypegroup']);
+
+} elseif ($action == 'tasktypegroup_delete') {
+///////////////////////////////////////////////////////////////////////////////
+  $retour = run_query_tasktypegroup_delete($params['tasktypegroup_id']);
+  if ($retour) {
+    $display['msg'] .= display_ok_msg("$l_tasktypegroup : $l_delete_ok");
+  } else {
+    $display['msg'] .= display_err_msg("$l_tasktypegroup : $l_delete_error");
+  }
+  $display['detail'] = dis_planning_admin_index();
+} elseif ($action == 'tasktype_update') {
+///////////////////////////////////////////////////////////////////////////////
+  $retour = run_query_tasktype_update($params);
+  if ($retour) {
+    $display['msg'] .= display_ok_msg("$l_header_tasktype : $l_update_ok");
+  } else {
+    $display['msg'] .= display_err_msg("$l_header_tasktype : $l_update_error");
+  }
+  $display['detail'] = dis_planning_admin_index();
+}
 else
 pt_assert(0,__FILE__,__LINE__,"unknown action: $action");
 
@@ -154,7 +181,6 @@ if (! $params['popup']) {
 
 display_page($display);
 
-
 ///////////////////////////////////////////////////////////////////////////////
 // Stores in $params hash, Planning parameters transmited
 // returns : $params hash with parameters set
@@ -167,25 +193,30 @@ function get_planning_params() {
   return $params;
 }
 
-
 ///////////////////////////////////////////////////////////////////////////////
 // Update session and parameters
 ///////////////////////////////////////////////////////////////////////////////
 function update_planning_session_params() {
   global $params, $obm;
+  global $cplanning_groups;
   
-  // We retrieve the selected groups if any, else we get them from session
+  // We retrieve the selected groups if any
   if (isset ($params['group_id']))
-  {
-    $_SESSION['group_id'] = $params['group_id'];
-  }
+  // and set it in the user preferences (last)
+  update_user_pref($obm['uid'],'last_planning',implode(',',$params['group_id']));
   else
   {
-    if(isset($_SESSION['group_id']))
-    $params['group_id'] = $_SESSION['group_id'];
+    //else we get them from preferences or default
+    $group_ids = get_one_user_pref($obm['uid'],'last_planning');
+    if(isset($group_ids))
+    $params['group_id'] = explode(',',$group_ids[$obm['uid']]['value']);
     else
     $params['group_id'] = get_planning_default_displayed_groups();
   }
+
+  // define the group list if unset in the configuration
+  if(!is_array($cplanning_groups))
+  $cplanning_groups = array($obm['group_prod']);
 }
 
 function get_planning_default_displayed_groups()
@@ -197,45 +228,14 @@ function get_planning_default_displayed_groups()
   // Otherwise, use the production system group defined for the domain
   else
   return array($obm['group_prod']);
-  
 };
-
-///////////////////////////////////////////////////////////////////////////////
-// Clean up a string coming from a textarea : 
-//  + remove empty lines
-//  + remove space at the beginning of the lines
-//  + remove space at the end of the lines
-//  + remove first empty line and last \n of the string
-// Parameters:
-//   - $str String to be cleaned up
-// Returns:
-//   - The string cleaned up  
-///////////////////////////////////////////////////////////////////////////////
-function cleanup($str) {
-  $result = '';
-  $lines = explode('\r\n', $str);
-  $i = 0;
-  while ($i < count($lines)) {
-    $currentline = trim($lines[$i]);
-    if ($currentline != '') {
-      $result .= $currentline.'\r\n';
-    }
-    $i++;
-  }
-
-  // Suppression du dernier retour chariot
-  // (plus pratique pour les traitements type explode/tokenizer, on obtient pas de derniere chaine vide)
-  $result = preg_replace('/\r\n$/', '', $result);
-  return $result;
-}
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // Planning Action 
 ///////////////////////////////////////////////////////////////////////////////
 function get_planning_action() {
   global $params, $actions, $path;
-  global $l_planning, $l_reporting, $l_header_import, $l_header_consult;
+  global $l_planning, $l_reporting, $l_header_consult,$l_header_admin;
   global $cright_read, $cright_write, $cright_read_admin, $cright_write_admin;
 
 // Index
@@ -245,27 +245,12 @@ function get_planning_action() {
     'Right'    => $cright_read,
     'Condition'=> array ('all') 
                                     );
-// Import
-  $actions['planning']['import_fileselect'] = array (
-    'Name'     => $l_header_import,
-    'Url'      => "$path/planning/planning_index.php?action=import_fileselect",
-    'Right'    => $cright_write_admin,
-    'Condition'=> array ('all') );
 
   $actions['planning']['reporting'] = array (
     'Name'     => $l_reporting,
     'Url'      => "$path/planning/planning_index.php?reportingmode=1",
     'Right'    => $cright_write,
     'Condition'=> array ('all') );
-
-  $actions['planning']['import_tabselect'] = array (
-    'Right'    => $cright_read,
-    'Condition'=> array ('None')
-                                  );
-  $actions['planning']['import'] = array (
-    'Right'    => $cright_read,
-    'Condition'=> array ('None')
-                                  );
 
 // New
   $actions['planning']['new'] = array (
@@ -290,32 +275,11 @@ function get_planning_action() {
     'Condition'=> array ('None')
                                      );
 
-// Detail Update
-  $actions['planning']['detailuserupdate'] = array (
-     'Right'    => $cright_write,
-     'Condition'=> array ('None')
-                              	   );
-
 // Delete
   $actions['planning']['delete'] = array (
     'Right'    => $cright_write,
     'Condition'=> array ('None') 
                                      );
-/*
-// Search
-  $actions['planning']['search'] = array (
-    'Right'    => $cright_read,
-    'Condition'=> array ('None')
-                                  );
-// New month
-// TODO : ecrire la fonction
-  $actions['planning']['new'] = array (
-    'Name'     => $l_header_new,
-    'Url'      => "$path/planning/planning_index.php?action=newform",
-    'Right'    => $cright_write_admin,
-    'Condition'=> array ('search','index','admin', 'showlist','detailconsult','display', 'insert', 'update')
-                                  );
-*/
 // Detail Consult
   $actions['planning']['detailconsult'] = array (
     'Name'     => $l_header_consult,
@@ -323,6 +287,49 @@ function get_planning_action() {
     'Right'    => $cright_read,
     'Condition'=> array ('None') 
                                   );
+  // Admin  
+  $actions['planning']['admin'] = array (
+    'Name'     => $l_header_admin,
+    'Url'      => "$path/planning/planning_index.php?action=admin",
+    'Right'    => $cright_read_admin,
+    'Condition'=> array ('all') 
+                                    );
+
+  // TaskType Group Insert
+  $actions['planning']['tasktypegroup_insert'] = array (
+    'Url'      => "$path/planning/planning_index.php?action=tasktypegroup_insert",
+    'Right'    => $cright_write_admin,
+    'Condition'=> array ('None') 
+                                           );
+
+  // TaskType Group Update
+  $actions['planning']['tasktypegroup_update'] = array (
+    'Url'      => "$path/planning/planning_index.php?action=tasktypegroup_update",
+    'Right'    => $cright_write_admin,
+    'Condition'=> array ('None') 
+                                           );
+
+  // Task Type Group Checklink 
+  $actions['planning']['tasktypegroup_checklink'] = array (
+    'Url'      => "$path/planning/planning_index.php?action=tasktypegroup_checklink",
+    'Right'    => $cright_write_admin,
+    'Condition'=> array ('None') 
+                                          );
+
+  // Task Type Group Delete 
+  $actions['planning']['tasktypegroup_delete'] = array (
+    'Url'      => "$path/planning/planning_index.php?action=tasktypegroup_delete",
+    'Right'    => $cright_write_admin,
+    'Condition'=> array ('None') 
+                                          );
+
+  // TaskType Update
+  $actions['planning']['tasktype_update'] = array (
+    'Url'      => "$path/planning/planning_index.php?action=tasktype_update",
+    'Right'    => $cright_write_admin,
+    'Condition'=> array ('None') 
+                                           );
+
 }
 
 </script>
