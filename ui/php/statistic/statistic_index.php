80a8954a3ff83b89eef8efd667e18522966c22ba
pierre <pierre@localhost>
2009-11-12 18:23:54 +0000

    #606

--- ui/php/statistic/statistic_index.php
+++ ui/php/statistic/statistic_index.php
@@ -26,21 +26,21 @@
 // $Id$ //
 ///////////////////////////////////////////////////////////////////////////////
 // Actions :
-// - index (default)                -- search fields  -- show the statistic search form
-// - contact_date_evolution_graph   --                -- show contact date evolution stats 
+// - index (default)              -- search fields -- the statistic search form
+// - contact_date_evolution_graph --               -- contact date evolution 
 ///////////////////////////////////////////////////////////////////////////////
 
-$path = "..";
-$module = "statistic";
-$obminclude = getenv("OBM_INCLUDE_VAR");
-if ($obminclude == "") $obminclude = "obminclude";
+$path = '..';
+$module = 'statistic';
+$obminclude = getenv('OBM_INCLUDE_VAR');
+if ($obminclude == '') $obminclude = 'obminclude';
 include("$obminclude/global.inc");
 $params = get_statistic_params();
-page_open(array("sess" => "OBM_Session", "auth" => $auth_class_name, "perm" => "OBM_Perm"));
+page_open(array('sess' => 'OBM_Session', 'auth' => $auth_class_name, 'perm' => 'OBM_Perm'));
 include("$obminclude/global_pref.inc");
-require("statistic_display.inc");
-require("statistic_query.inc");
-require("statistic_js.inc");
+require('statistic_display.inc');
+require('statistic_query.inc');
+require('statistic_js.inc');
 include("$obminclude/of/of_category.inc");
  
 get_statistic_action();
@@ -48,63 +48,63 @@ $perm->check_permissions($module, $action);
 
 page_close();
 
-if (! $params["popup"]) {
-  $display["header"] = display_menu($module);
+if (! $params['popup']) {
+  $display['header'] = display_menu($module);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // Main Program                                                              //
 ///////////////////////////////////////////////////////////////////////////////
-if ($action == "index" || $action == "") {
+if ($action == 'index' || $action == '') {
 ///////////////////////////////////////////////////////////////////////////////
-  $display["detail"] = dis_statistic_index(); 
+  $display['detail'] = dis_statistic_index(); 
 
-} elseif ($action == "manager") {
+} elseif ($action == 'manager') {
 ///////////////////////////////////////////////////////////////////////////////
   $cont_q = run_query_statistic_contact_manager();
   $comp_q = run_query_statistic_company_manager();
-  $display["title"] = display_title($l_stats_manager);
-  $display["detail"] = dis_statistic_resp_stats($cont_q,$comp_q);
+  $display['title'] = display_title($l_stats_manager);
+  $display['detail'] = dis_statistic_resp_stats($cont_q,$comp_q);
 
-} elseif ($action == "stats_category") {
+} elseif ($action == 'stats_category') {
 ///////////////////////////////////////////////////////////////////////////////
   require("$path/list/list_query.inc");
-  $category = $params["category"];
-  $entity = $params["entity"];
-  if ($params["list_id"] == $c_all) {
+  $category = $params['category'];
+  $entity = $params['entity'];
+  if ($params['list_id'] == $c_all) {
     $cat_q = run_query_statistic_entity_per_country_per_cat($entity, $category);
     $nb_ent = get_entity_nb($entity);
   } else {
-    $obm_q = run_query_statistic_get_list($params["list_id"]);
+    $obm_q = run_query_statistic_get_list($params['list_id']);
     $ext_list_function = "ext_list_get_${entity}_ids";
-    $ent_q = $ext_list_function($params["list_id"]);
+    $ent_q = $ext_list_function($params['list_id']);
     $cat_q = run_query_statistic_selected_entity_per_country_per_cat($ent_q, $entity, $category);
     $nb_ent = $ent_q->nf();
-    $title = " : " . $obm_q->f("list_name");
+    $title = ' : ' . $obm_q->f('list_name');
   }
-  $display["detail"] = dis_statistic_cat_stats($cat_q, $nb_ent);
+  $display['detail'] = dis_statistic_cat_stats($cat_q, $nb_ent);
   $l_entity = ${"l_$entity"};
   $l_category = ${"l_$category"};
-  $display["title"] = display_title("$l_stats : $l_entity / $l_category$title");
+  $display['title'] = display_title("$l_stats : $l_entity / $l_category$title");
 
-} elseif ($action == "stats_category_export") {
+} elseif ($action == 'stats_category_export') {
 ///////////////////////////////////////////////////////////////////////////////
   require("$path/list/list_query.inc");
-  $category = $params["category"];
-  $entity = $params["entity"];
-  if ($params["list_id"] == $c_all) {
+  $category = $params['category'];
+  $entity = $params['entity'];
+  if ($params['list_id'] == $c_all) {
     $cat_q = run_query_statistic_entity_per_country_per_cat($entity, $category);
     $nb_ent = get_entity_nb($entity);
   } else {
-    $obm_q = run_query_statistic_get_list($params["list_id"]);
+    $obm_q = run_query_statistic_get_list($params['list_id']);
     $ext_list_function = "ext_list_get_${entity}_ids";
-    $ent_q = @$ext_list_function($params["list_id"]);
+    $ent_q = @$ext_list_function($params['list_id']);
     $cat_q = run_query_statistic_selected_entity_per_country_per_cat($ent_q, $entity, $category);
     $nb_ent = $ent_q->nf();
   }
   export_statistic_cat_stats($cat_q, $nb_ent);
 
-} elseif ($action == "contact_date_evolution_graph") {
+} elseif ($action == 'contact_date_evolution_graph') {
 ///////////////////////////////////////////////////////////////////////////////
   $stats = get_statistic_contact_date_total();
 //  $tot = get_stats_contact_date_categories($stats);
@@ -117,8 +117,8 @@ if ($action == "index" || $action == "") {
 ///////////////////////////////////////////////////////////////////////////////
 // Display
 ///////////////////////////////////////////////////////////////////////////////
-$display["head"] = display_head($l_statistic);
-$display["end"] = display_end();
+$display['head'] = display_head($l_statistic);
+$display['end'] = display_end();
 
 display_page($display);
 
@@ -145,25 +145,25 @@ function get_statistic_action() {
   global $l_header_contact_date_evolution_stats;
 
 // Index
-  $actions["statistic"]["index"] = array (
+  $actions['statistic']['index'] = array (
     'Name'     => $l_header_index,
     'Url'      => "$path/statistic/statistic_index.php?action=index",
     'Right'    => $cright_read,
     'Condition'=> array ('all') 
                                         );
 // Index
-  $actions["statistic"]["manager"] = array (
+  $actions['statistic']['manager'] = array (
     'Right'    => $cright_read,
     'Condition'=> array ('content') 
                                         );
 					
 // Stats entity by category 
-  $actions["statistic"]["stats_category"] = array (
+  $actions['statistic']['stats_category'] = array (
     'Right'    => $cright_read,
     'Condition'=> array ('None') 
                                         );
 					
-  $actions["statistic"]["stats_category_export"] = array (
+  $actions['statistic']['stats_category_export'] = array (
     'Name'     => $l_header_export,
     'Url'      => "$path/statistic/statistic_index.php?action=stats_category_export&amp;category=$params[category]&amp;popup=1&amp;list_id=$params[list_id]&amp;entity=$params[entity]",
     'Right'    => $cright_read,
@@ -172,14 +172,14 @@ function get_statistic_action() {
     'Condition'=> array ('stats_category')
                                         );					
 					
-  $actions["statistic"]["contact_date_evolution"] = array (
+  $actions['statistic']['contact_date_evolution'] = array (
     'Name'     => $l_header_contact_date_evolution_stats,
     'Url'      => "$path/statistic/statistic_index.php?action=contact_date_evolution",
     'Right'    => $cright_read,
     'Condition'=> array ('content')
                                         );
 
-  $actions["statistic"]["contact_date_evolution_graph"] = array (
+  $actions['statistic']['contact_date_evolution_graph'] = array (
     'Url'      => "$path/statistic/statistic_index.php?action=contact_date_evolution_graph",
     'Right'    => $cright_read,
     'Condition'=> array ('None')
