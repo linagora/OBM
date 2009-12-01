7c67d350690b922fc81df3158751a57069a492c1
david <david@localhost>
2009-12-01 15:22:20 +0000

    Host:imap_frontend

--- ui/php/domain/domain_index.php
+++ ui/php/domain/domain_index.php
@@ -78,7 +78,7 @@ if ($action == 'index' || $action == '') {
 } elseif ($action == 'new') {
 ///////////////////////////////////////////////////////////////////////////////
   $prop_q = run_query_domain_properties();
-  $display['detail'] = html_domain_form('','','', null, null, $prop_q,$params);
+  $display['detail'] = html_domain_form('','','', null, null ,null, $prop_q,$params);
 
 } elseif ($action == 'detailconsult') {
 ///////////////////////////////////////////////////////////////////////////////
@@ -91,10 +91,11 @@ if ($action == 'index' || $action == '') {
   $ms_q = of_domain_get_domain_mailserver('', $params['domain_id']);
   $sync_q = of_domain_get_domain_syncserver($params['domain_id']);
   $solr_q = of_domain_get_domain_solrserver($params['domain_id']);
+  $imapfrontend_q = of_domain_get_domain_imapfrontendserver($params['domain_id']);
   $samba = run_query_domain_samba_properties($params['domain_id']);
   if ($obm_q->num_rows() == 1) {
     $display['detailInfo'] = display_record_info($obm_q);
-    $display['detail'] = html_domain_form($obm_q, $ms_q, $sync_q, $solr_q, $samba, $prop_q, $params);
+    $display['detail'] = html_domain_form($obm_q, $ms_q, $sync_q, $solr_q, $imapfrontend_q, $samba, $prop_q, $params);
   } else {
     $display['msg'] .= display_err_msg($l_query_error . ' - ' . $query . ' !');
   }
@@ -111,13 +112,13 @@ if ($action == 'index' || $action == '') {
     } else {
       $display['msg'] .= display_err_msg($l_insert_error);
       $prop_q = run_query_domain_properties();
-      $display['detail'] = html_domain_form('','','', null, null, $prop_q,$params);
+      $display['detail'] = html_domain_form('','','', null, null, null, $prop_q,$params);
     }
     // Form data are not valid
   } else {
     $display['msg'] .= display_warn_msg($l_invalid_data . ' : ' . $err['msg']);
     $prop_q = run_query_domain_properties();
-    $display['detail'] = html_domain_form('','','', null, null, $prop_q,$params);
+    $display['detail'] = html_domain_form('','','', null, null, null, $prop_q,$params);
   }
 
 } elseif ($action == 'update') {
@@ -134,7 +135,7 @@ if ($action == 'index' || $action == '') {
   } else {
     $display['msg'] .= display_err_msg($err['msg']);
     $prop_q = run_query_domain_properties($params['domain_id']);
-    $display['detail'] = html_domain_form('','','', null, null,  $prop_q, $params);
+    $display['detail'] = html_domain_form('','','', null, null, null, $prop_q, $params);
   }
 
 } elseif ($action == 'check_delete') {
