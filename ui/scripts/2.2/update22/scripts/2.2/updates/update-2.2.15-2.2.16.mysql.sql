a41a01164e525a83f390952f32a25a1d51ffa47e
pierre <pierre@localhost>
2009-12-06 14:45:15 +0000

    Fix inconsistency in ProjectUser table

--- ui/scripts/2.2/update22/scripts/2.2/updates/update-2.2.15-2.2.16.mysql.sql
+++ ui/scripts/2.2/update22/scripts/2.2/updates/update-2.2.15-2.2.16.mysql.sql
@@ -1,14 +1,18 @@
 -- /////////////////////////////////////////////////////////////////////////////
--- OBM - File : update-2.2.14-2.2.15.mysql.sql
--- 2009-10-14 Mehdi Rande
+-- OBM - File : update-2.2.15-2.2.16.mysql.sql
+-- 2009-12-06 Pierre Baudracco
 -- /////////////////////////////////////////////////////////////////////////////
 -- $Id: $
 -- /////////////////////////////////////////////////////////////////////////////
 
+
 UPDATE ObmInfo SET obminfo_value = '2.2.16-pre' WHERE obminfo_name = 'db_version';
---UPDATE ObmInfo SET obminfo_value = '2.2.16' WHERE obminfo_name = 'db_version';
 
 ALTER TABLE UGroup DROP KEY group_gid;
 ALTER TABLE UGroup ADD UNIQUE KEY group_gid (group_domain_id, group_gid);
 ALTER TABLE P_UGroup DROP KEY group_gid;
 ALTER TABLE P_UGroup ADD UNIQUE KEY group_gid (group_domain_id, group_gid);
+
+UPDATE ProjectUser SET projectuser_projectedtime=NULL WHERE projectuser_projecttask_id IS NULL AND projectuser_projected_time=0;
+
+UPDATE ObmInfo SET obminfo_value = '2.2.16' WHERE obminfo_name = 'db_version';
--- ui/scripts/2.2/update22/scripts/2.2/updates/update-2.2.15-2.2.16.pgsql.sql
+++ ui/scripts/2.2/update22/scripts/2.2/updates/update-2.2.15-2.2.16.pgsql.sql
@@ -1,9 +1,13 @@
 -- /////////////////////////////////////////////////////////////////////////////
--- OBM - File : update-2.2.14-2.2.15.mysql.sql
--- 2009-10-14 Mehdi Rande
+-- OBM - File : update-2.2.15-2.2.16.pgsql.sql
+-- 2009-12-06 Pierre Baudracco
 -- /////////////////////////////////////////////////////////////////////////////
 -- $Id: $
 -- /////////////////////////////////////////////////////////////////////////////
 
+
 UPDATE ObmInfo SET obminfo_value = '2.2.16-pre' WHERE obminfo_name = 'db_version';
---UPDATE ObmInfo SET obminfo_value = '2.2.16' WHERE obminfo_name = 'db_version';
+
+UPDATE ProjectUser SET projectuser_projectedtime=NULL WHERE projectuser_projecttask_id IS NULL AND projectuser_projected_time=0;
+
+UPDATE ObmInfo SET obminfo_value = '2.2.16' WHERE obminfo_name = 'db_version';
