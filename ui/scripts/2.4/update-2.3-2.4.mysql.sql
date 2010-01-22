7684e2bcf0bb36b7c4e4e99b18fb4da969e2b972
vincenta <vincenta@localhost>
2010-01-22 14:42:06 +0000

    initialization of the sql scripts to upgrade from 2.3 to 2.4

---
+++ ui/scripts/2.4/update-2.3-2.4.mysql.sql
@@ -0,0 +1,11 @@
+-- Write that the 2.3->2.4 has started
+UPDATE ObmInfo SET obminfo_value='2.3.x->2.4.0' WHERE obminfo_name='db_version';
+-- -----------------------------------------------------------------------------
+
+
+-- FIXME: put upgrades here
+
+
+-- ----------------------------------------------------------------------------
+-- Write that the 2.3->2.4 is completed
+UPDATE ObmInfo SET obminfo_value='2.4.0' WHERE obminfo_name='db_version';
