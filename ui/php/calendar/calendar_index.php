8f4b716eee0f29fb9ff3778b7c41f307d65dc5d2
david <david@localhost>
2010-02-02 11:24:50 +0000

    Calendar: freebusy enhancements
    #390 - End date listbox disappear after specific use of free-busy (ie6 only)
    #399 - IE6: freebusy is too slow

--- ui/php/calendar/calendar_index.php
+++ ui/php/calendar/calendar_index.php
@@ -219,6 +219,7 @@ if ($action == 'search') {
 ///////////////////////////////////////////////////////////////////////////////
   $extra_js_include[] = 'inplaceeditor.js';
   $extra_js_include[] = 'mootools/plugins/mooRainbow.1.2b2.js' ;
+  $extra_js_include[] = 'freebusy.js';
   $extra_css[] = $css_ext_color_picker ;
   if (is_array($params['sel_user_id']) || is_array($params['sel_resource_id'])) {
     $entities = array(
@@ -259,6 +260,7 @@ if ($action == 'search') {
         }
         $extra_js_include[] = 'inplaceeditor.js';
         $extra_js_include[] = 'mootools/plugins/mooRainbow.1.2b2.js' ;
+        $extra_js_include[] = 'freebusy.js';
         $extra_css[] = $css_ext_color_picker ;
         $display['detail'] .= html_calendar_dis_conflict($params,$conflicts) ;
         $display['msg'] .= display_err_msg("$l_event : $l_insert_error");
@@ -300,6 +302,7 @@ if ($action == 'search') {
     $display['msg'] .= display_warn_msg($l_invalid_data . ' : ' . $err['msg']);
     $extra_js_include[] = 'inplaceeditor.js';
     $extra_js_include[] = 'mootools/plugins/mooRainbow.1.2b2.js' ;
+    $extra_js_include[] = 'freebusy.js';
     $extra_css[] = $css_ext_color_picker ;
     $display['detail'] = dis_calendar_event_form($action, $params, '', $entities, $current_view);
   }
@@ -317,6 +320,7 @@ if ($action == 'search') {
   if ($params['calendar_id'] > 0) {  
     $extra_js_include[] = 'inplaceeditor.js';
     $extra_js_include[] = 'mootools/plugins/mooRainbow.1.2b2.js' ;
+    $extra_js_include[] = 'freebusy.js';
     $extra_css[] = $css_ext_color_picker ;
     if (check_calendar_access($params['calendar_id'], 'read')) {
       $eve_q = run_query_calendar_detail($params['calendar_id']);
@@ -335,6 +339,7 @@ if ($action == 'search') {
   if ($params['calendar_id'] > 0) {  
     $extra_js_include[] = 'inplaceeditor.js';
     $extra_js_include[] = 'mootools/plugins/mooRainbow.1.2b2.js' ;
+    $extra_js_include[] = 'freebusy.js';
     $extra_css[] = $css_ext_color_picker ;
     $eve_q = run_query_calendar_detail($params['calendar_id']);
     $entities = get_calendar_event_entity($params['calendar_id']);
@@ -373,6 +378,7 @@ if ($action == 'search') {
         }
         $extra_js_include[] = 'inplaceeditor.js';
         $extra_js_include[] = 'mootools/plugins/mooRainbow.1.2b2.js' ;
+        $extra_js_include[] = 'freebusy.js';
         $extra_css[] = $css_ext_color_picker ;
         $display['detail'] = html_calendar_dis_conflict($params,$conflicts) ;
         $display['msg'] .= display_err_msg("$l_event : $l_update_error");
@@ -409,6 +415,7 @@ if ($action == 'search') {
       $display['msg'] .= display_warn_msg($l_invalid_data . ' : ' . $err['msg']);
       $extra_js_include[] = 'inplaceeditor.js';
       $extra_js_include[] = 'mootools/plugins/mooRainbow.1.2b2.js' ;
+      $extra_js_include[] = 'freebusy.js';
       $extra_css[] = $css_ext_color_picker ;
       $display['detail'] = dis_calendar_event_form($action, $params, '', $entities, $current_view);
     }
@@ -686,6 +693,7 @@ if ($action == 'search') {
 
 } elseif ($action == 'new_meeting')  {
 ///////////////////////////////////////////////////////////////////////////////
+  $extra_js_include[] = 'freebusy.js';
   $display['detail'] = dis_calendar_meeting_form($current_view, $params);
 
 } elseif ($action == 'admin')  {
@@ -768,6 +776,7 @@ if ($action == 'search') {
   }
   $extra_js_include[] = 'inplaceeditor.js';
   $extra_js_include[] = 'mootools/plugins/mooRainbow.1.2b2.js' ;
+  $extra_js_include[] = 'freebusy.js';
   $extra_css[] = $css_ext_color_picker ;
   $display['detail'] = dis_calendar_event_form($action, $params, '', $entities, $current_view);
   
@@ -952,6 +961,7 @@ if ($action == 'search') {
       $conflicts = check_calendar_conflict($params, $conflicts_entities);
       $extra_js_include[] = 'inplaceeditor.js';
       $extra_js_include[] = 'mootools/plugins/mooRainbow.1.2b2.js' ;
+      $extra_js_include[] = 'freebusy.js';      
       $extra_css[] = $css_ext_color_picker ;
       $display['detail'] = html_calendar_dis_conflict($params, $conflicts) ;
       $display['detail'] .= dis_calendar_event_form($action, $params, $eve_q, $entities, $current_view);
@@ -1056,8 +1066,7 @@ if (!$params['ajax']) {
 
 } elseif ($action == 'perform_meeting')  {
 ///////////////////////////////////////////////////////////////////////////////
-  dis_calendar_free_interval($current_view);
-  echo "({".$display['json']."})";
+  echo dis_calendar_free_interval($current_view, $params);
   exit();
 
 } elseif ($action == 'set_entity_class')  {
