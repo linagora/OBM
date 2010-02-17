72120e1c0fb28b6c1f0e66674d025d2cf843a786
Thomas <Thomas@linagora.com>
2010-02-17 14:17:19 +0000

    fix blank lines in BB quoted printables

--- funambol/src/java/fr/aliasource/funambol/utils/FunisHelper.java
+++ funambol/src/java/fr/aliasource/funambol/utils/FunisHelper.java
@@ -18,6 +18,7 @@ public class FunisHelper {
 
 	public static String removeQuotedPrintableFromVCalString(String vcal) {
 		String cleaned = vcal.replace("\r\n", "\n");
+		cleaned = vcal.replace("\n\n", "\n");
 		String[] lines = safeSplit(cleaned, '\n');
 		StringBuffer noQuoted = new StringBuffer(lines.length * 76);
 		boolean quotedMode = false;
