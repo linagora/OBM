9e7cf044fc0b393ff2dd8dade6b0840d9f18fea1
Madhi <medhi@linagora.com>
2010-03-01 09:09:16 +0000

    Fix #1007

--- ui/scripts/2.3/updates/update-2.3.2-2.3.3.pgsql.sql
+++ ui/scripts/2.3/updates/update-2.3.2-2.3.3.pgsql.sql
@@ -1,5 +1,6 @@
 UPDATE ObmInfo SET obminfo_value = '2.3.3-pre' WHERE obminfo_name = 'db_version';
 
 ALTER TABLE Account ALTER COLUMN account_number TYPE varchar(64);
+ALTER TABLE UGroup ALTER COLUMN group_ext_id TYPE varchar(255);
 
 UPDATE ObmInfo SET obminfo_value = '2.3.3' WHERE obminfo_name = 'db_version';
