5c08182590c4fa5a45e8109b57b41fd26d575543
Madhi <medhi@linagora.com>
2010-03-01 09:05:52 +0000

    Fix : # 1123

--- ui/scripts/2.3/updates/update-2.3.2-2.3.3.mysql.sql
+++ ui/scripts/2.3/updates/update-2.3.2-2.3.3.mysql.sql
@@ -1,5 +1,5 @@
-UPDATE ObmInfo SET obminfo_value = '2.3.2-pre' WHERE obminfo_name = 'db_version';
+UPDATE ObmInfo SET obminfo_value = '2.3.3-pre' WHERE obminfo_name = 'db_version';
 
 ALTER TABLE Account CHANGE COLUMN account_number account_number varchar(64) default NULL;
 
-UPDATE ObmInfo SET obminfo_value = '2.3.2' WHERE obminfo_name = 'db_version';
+UPDATE ObmInfo SET obminfo_value = '2.3.3' WHERE obminfo_name = 'db_version';
