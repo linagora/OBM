6c33d89c3b50e32963e55be8ba11ab929ed6f31d
pierre <pierre@localhost>
2010-03-19 11:40:45 +0000

    #1089

--- ui/locale/fr.php
+++ ui/locale/fr.php
@@ -28,7 +28,7 @@ return array(
   'Event cancelled on OBM: %title%' => 'Evénement annulé sur OBM : %title%',
   'Event updated on OBM: %title%' => 'Mise à jour d\'un événement sur OBM : %title%',
   'New resource reservation on OBM: %title%' => 'Nouvelle réservation de ressource sur OBM : %title%',
-  'Resource reservation updated on OBM' => 'Mise à jour d\'une réservation de ressource sur OBM : %title%',
+  'Resource reservation updated on OBM: %title%' => 'Mise à jour d\'une réservation de ressource sur OBM : %title%',
   'New resource reservation on OBM: %title%' => 'Nouvelle réservation de ressource sur OBM : %title%',
   'Exploitation report : %name%' => 'Rapport d\'exploitation : %name%',
   '%displayname% validation confirmation' => 'Confirmation de la validation de l\'utilisateur %displayname%',
