f3aa700fd39d4fb2184c3db71cb5c7c48ea00963
Thomas <Thomas@linagora.com>
2010-04-14 07:53:44 +0000

    add dummy sections 'module' implementation to ease testing of minig integration

---
+++ ui/php/sections/sections_index.php
@@ -0,0 +1,11 @@
+<?php
+
+if ($_GET["action"] == 'gw_links') {
+   echo "<a href=\"https://obm23.buffy.kvm/calendar/calendar_index.php\">Caltos</a>\n";
+   echo "<a href=\"https://obm23.buffy.kvm/calendar/calendar_index.php\">Caltos</a>\n";
+   echo "<a href=\"https://obm23.buffy.kvm/calendar/calendar_index.php\">Caltos</a>\n";
+} else if ($_GET["action"] == 'sections_html') {
+  echo "<table><tr><td>Pouic</td></tr></td></table>";
+}
+
+?>
