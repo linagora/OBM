8cd2211c9af6e0d834fbdf0409ea7573751bd786
david <david@localhost>
2010-05-04 10:08:05 +0000

    fix calendar resource mail html

--- ui/views/mail/calendar/fr/resource_state_update.html.php
+++ ui/views/mail/calendar/fr/resource_state_update.html.php
@@ -17,9 +17,9 @@
  +-------------------------------------------------------------------------+
 */
 ?>
-<table style="width:100%; border:3px solid #000;">
+<table style="width:80%; border:1px solid #000; border-collapse:collapse;background:#EFF0F2;font-size:12px;">
     <tr>
-        <th style="text-align:left; background-color: blue; color:#fff; font-size:16px" colspan="2">
+        <th style="text-align:center; background-color: #509CBC; color:#FFF; font-size:14px" colspan="2">
           Participation : mise à jour
         </th>
     </tr>
