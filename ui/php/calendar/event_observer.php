e5dedd0930151ea6b331cfeb00f224692ce7df5d
david <david@localhost>
2010-05-05 07:37:05 +0000

    fix calendar mail resource update

--- ui/php/calendar/event_observer.php
+++ ui/php/calendar/event_observer.php
@@ -819,7 +819,7 @@ class OBM_EventMailObserver /*implements  OBM_Observer*/{
     foreach ($recipients as $resource) {
       $resourceOwners = array_keys(OBM_Acl::getEntityWriters('resource', $resource->id));
       if (!in_array($GLOBALS['obm']['uid'], $resourceOwners) && count($resourceOwners) > 0) {
-        $this->mailer->sendResourceCancel($old, $resourceOwners, $resource);
+        $this->mailer->sendResourceCancel($old, $resourceOwners);
       }
     }       
   }
