e2baab0bf623cafbde280c982d986f631b25f842
blandined <blandined@localhost>
2010-11-22 09:11:49 +0000

    petite correction liée au bug M#2304

--- ui/php/calendar/calendar_index.php
+++ ui/php/calendar/calendar_index.php
@@ -1521,7 +1521,7 @@ function get_calendar_action() {
     'Name'     => $l_header_consult,
     'Url'      => "$path/calendar/calendar_index.php?action=detailconsultExt&amp;calendar_id=$id",
     'Right'    => $cright_read,
-    'Condition'=> array ('detailconsult') 
+    'Condition'=> array ('detailconsultExt') 
   );
 
   // Detail Update
