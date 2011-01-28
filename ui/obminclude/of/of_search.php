8d8a70fcd4206ae2846780974d0fcbd9195e4aeb
blandined <blandined@localhost>
2011-01-28 10:32:41 +0000

    fix M#2680 : Contact - Bug on solr queries with dash

--- ui/obminclude/of/of_search.php
+++ ui/obminclude/of/of_search.php
@@ -36,6 +36,8 @@ class  OBM_Search {
             $pattern = "domain:$obm[domain_id]";
           }
           // $pattern = strtolower($pattern); => do not work because of solr keyword "OR" and "AND"
+          // replace compound word in pattern
+          $pattern = preg_replace("/(?<=[a-zA-Z])-(?=[a-zA-Z])/"," + ", $pattern);
           $pattern = preg_replace("/(\w*)\*/e", "strtolower('$1').'*'", $pattern);
           $response = $solr->search($pattern, $offset, $limit, $options);
           display_debug_solr($pattern, $cdg_solr, "OBM_Search::search($core)");
@@ -64,6 +66,7 @@ class  OBM_Search {
         } else {
           $pattern = "domain:$obm[domain_id]";
         }
+        $pattern = preg_replace("/(?<=[a-zA-Z])-(?=[a-zA-Z])/"," + ", $pattern);
         $pattern = preg_replace("/(\w*)\*/e", "strtolower('$1').'*'", $pattern);
         $response = $solr->search($pattern);
         return $response->response->numFound;
