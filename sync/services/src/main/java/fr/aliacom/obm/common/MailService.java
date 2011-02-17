2ae0ba2e311a3d3972ee983359d33428dbf93e97
Adrien Poupard <apoupard@linagora.com>
2011-02-17 16:05:47 +0000

    catch exception

--- sync/services/src/main/java/fr/aliacom/obm/common/MailService.java
+++ sync/services/src/main/java/fr/aliacom/obm/common/MailService.java
@@ -1,19 +1,23 @@
 package fr.aliacom.obm.common;
 
+import java.util.Iterator;
 import java.util.List;
 
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.obm.sync.ObmSmtpProvider;
+import org.obm.sync.server.mailer.AbstractMailer.NotificationException;
 
 import com.google.inject.Inject;
 
 public class MailService {
 
+	private static final Log logger = LogFactory.getLog(MailService.class);
 	private final ObmSmtpProvider provider;
 
 	@Inject
@@ -21,9 +25,35 @@ public class MailService {
 		this.provider = provider;
 	}
 	
-	public void sendMessage(Session session, List<InternetAddress> to, MimeMessage message) throws MessagingException {
-		message.setRecipients(RecipientType.TO, to.toArray(new InternetAddress[0]));
-		provider.sendEmail(session, message);
+	public void sendMessage(Session session, List<InternetAddress> to, MimeMessage message) {
+		
+		try{
+			message.setRecipients(RecipientType.TO, to.toArray(new InternetAddress[0]));
+			provider.sendEmail(session, message);
+		} catch (Throwable e) {
+			logger.error(getErrorLog(to, message), e);
+		}
+	}
+	
+	private String getErrorLog(List<InternetAddress> to, MimeMessage message){
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("Error while sending mail[subject:");
+			sb.append(message.getSubject());
+			sb.append("; from:");
+			sb.append(message.getFrom() != null && message.getFrom().length> 0 ? ""+message.getFrom()[0] : "null");
+			sb.append("; to:");
+			for(Iterator<InternetAddress> it = to.iterator(); it.hasNext();){
+				sb.append(it.next().toString());
+				if(it.hasNext()){
+					sb.append(",");
+				}
+			}
+			return sb.toString();
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+		
 	}
 	
 }
