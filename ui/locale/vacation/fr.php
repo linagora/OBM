09690041eeb970b9205c784f6664e83f99c54c5a
Vincent Bourdeix <vbourdeix@linagora.com>
2011-03-24 13:39:26 +0000

    Modification of the message for vacations - see Mantis 2892

---
+++ ui/locale/vacation/fr.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+  "The vacation message will be on starting the %date_start% and until the %date_end%" => "Le message d'absence sera actif du %date_start% au %date_end%",
+  "Vacation programmation" => "Programmation"
+);
