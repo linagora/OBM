1cc61d2fb030e7b882a56c3b9c9c4fee767b4228
Raphael Rougeron <rrougeron@linagora.com>
2011-04-14 14:14:18 +0000

    Fixed #2182

--- ui/php/contact/addressbook.php
+++ ui/php/contact/addressbook.php
@@ -49,7 +49,6 @@ class OBM_AddressBook implements OBM_ISearchable {
     $this->owner = $owner;
     $this->syncable = TRUE;
     if ($this->name == 'public_contacts' && $this->isDefault) $this->syncable = FALSE;
-    if ($this->name == 'contacts' && $this->isDefault && $this->owner == $GLOBALS['obm']['uid']) $this->syncable = FALSE;
     $this->synced = $synced;
     $this->db = new DB_OBM;
     $this->setQueryFilter();
