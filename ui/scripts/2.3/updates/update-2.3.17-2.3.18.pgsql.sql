c0188cca87a0ba02c48026865c606a2be7a2c838
Vincent Bourdeix <vbourdeix@linagora.com>
2011-04-18 10:07:16 +0000

    Merge modifications from 2.3 : rename scripts psql.sql into pgsql.sql

--- ui/scripts/2.3/updates/update-2.3.17-2.3.18.pgsql.sql
+++ ui/scripts/2.3/updates/update-2.3.17-2.3.18.pgsql.sql
@@ -5,7 +5,7 @@ UPDATE Deal SET deal_hitrate=0 WHERE deal_hitrate IS NULL;
 ALTER TABLE Deal ALTER COLUMN deal_hitrate SET NOT NULL;
 
 -- Modification the lastname to allow NULL values (necessary when importing ics)
-ALTER TABLE `Contact` CHANGE COLUMN contact_lastname `contact_lastname` character varying(64) DEFAULT NULL;
+ALTER TABLE contact ALTER COLUMN contact_lastname DROP NOT NULL;
 
 ALTER TABLE opush_sync_mail ADD COLUMN is_read boolean DEFAULT false;
 
