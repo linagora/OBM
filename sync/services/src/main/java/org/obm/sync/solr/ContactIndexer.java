75b8330a1b467ba945402aa05ed23d998e84fc5a
Vincent Bourdeix <vbourdeix@linagora.com>
2011-05-04 07:11:11 +0000

    Modification of a variable name from  to e (bad habit taken from php)

--- sync/services/src/main/java/org/obm/sync/solr/ContactIndexer.java
+++ sync/services/src/main/java/org/obm/sync/solr/ContactIndexer.java
@@ -237,7 +237,7 @@ public class ContactIndexer implements Runnable {
 					return true;
 				}
 			}
-			catch (NoSuchElementException $e){
+			catch (NoSuchElementException e){
 				return false;
 			}
 		}
