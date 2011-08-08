438fb87f5a45881d58b03a67d915a22a94152dd8
Ronan Lanore <rlanore@linagora.com>
2011-08-08 14:27:19 +0000

    Revert "modified build script for use with rpm and deb"
    This reverts commit 636de32bf201cb73203948c2a42ff1ca0375d196.

--- sync/opush/packaging/opush-build.sh
+++ sync/opush/packaging/opush-build.sh
@@ -3,17 +3,10 @@
 set -e 
 
 CURDIR=$1
-PKG_TYPE=$2
 maven_home=${HOME}
 BUILD_DEB_DIR="${CURDIR}/debian"
-BUILD_RPM_DIR="${CURDIR}/rpm"
 OBM_SYNC_BUILD_DEB_DIR="${BUILD_DEB_DIR}/opush"
-OBM_SYNC_BUILD_RPM_DIR="${BUILD_RPM_DIR}/opush"
-if [ "x${PKG_TYPE}" == "xdeb" ]; then
-	INSTALL_DIR="${OBM_SYNC_BUILD_DEB_DIR}/var/lib/jetty/webapps/"
-elif [ "x${PKG_TYPE}" == "xrpm" ]; then
-	INSTALL_DIR="${OBM_SYNC_BUILD_RPM_DIR}/var/lib/jetty/webapps/"
-fi
+INSTALL_DIR="${OBM_SYNC_BUILD_DEB_DIR}/var/lib/jetty/webapps/"
 MVN_BIN="/usr/bin/mvn -Dmaven.test.skip -Duser.home=${maven_home}"
 POM_FILE="pom.xml"
 CHANGELOG_FILE="$BUILD_DEB_DIR/changelog"
