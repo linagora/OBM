e294a8cbd5b6b77fa97c0bc06ed04110268d32fc
Ronan Lanore <rlanore@linagora.com>
2011-08-08 10:22:18 +0000

    modified build script for use with rpm and deb

--- sync/opush/packaging/opush-build.sh
+++ sync/opush/packaging/opush-build.sh
@@ -3,10 +3,17 @@
 set -e 
 
 CURDIR=$1
+PKG_TYPE=$2
 maven_home=${HOME}
 BUILD_DEB_DIR="${CURDIR}/debian"
+BUILD_RPM_DIR="${CURDIR}/rpm"
 OBM_SYNC_BUILD_DEB_DIR="${BUILD_DEB_DIR}/opush"
-INSTALL_DIR="${OBM_SYNC_BUILD_DEB_DIR}/var/lib/jetty/webapps/"
+OBM_SYNC_BUILD_RPM_DIR="${BUILD_RPM_DIR}/opush"
+if [ "x${PKG_TYPE}" == "xdeb" ]; then
+	INSTALL_DIR="${OBM_SYNC_BUILD_DEB_DIR}/var/lib/jetty/webapps/"
+elif [ "x${PKG_TYPE}" == "xrpm" ]; then
+	INSTALL_DIR="${OBM_SYNC_BUILD_RPM_DIR}/var/lib/jetty/webapps/"
+fi
 MVN_BIN="/usr/bin/mvn -Dmaven.test.skip -Duser.home=${maven_home}"
 POM_FILE="pom.xml"
 CHANGELOG_FILE="$BUILD_DEB_DIR/changelog"
