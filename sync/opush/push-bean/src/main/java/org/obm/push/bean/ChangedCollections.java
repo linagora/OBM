fabcc178d43dd9121b39eb1c906cb56e3d5d9f43
Matthieu Baechler <mbaechler@linagora.com>
2011-08-22 15:50:45 +0000

    - fix ChangedCollections.toString method

--- sync/opush/push-bean/src/main/java/org/obm/push/bean/ChangedCollections.java
+++ sync/opush/push-bean/src/main/java/org/obm/push/bean/ChangedCollections.java
@@ -3,9 +3,7 @@ package org.obm.push.bean;
 import java.util.Date;
 import java.util.Set;
 
-
 import com.google.common.base.Objects;
-import com.google.common.base.Objects.ToStringHelper;
 
 public class ChangedCollections {
 
@@ -28,13 +26,10 @@ public class ChangedCollections {
 	
 	@Override
 	public String toString() {
-		ToStringHelper h = Objects.toStringHelper(getClass());
-		h.addValue(h);
-		for(SyncCollection change : changed){
-			h.addValue(change);
-		}
-		return h.toString();
-
+		return Objects
+				.toStringHelper(getClass())
+				.addValue(changed)
+				.toString();
 	}
 	
 }
