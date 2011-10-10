c4a2513d338244a978c7e78081a37293ef5f42e0
Emmanuel Surleau <esurleau@linagora.com>
2011-10-10 15:01:29 +0200

    Reverted to Tomcat packaging for obm-sync

--- java/sync/debian/obm-sync-build.sh
+++ java/sync/debian/obm-sync-build.sh
@@ -7,7 +7,7 @@ CURDIR=$1
 maven_home=${HOME}
 BUILD_DEB_DIR="${CURDIR}/debian"
 OBM_SYNC_BUILD_DEB_DIR="${BUILD_DEB_DIR}/obm-sync"
-INSTALL_DIR="${OBM_SYNC_BUILD_DEB_DIR}/var/lib/jetty/webapps/"
+INSTALL_DIR="${OBM_SYNC_BUILD_DEB_DIR}/usr/share/obm-sync"
 MVN_BIN="/usr/bin/mvn -Duser.home=${maven_home}"
 POM_FILE="pom.xml"
 CHANGELOG_FILE="$BUILD_DEB_DIR/changelog"
@@ -27,11 +27,11 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# copie du web-inf et du meta-inf
+# copie du web-inf
 WEB_INF=`find ${PROJECT_NAME}/target -name WEB-INF `
-META_INF=`find ${PROJECT_NAME}/target -name META-INF `
-mkdir ${INSTALL_DIR}obm-sync
-cp -r ${WEB_INF} ${INSTALL_DIR}obm-sync/
-cp -r ${META_INF} ${INSTALL_DIR}obm-sync/
-
-exit 0 
+cp -r ${WEB_INF} ${INSTALL_DIR}
+rm -f ${INSTALL_DIR}/WEB-INF/lib/postgresql-9.0-801.jdbc4.jar
+rm -f ${INSTALL_DIR}/WEB-INF/lib/slf4j-api-*.jar
+rm -f ${INSTALL_DIR}/WEB-INF/lib/logback*.jar
+rm -f ${INSTALL_DIR}/WEB-INF/lib/jta-1.1.jar
+exit 0
