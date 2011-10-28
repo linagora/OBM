72effb56e04136c82a9c1540c220e6c83e0980a1
Michael Bailly <mbailly@linagora.com>
2011-10-28 11:31:26 +0200

    OBMFULL-2732 Update license

--- ui/php/calendar/event_observer.php
+++ ui/php/calendar/event_observer.php
@@ -1,32 +1,43 @@
 <?php
-/*
- +-------------------------------------------------------------------------+
- |  Copyright (c) 1997-2009 OBM.org project members team                   |
- |                                                                         |
- | This program is free software; you can redistribute it and/or           |
- | modify it under the terms of the GNU General Public License             |
- | as published by the Free Software Foundation; version 2                 |
- | of the License.                                                         |
- |                                                                         |
- | This program is distributed in the hope that it will be useful,         |
- | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
- | GNU General Public License for more details.                            |
- +-------------------------------------------------------------------------+
- | http://www.obm.org                                                      |
- +-------------------------------------------------------------------------+
-*/
-?>
-<?php
+/******************************************************************************
+Copyright (C) 2011-2012 Linagora
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version, provided you comply with the Additional Terms applicable for OBM
+software by Linagora pursuant to Section 7 of the GNU Affero General Public
+License, subsections (b), (c), and (e), pursuant to which you must notably (i)
+retain the displaying by the interactive user interfaces of the “OBM, Free
+Communication by Linagora” Logo with the “You are using the Open Source and
+free version of OBM developed and supported by Linagora. Contribute to OBM R&D
+by subscribing to an Enterprise offer !” infobox, (ii) retain all hypertext
+links between OBM and obm.org, between Linagora and linagora.com, as well as
+between the expression “Enterprise offer” and pro.obm.org, and (iii) refrain
+from infringing Linagora intellectual property rights over its trademarks and
+commercial brands. Other Additional Terms apply, see
+<http://www.linagora.com/licenses/> for more details.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License and
+its applicable Additional Terms for OBM along with this program. If not, see
+<http://www.gnu.org/licenses/> for the GNU Affero General   Public License
+version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+applicable to the OBM software.
+******************************************************************************/
+
+
 
 /**
  *  
  * 
  * @package 
  * @version $Id:$
- * @copyright Copyright (c) 1997-2009 Aliasource - Groupe LINAGORA
+ * @copyright Copyright (c) 1997-2009 Groupe LINAGORA
  * @author Mehdi Rande <mehdi.rande@aliasource.fr> 
- * @license GPL 2.0
  */
 
 $obminclude = getenv('OBM_INCLUDE_VAR');
@@ -299,9 +310,8 @@ class OBM_Event /*Implements OBM_PropertyChangeSupport*/{
  * 
  * @package 
  * @version $Id:$
- * @copyright Copyright (c) 1997-2009 Aliasource - Groupe LINAGORA
+ * @copyright Copyright (c) 1997-2009 Groupe LINAGORA
  * @author Mehdi Rande <mehdi.rande@aliasource.fr> 
- * @license GPL 2.0
  */
 class OBM_EventAttendee {
 
@@ -413,9 +423,8 @@ class OBM_EventAttendee {
  * 
  * @package 
  * @version $Id:$
- * @copyright Copyright (c) 1997-2009 Aliasource - Groupe LINAGORA
+ * @copyright Copyright (c) 1997-2009 Groupe LINAGORA
  * @author Mehdi Rande <mehdi.rande@aliasource.fr> 
- * @license GPL 2.0
  */
 class OBM_EventFactory extends OBM_ASubject {
 
@@ -579,9 +588,8 @@ class OBM_EventFactory extends OBM_ASubject {
  * 
  * @package 
  * @version $Id:$
- * @copyright Copyright (c) 1997-2009 Aliasource - Groupe LINAGORA
+ * @copyright Copyright (c) 1997-2009 Groupe LINAGORA
  * @author Mehdi Rande <mehdi.rande@aliasource.fr> 
- * @license GPL 2.0
  */
 class OBM_EventMailObserver implements  OBM_IObserver {
 
--- ui/php/obm.php
+++ ui/php/obm.php
@@ -1,33 +1,35 @@
 <?php
-/*
- +-------------------------------------------------------------------------+
- | Copyright © LINAGORA 2010                                               |
- |                                                                         |
- | This file is part of OBM. OBM includes and uses many components working |
- | together as a complete end-to-end solution: Groupware, messaging        |
- | server, CRM,  LDAP, project, smartphones and PDA synchronization        |
- |                                                                         |
- | OBM is free software; you can redistribute it and/or                    |
- | modify it under the terms of the GNU Affero General Public License      |
- | as published by the Free Software Foundation; version 3 of the License. |
- |                                                                         |
- | There are special exceptions to the terms and conditions of the         |
- | licenses as they are applied to this software. View the full text of    | 
- | the exception in file LICENSE.txt in the directory of this software     |
- | distribution.                                                           |
- |                                                                         |
- | OBM is distributed in the hope that it will be useful,                  |
- | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
- | License.txt for more details.                                           |
- |                                                                         |
- | You should have received a copy of the GNU Affero General Public        |
- |  License  with this file. If not, see :                                 |
- | <http://www.fsf.org/licensing/licenses/agpl-3.0.html>                   |
- +-------------------------------------------------------------------------+
- | http://www.obm.org                                                      |
- +-------------------------------------------------------------------------+
-*/
+/******************************************************************************
+Copyright (C) 2011-2012 Linagora
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version, provided you comply with the Additional Terms applicable for OBM
+software by Linagora pursuant to Section 7 of the GNU Affero General Public
+License, subsections (b), (c), and (e), pursuant to which you must notably (i)
+retain the displaying by the interactive user interfaces of the “OBM, Free
+Communication by Linagora” Logo with the “You are using the Open Source and
+free version of OBM developed and supported by Linagora. Contribute to OBM R&D
+by subscribing to an Enterprise offer !” infobox, (ii) retain all hypertext
+links between OBM and obm.org, between Linagora and linagora.com, as well as
+between the expression “Enterprise offer” and pro.obm.org, and (iii) refrain
+from infringing Linagora intellectual property rights over its trademarks and
+commercial brands. Other Additional Terms apply, see
+<http://www.linagora.com/licenses/> for more details.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License and
+its applicable Additional Terms for OBM along with this program. If not, see
+<http://www.gnu.org/licenses/> for the GNU Affero General   Public License
+version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+applicable to the OBM software.
+******************************************************************************/
+
+
 ?>
 <script language="php">
 ///////////////////////////////////////////////////////////////////////////////
