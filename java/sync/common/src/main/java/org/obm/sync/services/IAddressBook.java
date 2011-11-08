aa2ba234aa85c27f983f442384419e2dff0dae2b
Joakim Ribier <jribier@linagora.com>
2011-11-08 11:54:19 +0100

    - [OBMFULL-2724] replace opush getContentChanges to listContactsChanged with addressBookId ws

--- java/sync/common/src/main/java/org/obm/sync/services/IAddressBook.java
+++ java/sync/common/src/main/java/org/obm/sync/services/IAddressBook.java
@@ -164,5 +164,7 @@ public interface IAddressBook {
 	 * @throws ServerFault 
 	 */
 	FolderChanges listAddressBooksChanged(AccessToken token, Date timestamp) throws ServerFault;
+
+	ContactChanges listContactsChanged(AccessToken token, Date lastSync, Integer addressBookId) throws ServerFault;
 	
 }
