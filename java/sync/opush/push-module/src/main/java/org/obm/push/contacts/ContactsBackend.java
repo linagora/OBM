3fff9d61e9c9bbe1e9e6400749f0ee6cb26eef44
Joakim Ribier <jribier@linagora.com>
2011-11-09 15:36:03 +0100

    - [OBMFULL-2724] remove BookType parameter of modifyContact and delete other modifyContact* ws

--- java/sync/opush/push-module/src/main/java/org/obm/push/contacts/ContactsBackend.java
+++ java/sync/opush/push-module/src/main/java/org/obm/push/contacts/ContactsBackend.java
@@ -54,7 +54,6 @@ import org.obm.push.store.CollectionDao;
 import org.obm.sync.auth.AccessToken;
 import org.obm.sync.auth.ServerFault;
 import org.obm.sync.book.AddressBook;
-import org.obm.sync.book.BookType;
 import org.obm.sync.book.Contact;
 import org.obm.sync.book.Folder;
 import org.obm.sync.client.CalendarType;
@@ -257,7 +256,7 @@ public class ContactsBackend extends ObmSyncBackend {
 				itemId = serverId.substring(idx + 1);
 				Contact convertedContact = new ContactConverter().contact(data);
 				convertedContact.setUid(Integer.parseInt(itemId));
-				bc.modifyContact(token, BookType.contacts, convertedContact);
+				bc.modifyContact(token, addressBookId, convertedContact);
 			} else {
 				Contact createdContact = bc.createContact(token, addressBookId,
 						new ContactConverter().contact(data));
@@ -270,6 +269,8 @@ public class ContactsBackend extends ObmSyncBackend {
 		} catch (ContactAlreadyExistException e) {
 			throw new UnknownObmSyncServerException(e);
 		} catch (DaoException e) {
+			throw new UnknownObmSyncServerException(e);
+		} catch (ContactNotFoundException e) {
 			throw new UnknownObmSyncServerException(e);
 		} finally {
 			logout(token);
