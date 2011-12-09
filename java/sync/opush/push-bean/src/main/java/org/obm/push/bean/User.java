862df9231a729c2305bcd80817d92091776dd10d
Matthieu Baechler <matthieu.baechler@linagora.com>
2011-12-09 16:02:34 +0100

    - [OBMFULL-2869] use a better exception in case of invalid input

--- java/sync/opush/push-bean/src/main/java/org/obm/push/bean/User.java
+++ java/sync/opush/push-bean/src/main/java/org/obm/push/bean/User.java
@@ -32,7 +32,6 @@
 package org.obm.push.bean;
 
 import java.io.Serializable;
-import java.security.InvalidParameterException;
 import java.util.Iterator;
 
 import com.google.common.base.Objects;
@@ -73,7 +72,7 @@ public class User implements Serializable {
 				loginAndDomain = buildUserFromLoginParts(parts);
 			}
 			if (loginAndDomain == null) {
-				throw new InvalidParameterException();
+				throw new IllegalArgumentException();
 			}
 			return loginAndDomain;
 		}
@@ -91,7 +90,7 @@ public class User implements Serializable {
 		private String[] buildUserFromLoginParts(Iterable<String> parts) {
 			int nbParts = Iterables.size(parts);
 			if (nbParts > 2) {
-				throw new InvalidParameterException();
+				throw new IllegalArgumentException();
 			} else if (nbParts == 2) {
 				Iterator<String> iterator = parts.iterator();
 				String domain = iterator.next();
@@ -105,7 +104,7 @@ public class User implements Serializable {
 		
 		private void checkField(String key, String field) {
 			if (field.contains("@") || field.contains("\\")) {
-				throw new InvalidParameterException(key + " is invalid : " + field);
+				throw new IllegalArgumentException(key + " is invalid : " + field);
 			}
 		}
 	}
--- java/sync/opush/push-module/src/test/java/org/obm/push/UserTest.java
+++ java/sync/opush/push-module/src/test/java/org/obm/push/UserTest.java
@@ -31,8 +31,6 @@
  * ***** END LICENSE BLOCK ***** */
 package org.obm.push;
 
-import java.security.InvalidParameterException;
-
 import org.fest.assertions.Assertions;
 import org.junit.Test;
 import org.obm.push.bean.User;
@@ -48,14 +46,14 @@ public class UserTest {
 		Assertions.assertThat(result).isEqualTo(text);
 	}
 
-	@Test(expected=InvalidParameterException.class)
+	@Test(expected=IllegalArgumentException.class)
 	public void testGetLoginAtDomainWithTwoArrobases() {
 		String text = "login@domain@error";
 		User loginAtDomain = Factory.create().createUser(text, "email@domain");
 		loginAtDomain.getLoginAtDomain();
 	}
 	
-	@Test(expected=InvalidParameterException.class)
+	@Test(expected=IllegalArgumentException.class)
 	public void testGetLoginAtDomainWithNoDomain() {
 		String text = "login";
 		User loginAtDomain = Factory.create().createUser(text, "email@domain");
@@ -70,14 +68,14 @@ public class UserTest {
 		Assertions.assertThat(result).isEqualTo("login@domain");
 	}
 	
-	@Test(expected=InvalidParameterException.class)
+	@Test(expected=IllegalArgumentException.class)
 	public void testGetLoginAtDomainWithSlashesAndArrobase() {
 		String text = "domain\\login@domain2";
 		User loginAtDomain = Factory.create().createUser(text, "email@domain");
 		loginAtDomain.getLoginAtDomain();
 	}
 	
-	@Test(expected=InvalidParameterException.class)
+	@Test(expected=IllegalArgumentException.class)
 	public void testGetLoginAtDomainWithArrobaseAndSlashes() {
 		String text = "doma@in\\login";
 		User loginAtDomain = Factory.create().createUser(text, "email@domain");
