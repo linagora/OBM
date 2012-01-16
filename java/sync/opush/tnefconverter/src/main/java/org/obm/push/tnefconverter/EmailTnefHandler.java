4ca713a465456363f1492fd1a74d1af3230de933
Joakim Ribier <jribier@linagora.com>
2012-01-16 18:33:23 +0100

    - [OBMFULL-2542] removed unnecessary code to fix the bug !

--- java/sync/opush/tnefconverter/src/main/java/org/obm/push/tnefconverter/EmailTnefHandler.java
+++ java/sync/opush/tnefconverter/src/main/java/org/obm/push/tnefconverter/EmailTnefHandler.java
@@ -36,8 +36,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashSet;
-import java.util.Set;
 
 import net.freeutils.tnef.Message;
 import net.freeutils.tnef.TNEFInputStream;
@@ -48,10 +46,6 @@ import org.apache.james.mime4j.MimeException;
 import org.apache.james.mime4j.parser.ContentHandler;
 import org.apache.james.mime4j.stream.BodyDescriptor;
 import org.apache.james.mime4j.stream.Field;
-import org.columba.ristretto.coder.EncodedWord;
-import org.columba.ristretto.message.Address;
-import org.columba.ristretto.parser.AddressParser;
-import org.columba.ristretto.parser.ParserException;
 import org.obm.push.utils.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,84 +54,12 @@ public class EmailTnefHandler implements ContentHandler {
 
 	protected Logger logger = LoggerFactory.getLogger(getClass());
 
-	private Set<Address> to;
-	private Set<Address> cc;
-	private Address from;
-	private String subject;
-
 	private Message tnefMsg;
-	private InputStream tnefDoc;
-
-	private String threadTopic;
-
-	public EmailTnefHandler() {
-		this.to = new HashSet<Address>();
-		this.cc = new HashSet<Address>();
-	}
-
-	public Address getFrom() {
-		return from;
-	}
-
-	public Set<Address> getTo() {
-		return to;
-	}
-
-	public Set<Address> getCc() {
-		return cc;
-	}
-
-	public String getSubject() {
-		return threadTopic != null && threadTopic.length()>0 ? threadTopic : subject;
-	}
-
-	public Message getTNEFMsg() {
-		return tnefMsg;
-	}
-
-	public InputStream getTnefDoc() {
-		return tnefDoc;
-	}
 
 	@Override
-	public void field(Field field) throws MimeException {
-		if ("subject".equalsIgnoreCase(field.getName())) {
-			this.subject = EncodedWord.decode(field.getBody()).toString().trim();
-		} else if ("thread-topic".equalsIgnoreCase(field.getName())) {
-			this.threadTopic = EncodedWord.decode(field.getBody()).toString().trim();
-		} else if ("to".equalsIgnoreCase(field.getName())) {
-			try {
-				Address[] adds = AddressParser.parseMailboxList(field.getBody());
-				for (Address add : adds) {
-					this.to.add(add);
-				}
-			} catch (ParserException e) {
-				throw new MimeException(e.getMessage());
-			}
-		} else if ("cc".equalsIgnoreCase(field.getName())) {
-			try {
-				Address[] adds = AddressParser.parseMailboxList(field.getBody());
-				for (Address add : adds) {
-					this.cc.add(add);
-				}
-			} catch (ParserException e) {
-				throw new MimeException(e.getMessage());
-			}
-		} else if ("from".equalsIgnoreCase(field.getName())) {
-			try {
-				this.from = AddressParser.parseAddress(field.getBody());
-			} catch (ParserException e) {
-				throw new MimeException(e.getMessage());
-			}
-		}
-	}
-
-	@Override
-	public void body(BodyDescriptor arg0, InputStream arg1)
-			throws MimeException, IOException {
+	public void body(BodyDescriptor arg0, InputStream arg1) throws MimeException, IOException {
 		if (TNEFUtils.isTNEFMimeType(arg0.getMimeType())) {
 			byte[] bb = FileUtils.streamBytes(arg1, false);
-			this.tnefDoc = new ByteArrayInputStream(bb);
 			try {
 				if ("base64".equalsIgnoreCase(arg0.getTransferEncoding())) {
 					bb = Base64.decodeBase64(new String(bb));
@@ -174,6 +96,14 @@ public class EmailTnefHandler implements ContentHandler {
 		}
 	}
 
+	public Message getTNEFMsg() {
+		return tnefMsg;
+	}
+
+	@Override
+	public void field(Field field) throws MimeException {
+	}
+	
 	@Override
 	public void startMultipart(BodyDescriptor arg0) throws MimeException {
 	}
--- java/sync/opush/tnefconverter/src/main/java/org/obm/push/tnefconverter/TNEFUtils.java
+++ java/sync/opush/tnefconverter/src/main/java/org/obm/push/tnefconverter/TNEFUtils.java
@@ -31,41 +31,33 @@
  * ***** END LICENSE BLOCK ***** */
 package org.obm.push.tnefconverter;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import net.freeutils.tnef.Message;
 
+import org.apache.james.mime4j.MimeException;
 import org.apache.james.mime4j.parser.MimeStreamParser;
 import org.obm.push.tnefconverter.ScheduleMeeting.TNEFExtractorUtils;
 
 public class TNEFUtils {
 
-	public static Boolean isScheduleMeetingRequest(InputStream email)
-			throws TNEFConverterException {
+	public static boolean isScheduleMeetingRequest(InputStream email) throws TNEFConverterException {
 		try {
 			MimeStreamParser parser = new MimeStreamParser();
 			EmailTnefHandler handler = new EmailTnefHandler();
 			parser.setContentHandler(handler);
 			parser.parse(email);
 			Message message = handler.getTNEFMsg();
-			return (message != null && TNEFExtractorUtils
-					.isScheduleMeetingRequest(message));
-		} catch (Throwable e) {
-			throw new TNEFConverterException(e);
+			if (message != null && TNEFExtractorUtils.isScheduleMeetingRequest(message) ) {
+				return true;
+			}
+		} catch (MimeException e) {
+			throw new TNEFConverterException(e.getMessage(), e);
+		} catch (IOException e) {
+			throw new TNEFConverterException(e.getMessage(), e);
 		}
+		return false;
 	}
 
-	public static Boolean containsTNEFAttchment(InputStream email)
-			throws TNEFConverterException {
-		try {
-			MimeStreamParser parser = new MimeStreamParser();
-			EmailTnefHandler handler = new EmailTnefHandler();
-			parser.setContentHandler(handler);
-			parser.parse(email);
-			Message message = handler.getTNEFMsg();
-			return message != null;
-		} catch (Throwable e) {
-			throw new TNEFConverterException(e);
-		}
-	}
 }
