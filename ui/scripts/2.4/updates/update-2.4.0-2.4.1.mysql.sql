da94690164a1fda84b770807eb61354ebd42dc95
Matthieu Baechler <matthieu.baechler@linagora.com>
2012-01-17 17:05:50 +0100

    - [OBMFULL-2881] mistake on sql migration script

--- ui/scripts/2.4/updates/update-2.4.0-2.4.1.mysql.sql
+++ ui/scripts/2.4/updates/update-2.4.0-2.4.1.mysql.sql
@@ -1,5 +1,5 @@
-UPDATE ObmInfo SET obminfo_value = '2.4.0-pre' WHERE obminfo_name = 'db_version';
+UPDATE ObmInfo SET obminfo_value = '2.4.1-pre' WHERE obminfo_name = 'db_version';
 
 DROP TABLE IF EXISTS opush_invitation_mapping;
 
-UPDATE ObmInfo SET obminfo_value = '2.4.0' WHERE obminfo_name = 'db_version';
+UPDATE ObmInfo SET obminfo_value = '2.4.1' WHERE obminfo_name = 'db_version';
