ce5344cc5de33cbbb7ab5af7335c8b8a2ded3820
debian-jkornobis <jkornobis@jkornobis.par.lng>
2012-02-02 17:29:06 +0100

    OBMFULL-3101 Remove All licences Stato
    Conflicts:
    
    	ui/lib/Stato/Webflow/Forms/Fields.php
    	ui/lib/Stato/Webflow/Forms/Form.php
    	ui/lib/Stato/Webflow/Forms/Inputs.php

--- ui/lib/Stato/Webflow/Forms/Fields.php
+++ ui/lib/Stato/Webflow/Forms/Fields.php
@@ -1,20 +1,4 @@
 <?php
-/*************************************************************************************
- Copyright (c) 1997-2010 OBM.org project members team
-                                                                        
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License 
- as published by the Free Software Foundation; version 2
- of the License.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of 
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details..
-
- http://www.obm.org
-*************************************************************************************/
-
 class Stato_Webflow_Forms_ValidationError extends Exception
 {
     protected $args;
