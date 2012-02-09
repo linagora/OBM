9096ad63436dcb592347a8622a344104ab7071c1
Joakim Ribier <jribier@linagora.com>
2012-02-09 18:20:24 +0100

    - [OBMFULL-3184] move sql codes in 2.4.0-->2.4.1

--- ui/scripts/2.4/updates/update-2.4.0-2.4.1.mysql.sql
+++ ui/scripts/2.4/updates/update-2.4.0-2.4.1.mysql.sql
@@ -2,4 +2,13 @@ UPDATE ObmInfo SET obminfo_value = '2.4.1-pre' WHERE obminfo_name = 'db_version'
 
 DROP TABLE IF EXISTS opush_invitation_mapping;
 
+ALTER TABLE Domain ADD COLUMN domain_uuid CHAR(36) NOT NULL;
+UPDATE Domain SET domain_uuid=UUID() WHERE domain_uuid='';
+
+ALTER TABLE P_Domain ADD COLUMN domain_uuid CHAR(36);
+UPDATE P_Domain p, Domain d SET p.domain_uuid=d.domain_uuid where p.domain_id=d.domain_id;
+ALTER TABLE P_Domain MODIFY domain_uuid CHAR(36) NOT NULL;
+
+UPDATE opush_sync_mail SET timestamp='1970-01-01 01:00:01' WHERE timestamp='0000-00-00 00:00:00';
+
 UPDATE ObmInfo SET obminfo_value = '2.4.1' WHERE obminfo_name = 'db_version';
--- ui/scripts/2.4/updates/update-2.4.0-2.4.1.pgsql.sql
+++ ui/scripts/2.4/updates/update-2.4.0-2.4.1.pgsql.sql
@@ -2,4 +2,12 @@ UPDATE ObmInfo SET obminfo_value = '2.4.1-pre' WHERE obminfo_name = 'db_version'
 
 DROP TABLE IF EXISTS opush_invitation_mapping;
 
+ALTER TABLE domain ADD COLUMN domain_uuid char(36);
+UPDATE domain SET domain_uuid=UUID() WHERE domain_uuid IS NULL;
+ALTER TABLE domain ALTER domain_uuid SET NOT NULL;
+
+ALTER TABLE p_domain ADD COLUMN domain_uuid CHAR(36);
+UPDATE p_domain p SET domain_uuid=( select domain_uuid FROM domain d where d.domain_id=p.domain_id);
+ALTER TABLE p_domain ALTER domain_uuid SET NOT NULL;
+
 UPDATE ObmInfo SET obminfo_value = '2.4.1' WHERE obminfo_name = 'db_version';
