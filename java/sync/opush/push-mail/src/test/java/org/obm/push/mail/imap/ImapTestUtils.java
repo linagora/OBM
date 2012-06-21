3610a0d23da531c16b83b913592828a9fc0486f7
Thomas Hilaire <thilaire@linagora.com>
2012-06-21 17:09:05 +0200

    - [OBMFULL-2868] Implement subscription imap commands

--- java/sync/opush/push-mail/src/test/java/org/obm/push/mail/imap/ImapTestUtils.java
+++ java/sync/opush/push-mail/src/test/java/org/obm/push/mail/imap/ImapTestUtils.java
@@ -124,4 +124,12 @@ public class ImapTestUtils {
 	public MailboxFolder folder(String name) {
 		return new MailboxFolder(name);
 	}
+
+	public MailboxFolder inbox() {
+		return folder(EmailConfiguration.IMAP_INBOX_NAME);
+	}
+
+	public InputStream getInputStreamFromFile(String name) {
+		return ClassLoader.getSystemResourceAsStream("eml/" + name);
+	}
 }
