00b82048e2e00d784115e7eef3228289801a4bf3
Romeu MOURA <romeu.moura@linagora.com>
2012-06-26 11:34:50 +0200

    OBMFULL-3695 .
    Conflicts:
    
    	java/sync/services/src/main/java/org/obm/sync/server/handler/LoginHandler.java

--- java/sync/services/src/main/java/org/obm/sync/server/XmlResponder.java
+++ java/sync/services/src/main/java/org/obm/sync/server/XmlResponder.java
@@ -1,33 +1,33 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * 
+ *
  * Copyright (C) 2011-2012  Linagora
  *
- * This program is free software: you can redistribute it and/or 
- * modify it under the terms of the GNU Affero General Public License as 
- * published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version, provided you comply 
- * with the Additional Terms applicable for OBM connector by Linagora 
- * pursuant to Section 7 of the GNU Affero General Public License, 
- * subsections (b), (c), and (e), pursuant to which you must notably (i) retain 
- * the “Message sent thanks to OBM, Free Communication by Linagora” 
- * signature notice appended to any and all outbound messages 
- * (notably e-mail and meeting requests), (ii) retain all hypertext links between 
- * OBM and obm.org, as well as between Linagora and linagora.com, and (iii) refrain 
- * from infringing Linagora intellectual property rights over its trademarks 
- * and commercial brands. Other Additional Terms apply, 
- * see <http://www.linagora.com/licenses/> for more details. 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version, provided you comply
+ * with the Additional Terms applicable for OBM connector by Linagora
+ * pursuant to Section 7 of the GNU Affero General Public License,
+ * subsections (b), (c), and (e), pursuant to which you must notably (i) retain
+ * the “Message sent thanks to OBM, Free Communication by Linagora”
+ * signature notice appended to any and all outbound messages
+ * (notably e-mail and meeting requests), (ii) retain all hypertext links between
+ * OBM and obm.org, as well as between Linagora and linagora.com, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks
+ * and commercial brands. Other Additional Terms apply,
+ * see <http://www.linagora.com/licenses/> for more details.
  *
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License 
- * for more details. 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Affero General Public License 
- * and its applicable Additional Terms for OBM along with this program. If not, 
- * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License version 3 
- * and <http://www.linagora.com/licenses/> for the Additional Terms applicable to 
- * OBM connectors. 
- * 
+ * You should have received a copy of the GNU Affero General Public License
+ * and its applicable Additional Terms for OBM along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License version 3
+ * and <http://www.linagora.com/licenses/> for the Additional Terms applicable to
+ * OBM connectors.
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.obm.sync.server;
 
@@ -103,7 +103,7 @@ public class XmlResponder {
 		}
 		return res;
 	}
-	
+
 	public String sendError(String message) {
 		return sendError(Strings.nullToEmpty(message), null);
 	}
@@ -113,6 +113,9 @@ public class XmlResponder {
 	}
 
 	public String sendToken(AccessToken at) {
+		if(at == null) {
+			return null;
+		}
 		String res = "";
 		try {
 			Document doc = DOMUtils.createDoc(
@@ -124,9 +127,9 @@ public class XmlResponder {
 			v.setAttribute("major", version.getMajor());
 			v.setAttribute("minor", version.getMinor());
 			v.setAttribute("release", version.getRelease());
-			
+
             DOMUtils.createElementAndText(root, "email", at.getUserEmail());
-			
+
 			Element domain = DOMUtils.createElementAndText(root, "domain", at.getDomain().getName());
 			domain.setAttribute("uuid", at.getDomain().getUuid());
 			res = emitResponse(doc);
