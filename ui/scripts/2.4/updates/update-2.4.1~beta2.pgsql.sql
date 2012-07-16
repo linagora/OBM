27736ebdb670b00279d87a0ca479a42815b6dc19
ddolcimascolo <ddolcimascolo@linagora.com>
2012-07-16 18:06:45 +0200

    OBMFULL-3695 Refactored code to take CR-1741 comments into account:  - The token is now inserted by the UI, right before a trustedLogin. The token is now created per user to "isolate" the transaction per user.  - UUID generation delegated to the database.  - Better types used in TrustToken bean and associated classes.

--- ui/scripts/2.4/updates/update-2.4.1~beta2.pgsql.sql
+++ ui/scripts/2.4/updates/update-2.4.1~beta2.pgsql.sql
@@ -5,10 +5,20 @@ BEGIN;
 
 CREATE TABLE trusttoken
 (
-  token character varying(255),
-  time_created timestamp without time zone default current_timestamp
+  id integer NOT NULL PRIMARY KEY,
+  token character(36) NOT NULL UNIQUE,
+  login varchar(80) NOT NULL,
+  time_created timestamp without time zone DEFAULT current_timestamp
 );
-INSERT INTO trusttoken (token) VALUES (UUID());
+
+CREATE SEQUENCE trusttoken_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+ALTER SEQUENCE trusttoken_id_seq OWNED BY trusttoken.id;
+ALTER TABLE trusttoken ALTER COLUMN id SET DEFAULT nextval('trusttoken_id_seq'::regclass);
 
 COMMIT;
-
