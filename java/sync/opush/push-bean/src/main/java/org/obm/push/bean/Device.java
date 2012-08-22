49961a5ce7feeae0cb92d590393a36d87b26e044
jkornobis <jkornobis@linagora.com>
2012-08-22 16:36:33 +0200

    Revert "- [OBMFULL-3928] Remove logger from Device's toString"
    This reverts commit 52e69e72ef58c301f6841660d1e13cbcd29d4adc.

--- java/sync/opush/push-bean/src/main/java/org/obm/push/bean/Device.java
+++ java/sync/opush/push-bean/src/main/java/org/obm/push/bean/Device.java
@@ -149,6 +149,7 @@ public class Device implements Serializable {
 	@Override
 	public String toString() {
 		return Objects.toStringHelper(this)
+			.add("logger", logger)
 			.add("devType", devType)
 			.add("hints", hints)
 			.add("devId", devId)
