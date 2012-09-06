96623e63983640f13e737ddc5894d5b445cc7700
ddolcimascolo <ddolcimascolo@linagora.com>
2012-09-06 17:57:46 +0200

    OBMFULL-4087 Patched 2.4.1-beta1 DB upgrade script to consider duplicates in the DeletedEvent table.

--- ui/scripts/2.4/updates/update-2.4.1~beta1.pgsql.sql
+++ ui/scripts/2.4/updates/update-2.4.1~beta1.pgsql.sql
@@ -5,8 +5,14 @@
  
 BEGIN;
 
-    DELETE FROM DeletedEvent WHERE deletedevent_id NOT IN (SELECT MAX(deletedevent_id) FROM DeletedEvent GROUP BY deletedevent_event_id, deletedevent_user_id);
+	DELETE FROM DeletedEvent WHERE deletedevent_id NOT IN (SELECT MAX(deletedevent_id) FROM DeletedEvent GROUP BY deletedevent_event_id, deletedevent_user_id);
 
-    ALTER TABLE DeletedEvent ADD CONSTRAINT deletedevent_uniquekey UNIQUE (deletedevent_event_id, deletedevent_user_id);
+	CREATE TABLE Tmp_DeletedEvent AS SELECT DISTINCT * FROM DeletedEvent;
+	DROP TABLE DeletedEvent;
+	ALTER TABLE Tmp_DeletedEvent RENAME TO DeletedEvent;
+
+
+	ALTER TABLE DeletedEvent ADD CONSTRAINT deletedevent_uniquekey UNIQUE (deletedevent_event_id, deletedevent_user_id);
+	ALTER TABLE DeletedEvent ADD PRIMARY KEY (deletedevent_id);
 
 COMMIT;
