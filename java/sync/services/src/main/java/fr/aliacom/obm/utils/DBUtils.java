f2cee6ac21a2553fc4340d1d94220512328a2f36
Emmanuel Surleau <esurleau@linagora.com>
2012-09-28 16:07:34 +0200

    OBMFULL-3839 Adds DBUtils.
    Various database-related methods, which should all be static.

---
+++ java/sync/services/src/main/java/fr/aliacom/obm/utils/DBUtils.java
@@ -0,0 +1,45 @@
+package fr.aliacom.obm.utils;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DBUtils {
+	private static final Logger logger = LoggerFactory.getLogger(ObmHelper.class);
+
+	public static void cleanup(ResultSet rs) {
+		cleanup(null, null, rs);
+	}
+
+	public static void cleanup(Statement stat, ResultSet rs) {
+		cleanup(null, stat, rs);
+	}
+	
+	public static void cleanup(Connection conn, Statement stat, ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				logger.warn("Error closing ResultSet", e);
+			}
+		}
+		if (stat != null) {
+			try {
+				stat.close();
+			} catch (SQLException e) {
+				logger.warn("Error closing Statement", e);
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				logger.warn("Error closing Connection", e);
+			}
+		}
+	}
+}
