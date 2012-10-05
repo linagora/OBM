379fd807166535270d7c05a670c36483636b83a1
Matthieu Baechler <matthieu.baechler@linagora.com>
2012-10-05 11:01:08 +0200

    - [OBMFULL-3935] QoS: basic infrastructure for rejecting and throttling request into a QoSFilter

---
+++ java/sync/qos-filter/src/main/java/org/obm/servlet/filter/qos/QoSFilter.java
@@ -0,0 +1,127 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Copyright (C) 2011-2012  Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for OBM
+ * software by Linagora pursuant to Section 7 of the GNU Affero General Public
+ * License, subsections (b), (c), and (e), pursuant to which you must notably (i)
+ * retain the displaying by the interactive user interfaces of the “OBM, Free
+ * Communication by Linagora” Logo with the “You are using the Open Source and
+ * free version of OBM developed and supported by Linagora. Contribute to OBM R&D
+ * by subscribing to an Enterprise offer !” infobox, (ii) retain all hypertext
+ * links between OBM and obm.org, between Linagora and linagora.com, as well as
+ * between the expression “Enterprise offer” and pro.obm.org, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks and
+ * commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for OBM along with this program. If not, see
+ * <http://www.gnu.org/licenses/> for the GNU Affero General   Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to the OBM software.
+ * ***** END LICENSE BLOCK ***** */
+package org.obm.servlet.filter.qos;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * A reusable servlet {@link Filter} able to reject and throttle requests based on some business logic.
+ */
+@Singleton
+public class QoSFilter implements Filter {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private final QoSRequestHandler handler;
+	private final QoSContinuationSupport continuationSupport;
+
+	@Inject
+	@VisibleForTesting QoSFilter(QoSRequestHandler handler, QoSContinuationSupport continuationSupport) {
+		this.handler = handler;
+		this.continuationSupport = continuationSupport;
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	/**
+	 * Actually performs the filtering.<br />
+	 * This should delegate to our {@link QoSRequestHandler} implementation to:
+	 * <ol>
+	 * <li>Decide whether or not the request can be processed, based on some business criteria (the user sending the request, etc.).</li>
+	 * <li>Eventually throttle the request if necessary, if the server is on heavy load or based on some configuration settings (maxRequestsPerSeconds or similar...)</li>
+	 * </ol>
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+		QoSAction action = startRequestHandling(httpRequest);
+		logger.debug("request action {}", action);
+		
+		switch (action) {
+		case ACCEPT:
+			handleRequest(httpRequest, httpResponse, chain);
+			break;
+		case SUSPEND:
+			suspendRequest(httpRequest);
+			break;
+		case REJECT:
+			httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+			break;
+		}
+	}
+
+	private QoSAction startRequestHandling(HttpServletRequest httpRequest) throws ServletException {
+		logger.debug("incoming request");
+		return handler.startRequestHandling(httpRequest);
+	}
+
+	private void handleRequest(HttpServletRequest request, HttpServletResponse response, FilterChain chain) 
+			throws IOException, ServletException {
+		try {
+			chain.doFilter(request, response);
+		} finally {
+			requestDone(request);
+		}
+	}
+
+	private void requestDone(HttpServletRequest request) {
+		logger.debug("request done");
+		handler.finishRequestHandling(request);
+	}
+	
+	private void suspendRequest(HttpServletRequest request) {
+		logger.debug("suspend request");
+		continuationSupport.suspend(request);
+	}
+	
+	@Override
+	public void destroy() {
+	}
+
+}
---
+++ java/sync/qos-filter/src/test/java/org/obm/servlet/filter/qos/QoSFilterTest.java
@@ -0,0 +1,119 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Copyright (C) 2011-2012  Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for OBM
+ * software by Linagora pursuant to Section 7 of the GNU Affero General Public
+ * License, subsections (b), (c), and (e), pursuant to which you must notably (i)
+ * retain the displaying by the interactive user interfaces of the “OBM, Free
+ * Communication by Linagora” Logo with the “You are using the Open Source and
+ * free version of OBM developed and supported by Linagora. Contribute to OBM R&D
+ * by subscribing to an Enterprise offer !” infobox, (ii) retain all hypertext
+ * links between OBM and obm.org, between Linagora and linagora.com, as well as
+ * between the expression “Enterprise offer” and pro.obm.org, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks and
+ * commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for OBM along with this program. If not, see
+ * <http://www.gnu.org/licenses/> for the GNU Affero General   Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to the OBM software.
+ * ***** END LICENSE BLOCK ***** */
+package org.obm.servlet.filter.qos;
+
+import static org.easymock.EasyMock.createStrictControl;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.easymock.IMocksControl;
+import org.eclipse.jetty.continuation.Continuation;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.obm.filter.SlowFilterRunner;
+
+/**
+ * Tests the {@link QoSFilter} class.
+ */
+@RunWith(SlowFilterRunner.class)
+public class QoSFilterTest {
+	private QoSFilter testee;
+	private IMocksControl control;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	private FilterChain chain;
+	private Continuation continuation;
+	private QoSRequestHandler qosRequestHandler;
+	private QoSContinuationSupport suspender;
+
+	@Before
+	public void setUp() {
+		control = createStrictControl();
+		suspender = control.createMock(QoSContinuationSupport.class);
+		request = control.createMock(HttpServletRequest.class);
+		response = control.createMock(HttpServletResponse.class);
+		chain = control.createMock(FilterChain.class);
+		continuation = control.createMock(Continuation.class);
+		qosRequestHandler = control.createMock(QoSRequestHandler.class);
+
+		// Because we run the tests outside Jetty
+		expect(continuation.isInitial()).andReturn(true).anyTimes();
+		expect(request.getAttribute(eq(Continuation.ATTRIBUTE))).andReturn(continuation).anyTimes();
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void testNullHandlers() throws Exception {
+		control.replay();
+
+		testee = new QoSFilter(null, suspender);
+		testee.doFilter(request, response, chain);
+	}
+
+	@Test
+	public void handlerAcceptRequest() throws IOException, ServletException {
+		expect(qosRequestHandler.startRequestHandling(request)).andReturn(QoSAction.ACCEPT);
+		chain.doFilter(request, response);
+		qosRequestHandler.finishRequestHandling(request);
+		control.replay();
+		testee = new QoSFilter(qosRequestHandler, suspender);
+		testee.doFilter(request, response, chain);
+		control.verify();
+	}
+	
+	@Test
+	public void handlerRefuseRequest() throws IOException, ServletException {
+		expect(qosRequestHandler.startRequestHandling(request)).andReturn(QoSAction.REJECT);
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+		control.replay();
+		testee = new QoSFilter(qosRequestHandler, suspender);
+		testee.doFilter(request, response, chain);
+		control.verify();
+	}
+
+	@Test
+	public void handlerSuspendRequest() throws IOException, ServletException {
+		expect(qosRequestHandler.startRequestHandling(request)).andReturn(QoSAction.SUSPEND);
+		suspender.suspend(request);
+		expectLastCall().once();
+		control.replay();
+		testee = new QoSFilter(qosRequestHandler, suspender);
+		testee.doFilter(request, response, chain);
+		control.verify();
+	}
+}
