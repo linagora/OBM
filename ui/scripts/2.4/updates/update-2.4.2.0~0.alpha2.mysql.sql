7dba78676a26eced438c535e5669caf50ea614de
Michael Bailly <mbailly@linagora.com>
2012-10-15 15:23:27 +0200

    [OBMFULL-4185] Correctly drop constaint in MySQL
    using DROP FOREIGN KEY, as DROP CONSTRAINT doesn't seem to exist

--- ui/scripts/2.4/updates/update-2.4.2.0~0.alpha2.mysql.sql
+++ ui/scripts/2.4/updates/update-2.4.2.0~0.alpha2.mysql.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE `EventTag` DROP CONSTRAINT `eventtag_user_id_userobm_id_fkey`;
+ALTER TABLE `EventTag` DROP FOREIGN KEY `eventtag_user_id_userobm_id_fkey`;
 ALTER TABLE `EventTag` ADD CONSTRAINT `eventtag_user_id_userobm_id_fkey` FOREIGN KEY (`eventtag_user_id`) REFERENCES `UserObm` (`userobm_id`) ON UPDATE CASCADE ON DELETE CASCADE;
 
 ALTER TABLE Resource ADD COLUMN resource_email TEXT;
