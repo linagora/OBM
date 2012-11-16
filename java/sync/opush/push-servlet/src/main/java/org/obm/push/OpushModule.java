0bacbcd4177a9e56dc25da5bf2616eb6b4342312
Antoine Duprat <aduprat@linagora.com>
2012-11-16 10:33:18 +0100

    - [OBMFULL-4271] Use imap module depending on VM argument

--- java/sync/opush/push-servlet/src/main/java/org/obm/push/OpushModule.java
+++ java/sync/opush/push-servlet/src/main/java/org/obm/push/OpushModule.java
@@ -30,19 +30,50 @@
  * 
  * ***** END LICENSE BLOCK ***** */
 package org.obm.push;
+import org.obm.configuration.VMArgumentsUtils;
 import org.obm.configuration.module.LoggerModule;
+import org.obm.push.java.mail.ImapModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 
 public class OpushModule extends AbstractModule {
 
+	private final static String JAVA_MAIL_MODULE = "javaMail";
+	private static final Logger logger = LoggerFactory.getLogger(LoggerModule.CONFIGURATION);
+	
 	@Override
 	protected void configure() {
-		install(new LinagoraImapModule());
+		installImapModule();
 		install(new OpushImplModule());
 		install(new OpushMailModule());
 		install(new LoggerModule());
 		bind(Boolean.class).annotatedWith(Names.named("enable-push")).toInstance(false);
  	}
+
+	private void installImapModule() {
+		String imapModuleName = VMArgumentsUtils.stringArgumentValue(OptionalVMArguments.BACKEND_EMAIL_NAME);
+		install(imapModule(imapModuleName));
+	}
+	
+	private AbstractModule imapModule(String imapModuleName) {
+		if (Strings.isNullOrEmpty(imapModuleName)) {
+			return defaultImapModule();
+		}
+		
+		if (JAVA_MAIL_MODULE.equals(imapModuleName)) {
+			logger.debug("Using java mail imap module");
+			return new ImapModule();
+		}
+		
+		return defaultImapModule();
+	}
+	
+	private AbstractModule defaultImapModule() {
+		logger.debug("Using default imap module");
+		return new LinagoraImapModule();
+	}
 }
