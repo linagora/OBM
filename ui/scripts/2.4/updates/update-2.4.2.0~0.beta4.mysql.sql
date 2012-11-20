764e77b6ef192654def6e877f5fbc39e11a0a0df
jeremie kornobis <jkornobis@linagora.com>
2012-11-20 14:28:02 +0100

    OBMFULL-4284 MYSQL and PGSQL files to create Table DeletedEventLink

--- ui/scripts/2.4/updates/update-2.4.2.0~0.beta4.mysql.sql
+++ ui/scripts/2.4/updates/update-2.4.2.0~0.beta4.mysql.sql
@@ -1,11 +1,36 @@
 --
 -- Adds a new preference 'set_allday_opacity'
 --
+-- Add Table: deletedeventlink : In order to save deleted users from an event 
+--
 
 BEGIN;
 
 INSERT INTO UserObmPref (userobmpref_option, userobmpref_value)
 SELECT 'set_allday_opacity', 'TRANSPARENT' FROM Dual
 WHERE NOT EXISTS (SELECT 1 FROM UserObmPref WHERE userobmpref_option='set_allday_opacity');
+
+-- TABLE deletedeventlink;
+
+CREATE  TABLE `obm`.`DeletedEventLink` (
+  `deletedeventlink_id` INT NOT NULL AUTO_INCREMENT ,
+  `deletedeventlink_userobm_id` INT NOT NULL ,
+  `deletedeventlink_event_id` INT NOT NULL ,
+  `deletedeventlink_event_ext_id` VARCHAR(300) NOT NULL ,
+  `deletedeventlink_time_removed` TIMESTAMP NOT NULL DEFAULT now() ,
+  PRIMARY KEY (`deletedeventlink_id`) ,
+  INDEX `deletedeventlink_userobm_id_userobm_id_fkey` (`deletedeventlink_userobm_id` ASC) ,
+  INDEX `deletedeventlink_event_id_event_id_fkey` (`deletedeventlink_event_id` ASC) ,
+  CONSTRAINT `deletedeventlink_userobm_id_userobm_id_fkey`
+    FOREIGN KEY (`deletedeventlink_userobm_id` )
+    REFERENCES `obm`.`UserObm` (`userobm_id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `deletedeventlink_event_id_event_id_fkey`
+    FOREIGN KEY (`deletedeventlink_event_id` )
+    REFERENCES `obm`.`Event` (`event_id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
 
 COMMIT;
--- ui/scripts/2.4/updates/update-2.4.2.0~0.beta4.pgsql.sql
+++ ui/scripts/2.4/updates/update-2.4.2.0~0.beta4.pgsql.sql
@@ -1,11 +1,66 @@
 --
 -- Adds a new preference 'set_allday_opacity'
 --
+-- Add Table: deletedeventlink: In order to save deleted users from an event 
+--
 
 BEGIN;
 
 INSERT INTO UserObmPref (userobmpref_option, userobmpref_value)
 SELECT 'set_allday_opacity', 'TRANSPARENT'
 WHERE NOT EXISTS (SELECT 1 FROM UserObmPref WHERE userobmpref_option='set_allday_opacity');
+
+-- TABLE deletedeventlink;
+
+CREATE TABLE deletedeventlink
+(
+  deletedeventlink_id serial NOT NULL,
+  deletedeventlink_userobm_id integer NOT NULL,
+  deletedeventlink_event_id integer NOT NULL,
+  deletedeventlink_event_ext_id character varying(300) NOT NULL,
+  deletedeventlink_time_removed timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT deletedeventlink_deletedeventlink_id_pkey PRIMARY KEY (deletedeventlink_id ),
+  CONSTRAINT deletedeventlink_event_id_event_id_fkey FOREIGN KEY (deletedeventlink_event_id)
+      REFERENCES event (event_id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT deletedeventlink_userobm_id_userobm_id_fkey FOREIGN KEY (deletedeventlink_userobm_id)
+      REFERENCES userobm (userobm_id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE deletedeventlink
+  OWNER TO obm;
+
+-- Index: fki_deletedeventlink_event_id_event_id_fkey
+-- DROP INDEX fki_deletedeventlink_event_id_event_id_fkey;
+
+CREATE INDEX fki_deletedeventlink_event_id_event_id_fkey
+  ON deletedeventlink
+  USING btree
+  (deletedeventlink_event_id );
+
+-- Index: fki_deletedeventlink_userobm_id_userobm_id_fkey
+-- DROP INDEX fki_deletedeventlink_userobm_id_userobm_id_fkey;
+
+CREATE INDEX fki_deletedeventlink_userobm_id_userobm_id_fkey
+  ON deletedeventlink
+  USING btree
+  (deletedeventlink_userobm_id );
+
+-- Sequence: deletedeventlink_deletedeventlink_id_seq
+-- DROP SEQUENCE deletedeventlink_deletedeventlink_id_seq;
+
+CREATE SEQUENCE deletedeventlink_deletedeventlink_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 13
+  CACHE 1;
+ALTER TABLE deletedeventlink_deletedeventlink_id_seq
+  OWNER TO obm;
+
+ALTER TABLE deletedeventlink ALTER COLUMN deletedeventlink_id SET DEFAULT nextval('deletedeventlink_deletedeventlink_id_seq'::regclass);
 
 COMMIT;
