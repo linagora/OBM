08bdda8f209c751b1d9f4f3dc8cd9dbaa68f5996
ddolcimascolo <ddolcimascolo@linagora.com>
2012-11-23 09:45:21 +0100

    OBMFULL-4318 Implemented OBM_CCaptchaDriver.

--- ui/locale/fr.php
+++ ui/locale/fr.php
@@ -123,5 +123,7 @@ return array(
     => "Vous utilisez la version libre et gratuite d'<a href=\"http://www.obm.org/\">OBM</a> développée et supportée par <a href=\"http://www.linagora.com/\">Linagora</a>.",
   "Contribute to the product R&amp;D by subscribing to an <a href=\"http://pro.obm.org/\">enterprise offer</a>."
     => "Contribuez à la R&amp;D du produit en souscrivant à une <a href=\"http://pro.obm.org/\">offre entreprise</a>.",
- 'OBM free/busy information sharing : %firstname% %name%' => 'Partage de l\'agenda des disponibilités : %firstname% %name%'
+ 'OBM free/busy information sharing : %firstname% %name%' => 'Partage de l\'agenda des disponibilités : %firstname% %name%',
+  "Captcha" => "Captcha",
+  "Verification" => "Vérification"		
 );
