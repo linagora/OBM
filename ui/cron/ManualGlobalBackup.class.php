164242e1f98066d5fea40e0085e0a4a25df455c6
ddolcimascolo <ddolcimascolo@linagora.com>
2012-12-03 14:58:19 +0100

    OBMFULL-4368 Added a shell script to do parallel backups.

--- ui/cron/ManualGlobalBackup.class.php
+++ ui/cron/ManualGlobalBackup.class.php
@@ -46,7 +46,7 @@ include_once("$obminclude/global.inc");
 include_once("Logger.class.php");
 include_once("Cron.class.php");
   
-define("L_LEVEL", L_DEBUG);
+define("L_LEVEL", L_INFO);
   
 set_error_handler('errorHandler');
     
@@ -68,18 +68,10 @@ class ManualGlobalBackup extends CronJob {
     return ($min%$delta === $instant);
   }
   
-  function execute() {
-    $this->logger->info('Doing backup of users and mailshares using the Satellite BackupEntity module');
-    $domains = $this->getDomains();
-    foreach ($domains as $domain_id => $domain_name) {
-      $this->processDomain($domain_id, $domain_name);
-    }
-    try {
-      $this->prepareCurl($this->backup->getDetails());
-      $this->notifyEndOfBackups();
-    } catch (Exception $e) {
-      $this->logger->debug($e->getMessage());
-    }
+  function execute($domain_id, $backend, $offset, $nbUsers) {
+    $this->logger->debug('Doing backup of users and mailshares using the Satellite BackupEntity module');
+    $domain_name = $this->getDomainName($domain_id);
+    $this->processDomain($domain_id, $domain_name, $backend, $offset, $nbUsers);
   }
 
   protected function prepareCurl($args) {
@@ -108,26 +100,48 @@ class ManualGlobalBackup extends CronJob {
       throw new Exception('Invalid query options');
   }
 
-  protected function notifyEndOfBackups() {
-    $this->logger->info('Notify obm-satellite of the end of backups');
-    $body = curl_exec($this->request);
-    $code = curl_getinfo($this->request, CURLINFO_HTTP_CODE);
-    if ($body===FALSE)
-      throw new Exception(curl_error($this->request));
-
-    curl_close($this->request);
-    if (($code >= 200) && ($code < 300)) {
-      $this->logger->info('Successful file has been created in backupRoot directory on satellite host');
-      return;
+  function notifyEndOfBackups() {
+    $hosts = $this->getSatelliteHosts();
+    
+    foreach ($hosts as $host) {
+      $args = array("host" => $host);
+      $this->prepareCurl($args);
+      $this->logger->info('Notify obm-satellite (' . $host . ') of the end of backups');
+      $body = curl_exec($this->request);
+      $code = curl_getinfo($this->request, CURLINFO_HTTP_CODE);
+      if ($body===FALSE)
+        throw new Exception(curl_error($this->request));
+      
+      curl_close($this->request);
+      if (($code >= 200) && ($code < 300)) {
+        $this->logger->info('Successful file has been created in backupRoot directory on satellite host');
+      } else {
+        $this->logger->error("Unexpected response code ($code) from the satellite");
+      }
     }
-
-    throw new Exception("$code: Unexpected response code from the satellite");
   }
 
-  protected function processDomain($domain_id, $domain_name) {
+  protected function getSatelliteHosts() {
+    $obm_q = new DB_OBM;
+    $smtp_query = "SELECT
+        host_ip
+        FROM Host
+        WHERE host_id IN (SELECT userobm_mail_server_id FROM UserObm GROUP BY userobm_mail_server_id)";
+    $obm_q->query($smtp_query);
+    $smtp_servers = array();
+    $hosts = array();
+    
+    while ($obm_q->next_record()) {
+      $hosts[] = $obm_q->f('host_ip');
+    }
+    
+    return $hosts;
+  }
+
+  protected function processDomain($domain_id, $domain_name, $backend, $offset, $nbUsers) {
   	global $obminclude, $admin_mail_address;
 
-    $this->logger->info("Processing users and mailshares of domain $domain_name (id: $domain_id)");
+    $this->logger->info("Processing users and mailshares of domain $domain_name (id: $domain_id, backend: $backend)");
     $GLOBALS['obm']['domain_id'] = $domain_id;
 
     $options = array('sendMail'=>false);
@@ -136,7 +150,7 @@ class ManualGlobalBackup extends CronJob {
     $mailBody = "";
     $mailIntro = "";
 
-	$this->processUsersBackup($domain_id, $options, $errors, $mailBody, $count);
+    $this->processUsersBackup($domain_id, $options, $errors, $mailBody, $count, $backend, $offset, $nbUsers);
 
     if ($errors > 0) {
         $mailSubject = "Errors with the daily obm backups";
@@ -150,10 +164,10 @@ class ManualGlobalBackup extends CronJob {
     }
   }
 
-  protected function processUsersBackup($domain_id, $options, &$errors, &$mailBody, &$count) {
-  	$users = $this->getDomainUsers($domain_id);
+  protected function processUsersBackup($domain_id, $options, &$errors, &$mailBody, &$count, $backend, $offset, $nbUsers) {
+  	$users = $this->getDomainUsers($domain_id, $backend, $offset, $nbUsers);
   	$count = count($users);
-  	$this->logger->debug($count." users found in this domain (id: $domain_id)");
+  	$this->logger->debug($count." users found in this domain (id: $domain_id, backend: $backend)");
   	foreach ($users as $user_id => $login) {
   		$this->logger->debug("Processing user : $login (id: $user_id)...");
   		try {
@@ -163,22 +177,24 @@ class ManualGlobalBackup extends CronJob {
   		} catch (Exception $e) {
   			$errors++;
   			$mailBody .= "Error when processing user : $login (id: $user_id) :\n";
-  			$this->logger->error("Error when processing user $login (id: $user_id)");
+  			$this->logger->error("Error when processing user $login (id: $user_id), $e");
   			$mailBody .= "-> ".$e->getMessage()."\n";
   			$this->logger->error($e->getMessage());
   		}
   	}
   }
 
-  protected function getDomainUsers($domain_id) {
+  protected function getDomainUsers($domain_id, $backend, $offset, $nbUsers) {
     $users = array();
     $obm_q = new DB_OBM;
     $query = 
      "SELECT userobm_id, userobm_login
       FROM UserObm
       WHERE userobm_archive=0 
+      AND userobm_mail_server_id=$backend
       AND userobm_domain_id=$domain_id 
-      AND userobm_mail_perms=1;";
+      AND userobm_mail_perms=1
+      LIMIT $nbUsers OFFSET $offset;";
     $obm_q->query($query);
 
     while ($obm_q->next_record()) {
@@ -199,7 +215,21 @@ class ManualGlobalBackup extends CronJob {
     while ($obm_q->next_record()) {
       $domains[$obm_q->f('domain_id')] = $obm_q->f('domain_name');
     }
+
     return $domains;
+  }
+
+  protected function getDomainName($domain_id) {
+    $obm_q = new DB_OBM;
+    $query = 
+     "SELECT domain_name 
+      FROM Domain 
+      WHERE domain_global='f' 
+      AND domain_id=$domain_id";
+    $obm_q->query($query);
+    $obm_q->next_record();
+
+    return $obm_q->f('domain_name');
   }
 
   protected function buildUrl($args) {
@@ -207,9 +237,67 @@ class ManualGlobalBackup extends CronJob {
     $port = OBM_Satellite_Icredentials::$port;
     return "https://{$host}:{$port}/endOfBackups/";
   }
+
+  function countUsersByDomain() {
+    $domains = $this->getDomains();
+    $backupFile = fopen('/tmp/globalBackups.init', 'w+');
+
+    foreach ($domains as $domain_id => $domain_name) {
+        $usersByBackend = $this->countUsers($domain_id);
+        fputs($backupFile, $domain_id."=".$usersByBackend."\n");
+    }
+
+    fclose($backupFile);
+  }
+
+  protected function countUsers($domain_id) {
+    $obm_q = new DB_OBM;
+    $query = 
+     "SELECT userobm_mail_server_id as backend, count(userobm_id) as count 
+      FROM UserObm 
+      WHERE userobm_archive=0 
+      AND userobm_domain_id=$domain_id 
+      AND userobm_mail_perms=1
+      GROUP BY userobm_mail_server_id";
+    $obm_q->query($query);
+    
+    $usersByBackend = array();
+    while ($obm_q->next_record()) {
+      $usersByBackend[] = $obm_q->f('backend') . ':' . $obm_q->f('count');
+    }
+
+    return implode(' ', $usersByBackend);
+  }
+
+  function usage() {
+    echo "OPTIONS\n";
+    echo "\t--init-backup\n";
+    echo "\t   Create a /tmp/globalBackups.init file with a domain_id=nbUsers format.\n";
+    echo "\t--backup --domain=x --offset=y --nbUsers=z --backend=b\n";
+    echo "\t   Do the backup on the domain x of z users, starting at the y one.\n";
+    echo "\t--end-backup\n";
+    echo "\t   Notify the satellite the end of the backup.\n";
+  }
 };
 
+
 $manualGlobalBackup = new ManualGlobalBackup;
-$manualGlobalBackup->execute();
+
+if (strcmp($argv[1], "--init-backup") == 0) {
+    $manualGlobalBackup->countUsersByDomain();
+} else if (strcmp($argv[1], "--end-backup") == 0) {
+    $manualGlobalBackup->notifyEndOfBackups();
+} else if (strcmp($argv[1], "--backup") == 0 && count($argv) == 6) { 
+    $longopts = array("domain:", "offset:", "nbUsers:", "backend:");
+    $options = getopt(null, $longopts);
+    if (count($options) != 4) {
+      $manualGlobalBackup->usage();
+    } else {	
+      $manualGlobalBackup->execute($options['domain'], $options['backend'], $options['offset'], $options['nbUsers']);
+    }
+} else {
+    $manualGlobalBackup->usage();
+}
+
 
 ?>
