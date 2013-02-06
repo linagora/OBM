51a4a9ea0719717e03c08c45641d2a24f571ebc2
Antoine Duprat <aduprat@linagora.com>
2013-02-06 15:06:52 +0100

    - [OBMFULL-4569] New table AddedElement

--- ui/scripts/2.4/updates/update-2.4.2.2~0.mysql.sql
+++ ui/scripts/2.4/updates/update-2.4.2.2~0.mysql.sql
@@ -5,6 +5,23 @@ UPDATE ObmInfo SET obminfo_value = '2.4.2.2-pre' WHERE obminfo_name = 'db_versio
 ALTER TABLE opush_sync_perms ADD COLUMN pending_accept BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE opush_sync_perms ALTER pending_accept DROP DEFAULT;
 
-UPDATE ObmInfo SET obminfo_value = '2.4.2.2' WHERE obminfo_name = 'db_version';
+--
+-- Add Table: CommitedOperation: In order to map client_id (as SHA1) to entity_id on elements persisted
+--
 
+BEGIN;
+
+--
+-- Table structure for table CommitedOperation 
+--
+
+CREATE TABLE CommitedOperation (
+    `commitedoperation_hash_client_id` varchar(44) NOT NULL,
+    `commitedoperation_entity_id` integer NOT NULL,
+    `commitedoperation_kind` enum('VEVENT','VCONTACT') NOT NULL,
+    PRIMARY KEY  (`commitedoperation_hash_client_id`),
+    CONSTRAINT `commitedoperation_entity_id_fkey` FOREIGN KEY (`commitedoperation_entity_id`) REFERENCES `Entity` (`entity_id`) ON DELETE CASCADE
+);
+
+UPDATE ObmInfo SET obminfo_value = '2.4.2.2' WHERE obminfo_name = 'db_version';
 COMMIT;
--- ui/scripts/2.4/updates/update-2.4.2.2~0.pgsql.sql
+++ ui/scripts/2.4/updates/update-2.4.2.2~0.pgsql.sql
@@ -5,6 +5,31 @@ UPDATE ObmInfo SET obminfo_value = '2.4.2.2-pre' WHERE obminfo_name = 'db_versio
 ALTER TABLE opush_sync_perms ADD COLUMN pending_accept BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE opush_sync_perms ALTER pending_accept DROP DEFAULT;
 
-UPDATE ObmInfo SET obminfo_value = '2.4.2.2' WHERE obminfo_name = 'db_version';
+--
+-- Add Table: commitedoperation: In order to map client_id (as SHA1) to entity_id on elements persisted
+--
 
+BEGIN;
+
+--
+-- Name: vkind; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE vkind AS ENUM (
+    'VEVENT',
+    'VCONTACT'
+);
+
+--
+-- Name: commitedoperation; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE commitedoperation (
+    commitedoperation_hash_client_id varchar(44) NOT NULL PRIMARY KEY,
+    commitedoperation_entity_id integer NOT NULL, 
+    commitedoperation_kind vkind NOT NULL,
+    CONSTRAINT commitedoperation_entity_id_fkey FOREIGN KEY (commitedoperation_entity_id) REFERENCES entity(entity_id) ON DELETE CASCADE
+);
+
+UPDATE ObmInfo SET obminfo_value = '2.4.2.2' WHERE obminfo_name = 'db_version';
 COMMIT;
