1c02ef73e4c7c29d778e01e6d4cac4879e492f7e
Michael Bailly <mbailly@linagora.com>
2013-02-22 17:11:06 +0100

    [OBMFULL-4639] new badges module

---
+++ ui/php/healthcheck/assets/js/badges.js
@@ -0,0 +1,65 @@
+define([], function() {
+
+  var badgeName = {"1" : "#badge-warnings", "2" : "#badge-errors" };
+  
+  var increment = function(code) {
+    if ( !(code in badgeName) ) {
+      return ;
+    }
+    setCount(code, (getCount(code)+1));
+  };
+
+  var decrement = function(code) {
+    if ( !(code in badgeName) ) {
+      return ;
+    }
+    var count = getCount(code);
+    count--;
+    if ( count < 0 ) {
+      return ;
+    }
+    setCount(code, count);
+  };
+  
+  var setCount = function(code, count) {
+    if ( !(code in badgeName) ) {
+      return ;
+    }
+    return parseInt($( badgeName[code] ).text(count),10);
+  };
+  
+  var getCount = function(code) {
+    if ( !(code in badgeName) ) {
+      return ;
+    }
+    return $( badgeName[code] ).text();
+  };
+  
+  var getErrorCount = function() {
+    return getCount(2);
+  };
+  
+  var getWarningCount = function() {
+    return getCount(1);
+  };
+  
+  var decrementError = function() {
+    return decrement(2);
+  };
+  
+  var decrementWarning = function() {
+    return decrement(1);
+  };
+  
+  return {
+    increment: increment,
+    decrement: decrement,
+    getCount: getCount,
+    setCount: setCount,
+    getErrorCount: getErrorCount,
+    getWarningCount: getWarningCount,
+    decrementError: decrementError,
+    decrementWarning: decrementWarning
+  };
+  
+});
