f0fafdf37f4c57971dd5a482149db72781ddd508
Stéphen Le Maistre <slemaistre@linagora.com>
2013-04-16 11:06:23 +0200

    [OBMFULL-4923] - Use mv command of system instead of rename of Perl because rename does not support crossing file system boundaries.

--- satellite/libperl-ObmSatellite/ObmSatellite/Modules/postfixAccess.pm
+++ satellite/libperl-ObmSatellite/ObmSatellite/Modules/postfixAccess.pm
@@ -124,7 +124,9 @@ sub _updateTransportFile {
     }
     close $out_fh;
     close $in_fh;
-    if (! rename ($out_fh->filename(), TRANSPORT_MAP) ) {
+    my $cmd = 'mv "'.$out_fh->filename().'" "'.TRANSPORT_MAP.'"';
+    $self->_log("Executing ".$cmd, 5);
+    if (system($cmd)) {
         $self->_log("Can't rename ".$out_fh->filename()." to ".TRANSPORT_MAP, 1);
         return 0;
     }
