a36dea4589e665ec8ba1e2fcd334a7fbb0b35711
Michael Bailly <mbailly@linagora.com>
2013-06-20 16:44:35 +0200

    [OBMFULL-4100] remove debug logging in roundcube unread plugin

--- ui/php/webmail/plugins/unread/unread.php
+++ ui/php/webmail/plugins/unread/unread.php
@@ -7,15 +7,12 @@ class unread extends rcube_plugin {
   }
 
   function ready($args) {
-    error_log("unread PLUGIN: ready method starts");
     if ( $args["task"] != "mail" || $args["action"] != "unread_plugin" ) {
       return ;
     }
-    error_log("unread PLUGIN: I'm called");
     $RCMAIL = rcmail::get_instance();
     $storage = $RCMAIL->get_storage();
     $unseen = $storage->count('INBOX', 'UNSEEN', null);
-    error_log("unread PLUGIN: unseen = ".$unseen);
     echo $unseen;
     exit();
   }
