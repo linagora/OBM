8fd63bbc40bf65d942f1be5315e93b4aab0a96c6
Emmanuel Surleau <esurleau@linagora.com>
2013-06-28 11:09:24 +0200

    OBMFULL-5126 Puts the initial data in its own file.

---
+++ java/sync/obm-dao/src/test/resources/sql/initial_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO domain (domain_name, domain_uuid, domain_label) VALUES ('test.tlse.lng', 'ac21bc0c-f816-4c52-8bb9-e50cfbfec5b6', 'test.tlse.lng');
+INSERT INTO domain (domain_name, domain_uuid, domain_label) VALUES ('test2.tlse.lng', '3a2ba641-4ae0-4b40-aa5e-c3fd3acb78bf', 'test2.tlse.lng');
+
+INSERT INTO profile (profile_domain_id, profile_name) VALUES (1, 'admin');
+INSERT INTO profile (profile_domain_id, profile_name) VALUES (1, 'user');
+INSERT INTO profile (profile_domain_id, profile_name) VALUES (2, 'editor');
