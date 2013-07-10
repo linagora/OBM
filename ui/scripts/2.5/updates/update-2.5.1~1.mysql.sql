deae5df44f3d9ae990a93dbc69655b2882dcebae
Erwan Queffélec <equeffelec@linagora.com>
2013-07-10 08:54:10 +0200

    OBMFULL-5172 Add set_allday_opacity preference to 2.5.1 update script

--- ui/scripts/2.5/updates/update-2.5.1~1.mysql.sql
+++ ui/scripts/2.5/updates/update-2.5.1~1.mysql.sql
@@ -4,4 +4,8 @@ CREATE INDEX opush_synced_item_sync_state_id_fkey ON opush_synced_item(sync_stat
 
 CREATE INDEX opush_sync_state_device_id_collection_id_fkey ON opush_sync_state(device_id, collection_id);
 
+INSERT INTO UserObmPref (userobmpref_option, userobmpref_value)
+SELECT 'set_allday_opacity', 'TRANSPARENT' FROM Dual
+WHERE NOT EXISTS (SELECT 1 FROM UserObmPref WHERE userobmpref_option='set_allday_opacity');
+
 COMMIT;
