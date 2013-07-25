9deb914b09ba92b3750ad23e3ffe4c59a2d2bc9f
Matthieu Baechler <matthieu.baechler@linagora.com>
2013-07-25 15:31:19 +0200

    - [OBMFULL-5285] add MailRequest logging
    Conflicts:
    	java/sync/opush/push-core/src/main/java/org/obm/push/handler/MailRequestHandler.java
    	java/sync/opush/push-core/src/main/java/org/obm/push/handler/SendMailHandler.java
    	java/sync/opush/push-core/src/main/java/org/obm/push/handler/SmartForwardHandler.java
    	java/sync/opush/push-core/src/main/java/org/obm/push/handler/SmartReplyHandler.java

--- java/sync/opush/push-core/src/main/java/org/obm/push/handler/SendMailHandler.java
+++ java/sync/opush/push-core/src/main/java/org/obm/push/handler/SendMailHandler.java
@@ -49,9 +49,12 @@ public class SendMailHandler extends MailRequestHandler {
 
 	@Inject
 	protected SendMailHandler(MailBackend mailBackend, ErrorsManager errorManager, 
-			MailProtocol mailProtocol, @Named(LoggerModule.MAIL_DATA)Logger mailDataLogger) {
-		
-		super(mailBackend, errorManager, mailProtocol, mailDataLogger);
+			MailProtocol mailProtocol,
+			@Named(LoggerModule.MAIL_DATA)Logger mailDataLogger,
+			@Named(LoggerModule.FULL_REQUEST)Logger fullRequestLogger,
+			@Named(LoggerModule.TRIMMED_REQUEST)Logger trimmedRequestLogger) {
+
+		super(mailBackend, errorManager, mailProtocol, mailDataLogger, fullRequestLogger, trimmedRequestLogger);
 	}
 
 	@Override
