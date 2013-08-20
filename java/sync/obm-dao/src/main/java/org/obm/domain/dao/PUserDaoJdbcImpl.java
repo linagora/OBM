9962cceead71bb6f0e192e8f64a06f6ffb4110e7
Emmanuel Surleau <esurleau@linagora.com>
2013-08-20 09:23:03 +0200

    OBMFULL-5150 Adds P_UserObm and friends DAO.

---
+++ java/sync/obm-dao/src/main/java/org/obm/domain/dao/PUserDaoJdbcImpl.java
@@ -0,0 +1,364 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Copyright (C) 2011-2013  Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for OBM
+ * software by Linagora pursuant to Section 7 of the GNU Affero General Public
+ * License, subsections (b), (c), and (e), pursuant to which you must notably (i)
+ * retain the displaying by the interactive user interfaces of the “OBM, Free
+ * Communication by Linagora” Logo with the “You are using the Open Source and
+ * free version of OBM developed and supported by Linagora. Contribute to OBM R&D
+ * by subscribing to an Enterprise offer !” infobox, (ii) retain all hypertext
+ * links between OBM and obm.org, between Linagora and linagora.com, as well as
+ * between the expression “Enterprise offer” and pro.obm.org, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks and
+ * commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for OBM along with this program. If not, see
+ * <http://www.gnu.org/licenses/> for the GNU Affero General   Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to the OBM software.
+ * ***** END LICENSE BLOCK ***** */
+package org.obm.domain.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
+import javax.inject.Inject;
+
+import org.obm.dbcp.DatabaseConnectionProvider;
+import org.obm.provisioning.dao.exceptions.DaoException;
+import org.obm.push.utils.JDBCUtils;
+
+import com.google.common.collect.ImmutableList;
+
+public class PUserDaoJdbcImpl implements PUserDao {
+
+	private DatabaseConnectionProvider dbcp;
+
+	@Inject
+	public PUserDaoJdbcImpl(DatabaseConnectionProvider dbcp) {
+		this.dbcp = dbcp;
+	}
+
+	@Override
+	public void insertByUserExtIds(Collection<String> userExtIds) throws DaoException {
+		Connection conn = null;
+
+		try {
+			conn = dbcp.getConnection();
+			Collection<Integer> userIds = mapExtIdsToUserIds(conn, userExtIds);
+			String userObmQuery = "INSERT INTO P_UserObm " +
+					"(userobm_id, " +
+					"userobm_domain_id, " +
+					"userobm_timecreate, " +
+					"userobm_userupdate, " +
+					"userobm_usercreate, " +
+					"userobm_local, " +
+					"userobm_ext_id, " +
+					"userobm_system, " +
+					"userobm_archive, " +
+					"userobm_timelastaccess, " +
+					"userobm_login, " +
+					"userobm_nb_login_failed, " +
+					"userobm_password_type, " +
+					"userobm_password, " +
+					"userobm_password_dateexp, " +
+					"userobm_account_dateexp, " +
+					"userobm_perms, " +
+					"userobm_delegation_target, " +
+					"userobm_delegation, " +
+					"userobm_calendar_version, " +
+					"userobm_uid, " +
+					"userobm_gid, " +
+					"userobm_datebegin, " +
+					"userobm_hidden, " +
+					"userobm_kind, " +
+					"userobm_commonname, " +
+					"userobm_lastname, " +
+					"userobm_firstname, " +
+					"userobm_title, " +
+					"userobm_sound, " +
+					"userobm_company, " +
+					"userobm_direction, " +
+					"userobm_service, " +
+					"userobm_address1, " +
+					"userobm_address2, " +
+					"userobm_address3, " +
+					"userobm_zipcode, " +
+					"userobm_town, " +
+					"userobm_expresspostal, " +
+					"userobm_country_iso3166, " +
+					"userobm_phone, " +
+					"userobm_phone2, " +
+					"userobm_mobile, " +
+					"userobm_fax, " +
+					"userobm_fax2, " +
+					"userobm_web_perms, " +
+					"userobm_web_list, " +
+					"userobm_web_all, " +
+					"userobm_mail_perms, " +
+					"userobm_mail_ext_perms, " +
+					"userobm_email, " +
+					"userobm_mail_server_id, " +
+					"userobm_mail_quota, " +
+					"userobm_mail_quota_use, " +
+					"userobm_mail_login_date, " +
+					"userobm_nomade_perms, " +
+					"userobm_nomade_enable, " +
+					"userobm_nomade_local_copy, " +
+					"userobm_email_nomade, " +
+					"userobm_vacation_enable, " +
+					"userobm_vacation_datebegin, " +
+					"userobm_vacation_dateend, " +
+					"userobm_vacation_message, " +
+					"userobm_samba_perms, " +
+					"userobm_samba_home, " +
+					"userobm_samba_home_drive, " +
+					"userobm_samba_logon_script, " +
+					"userobm_status, " +
+					"userobm_host_id, " +
+					"userobm_description, " +
+					"userobm_location, " +
+					"userobm_education, " +
+					"userobm_photo_id " +
+					") SELECT    userobm_id, " +
+					"userobm_domain_id, " +
+					"userobm_timecreate, " +
+					"userobm_userupdate, " +
+					"userobm_usercreate, " +
+					"userobm_local, " +
+					"userobm_ext_id, " +
+					"userobm_system, " +
+					"userobm_archive, " +
+					"userobm_timelastaccess, " +
+					"userobm_login, " +
+					"userobm_nb_login_failed, " +
+					"userobm_password_type, " +
+					"userobm_password, " +
+					"userobm_password_dateexp, " +
+					"userobm_account_dateexp, " +
+					"userobm_perms, " +
+					"userobm_delegation_target, " +
+					"userobm_delegation, " +
+					"userobm_calendar_version, " +
+					"userobm_uid, " +
+					"userobm_gid, " +
+					"userobm_datebegin, " +
+					"userobm_hidden, " +
+					"userobm_kind, " +
+					"userobm_commonname, " +
+					"userobm_lastname, " +
+					"userobm_firstname, " +
+					"userobm_title, " +
+					"userobm_sound, " +
+					"userobm_company, " +
+					"userobm_direction, " +
+					"userobm_service, " +
+					"userobm_address1, " +
+					"userobm_address2, " +
+					"userobm_address3, " +
+					"userobm_zipcode, " +
+					"userobm_town, " +
+					"userobm_expresspostal, " +
+					"userobm_country_iso3166, " +
+					"userobm_phone, " +
+					"userobm_phone2, " +
+					"userobm_mobile, " +
+					"userobm_fax, " +
+					"userobm_fax2, " +
+					"userobm_web_perms, " +
+					"userobm_web_list, " +
+					"userobm_web_all, " +
+					"userobm_mail_perms, " +
+					"userobm_mail_ext_perms, " +
+					"userobm_email, " +
+					"userobm_mail_server_id, " +
+					"userobm_mail_quota, " +
+					"userobm_mail_quota_use, " +
+					"userobm_mail_login_date, " +
+					"userobm_nomade_perms, " +
+					"userobm_nomade_enable, " +
+					"userobm_nomade_local_copy, " +
+					"userobm_email_nomade, " +
+					"userobm_vacation_enable, " +
+					"userobm_vacation_datebegin, " +
+					"userobm_vacation_dateend, " +
+					"userobm_vacation_message, " +
+					"userobm_samba_perms, " +
+					"userobm_samba_home, " +
+					"userobm_samba_home_drive, " +
+					"userobm_samba_logon_script, " +
+					"userobm_status, " +
+					"userobm_host_id, " +
+					"userobm_description, " +
+					"userobm_location, " +
+					"userobm_education, " +
+					"userobm_photo_id " +
+					"FROM UserObm " +
+					"WHERE userobm_id=?";
+			batchQueryByUserIds(conn, userObmQuery, userIds);
+			batchQueryByUserIds(conn,
+					"INSERT INTO P_UserEntity " +
+							"(   userentity_entity_id, " +
+							"    userentity_user_id " +
+							") SELECT    userentity_entity_id, " +
+							"            userentity_user_id " +
+							"  FROM UserEntity " +
+							"  WHERE userentity_user_id=?",
+					userIds);
+			batchQueryByUserIds(conn,
+					"INSERT INTO P_CategoryLink " +
+							"(   categorylink_category_id, " +
+							"   categorylink_entity_id, " +
+							"    categorylink_category " +
+							") SELECT    categorylink_category_id, " +
+							"            categorylink_entity_id, " +
+							"            categorylink_category " +
+							"  FROM CategoryLink " +
+							"  WHERE categorylink_entity_id=(SELECT userentity_entity_id " +
+							"                                FROM UserEntity " +
+							"                                WHERE userentity_user_id = ?)",
+					userIds);
+			batchQueryByUserIds(conn,
+					"INSERT INTO P_field " +
+							"(   id, " +
+							"    entity_id, " +
+							"    field, " +
+							"    value " +
+							") SELECT    id, " +
+							"            entity_id, " +
+							"            field, " +
+							"            value " +
+							"  FROM field " +
+							"  WHERE entity_id=(SELECT userentity_entity_id " +
+							"                                FROM UserEntity " +
+							"                                WHERE userentity_user_id = ?)",
+					userIds);
+			batchQueryByUserIds(conn,
+					"INSERT INTO P_MailboxEntity " +
+							"(   mailboxentity_entity_id, " +
+							"    mailboxentity_mailbox_id " +
+							") SELECT    mailboxentity_entity_id, " +
+							"            mailboxentity_mailbox_id " +
+							"  FROM MailboxEntity " +
+							"  WHERE mailboxentity_mailbox_id = ?",
+					userIds);
+			batchQueryByUserIds(conn,
+					"INSERT INTO P_EntityRight " +
+							"(   entityright_id, " +
+							"    entityright_entity_id, " +
+							"    entityright_consumer_id, " +
+							"    entityright_read, " +
+							"    entityright_write, " +
+							"    entityright_admin, " +
+							"    entityright_access " +
+							") SELECT    entityright_id, " +
+							"            entityright_entity_id, " +
+							"            entityright_consumer_id, " +
+							"            entityright_read, " +
+							"            entityright_write, " +
+							"            entityright_admin, " +
+							"            entityright_access " +
+							"  FROM EntityRight " +
+							"  WHERE entityright_entity_id=(SELECT mailboxentity_entity_id " +
+							"                                FROM MailboxEntity " +
+							"                                WHERE mailboxentity_mailbox_id = ?)",
+					userIds);
+		} catch (SQLException e) {
+			throw new DaoException(e);
+		} finally {
+			JDBCUtils.cleanup(conn, null, null);
+		}
+	}
+
+	@Override
+	public void deleteByUserExtIds(Collection<String> userExtIds)
+			throws DaoException {
+		Connection conn = null;
+		try {
+			conn = dbcp.getConnection();
+			Collection<Integer> userIds = mapExtIdsToUserIds(conn, userExtIds);
+			batchQueryByUserIds(conn,
+					"DELETE FROM P_EntityRight " +
+							"WHERE entityright_entity_id=(SELECT mailboxentity_entity_id " +
+							"                                FROM P_MailboxEntity " +
+							"                                WHERE mailboxentity_mailbox_id = ?)",
+					userIds);
+			batchQueryByUserIds(conn,
+					"DELETE FROM P_MailboxEntity WHERE mailboxentity_mailbox_id = ?",
+					userIds);
+			batchQueryByUserIds(conn,
+					"DELETE FROM P_CategoryLink " +
+							"WHERE categorylink_entity_id=(SELECT userentity_entity_id " +
+							"                                FROM UserEntity " +
+							"                                WHERE userentity_user_id = ?)",
+					userIds);
+			batchQueryByUserIds(conn,
+					"DELETE FROM P_field " +
+							"WHERE entity_id=(SELECT userentity_entity_id " +
+							"                                FROM UserEntity " +
+							"                                WHERE userentity_user_id = ?)",
+					userIds);
+			batchQueryByUserIds(conn,
+					"DELETE FROM P_UserEntity WHERE userentity_user_id=?",
+					userIds);
+			batchQueryByUserIds(conn,
+					"DELETE FROM P_UserObm WHERE userobm_id=?", userIds);
+		} catch (SQLException e) {
+			throw new DaoException(e);
+		} finally {
+			JDBCUtils.cleanup(conn, null, null);
+		}
+	}
+
+	private void batchQueryByUserIds(Connection conn, String query, Collection<Integer> userIds)
+			throws SQLException {
+		PreparedStatement statement = null;
+		try {
+			statement = conn.prepareStatement(query);
+			for (Integer userId : userIds) {
+				statement.setInt(1, userId);
+				statement.addBatch();
+			}
+			statement.executeBatch();
+		} finally {
+			JDBCUtils.cleanup(null, statement, null);
+		}
+	}
+	
+	private Collection<Integer> mapExtIdsToUserIds(Connection conn, Collection<String> userExtIds) throws SQLException, DaoException {
+		PreparedStatement statement = null;
+		try {
+			statement = conn.prepareStatement("SELECT userobm_id FROM UserObm WHERE userobm_ext_id=?");
+			ImmutableList.Builder<Integer> builder = ImmutableList.builder();
+			for (String userExtId : userExtIds) {
+				statement.setString(1, userExtId);
+				ResultSet rs = statement.executeQuery();
+				if (rs.next()) {
+					builder.add(rs.getInt(1));
+				}
+				else {
+					throw new DaoException(String.format("The extid %s is missing from the database", userExtId));
+				}
+				rs.close();
+			}
+			return builder.build();
+		} finally {
+			JDBCUtils.cleanup(null, statement, null);
+		}
+		
+	}
+
+}
---
+++ java/sync/obm-dao/src/test/java/org/obm/provisioning/dao/PUserDaoJdbcImplTest.java
@@ -0,0 +1,360 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Copyright (C) 2011-2013  Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version, provided you comply with the Additional Terms applicable for OBM
+ * software by Linagora pursuant to Section 7 of the GNU Affero General Public
+ * License, subsections (b), (c), and (e), pursuant to which you must notably (i)
+ * retain the displaying by the interactive user interfaces of the “OBM, Free
+ * Communication by Linagora” Logo with the “You are using the Open Source and
+ * free version of OBM developed and supported by Linagora. Contribute to OBM R&D
+ * by subscribing to an Enterprise offer !” infobox, (ii) retain all hypertext
+ * links between OBM and obm.org, between Linagora and linagora.com, as well as
+ * between the expression “Enterprise offer” and pro.obm.org, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks and
+ * commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License and
+ * its applicable Additional Terms for OBM along with this program. If not, see
+ * <http://www.gnu.org/licenses/> for the GNU Affero General   Public License
+ * version 3 and <http://www.linagora.com/licenses/> for the Additional Terms
+ * applicable to the OBM software.
+ * ***** END LICENSE BLOCK ***** */
+package org.obm.provisioning.dao;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.sql.ResultSet;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.obm.dao.utils.H2ConnectionProvider;
+import org.obm.dao.utils.H2InMemoryDatabase;
+import org.obm.dbcp.DatabaseConnectionProvider;
+import org.obm.domain.dao.PUserDao;
+import org.obm.domain.dao.PUserDaoJdbcImpl;
+import org.obm.filter.Slow;
+import org.obm.guice.GuiceModule;
+import org.obm.guice.SlowGuiceRunner;
+
+import com.google.common.collect.Lists;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.name.Names;
+
+@Slow
+@RunWith(SlowGuiceRunner.class)
+@GuiceModule(PUserDaoJdbcImplTest.Env.class)
+public class PUserDaoJdbcImplTest {
+
+	public static class Env extends AbstractModule {
+
+		@Override
+		protected void configure() {
+			bindConstant().annotatedWith(Names.named("initialSchema")).to("sql/initial.sql");
+
+			bind(DatabaseConnectionProvider.class).to(H2ConnectionProvider.class);
+			bind(PUserDao.class).to(PUserDaoJdbcImpl.class);
+		}
+
+	}
+
+	@Inject
+	private PUserDao dao;
+
+	@Rule
+	@Inject
+	public H2InMemoryDatabase db;
+
+	@Before
+	public void setUp() throws Exception {
+		db.executeUpdate("INSERT INTO Entity (entity_mailing) VALUES (TRUE)");
+		db.executeUpdate(
+				"INSERT INTO MailboxEntity (mailboxentity_entity_id, mailboxentity_mailbox_id) " +
+						"SELECT *, 2 " +
+						"FROM (SELECT MAX(entity_id) FROM Entity)");
+		db.executeUpdate("INSERT INTO Entity (entity_mailing) VALUES (TRUE)");
+		db.executeUpdate(
+				"INSERT INTO MailboxEntity (mailboxentity_entity_id, mailboxentity_mailbox_id) " +
+						"SELECT * " +
+						"FROM (SELECT MAX(entity_id) FROM Entity), " +
+						"(SELECT userobm_id FROM userobm WHERE userobm_ext_id='6')");
+
+		db.executeUpdate(
+				"INSERT INTO EntityRight "
+						+
+						"(   "
+						+
+						"    entityright_entity_id, "
+						+
+						"    entityright_consumer_id, "
+						+
+						"    entityright_read, "
+						+
+						"    entityright_write, "
+						+
+						"    entityright_admin, "
+						+
+						"    entityright_access) "
+						+
+						"SELECT *, 1, 1, 1, 0, 0 "
+						+
+						"FROM (SELECT mailboxentity_entity_id FROM MailboxEntity WHERE mailboxentity_mailbox_id=2)");
+		db.executeUpdate(
+				"INSERT INTO EntityRight "
+						+
+						"(   "
+						+
+						"    entityright_entity_id, "
+						+
+						"    entityright_consumer_id, "
+						+
+						"    entityright_read, "
+						+
+						"    entityright_write, "
+						+
+						"    entityright_admin, "
+						+
+						"    entityright_access) "
+						+
+						"SELECT *, 1, 1, 1, 0, 0 "
+						+
+						"FROM (SELECT mailboxentity_entity_id FROM MailboxEntity WHERE mailboxentity_mailbox_id=6)");
+		db.executeUpdate("INSERT INTO Category (category_domain_id, category_code) VALUES (1, 'CAT')");
+		db.executeUpdate(
+				"INSERT INTO CategoryLink " +
+						"(   categorylink_category_id, " +
+						"    categorylink_entity_id " +
+						") " +
+						"  SELECT 1, * " +
+						"  FROM (SELECT userentity_entity_id " +
+						"                                FROM UserEntity " +
+						"                                WHERE userentity_user_id = 4)");
+		db.executeUpdate(
+				"INSERT INTO CategoryLink " +
+						"(   categorylink_category_id, " +
+						"    categorylink_entity_id " +
+						") " +
+						"  SELECT 1, * " +
+						"  FROM (SELECT userentity_entity_id " +
+						"                                FROM UserEntity " +
+						"                                WHERE userentity_user_id = 6)");
+		db.executeUpdate(
+				"INSERT INTO Field " +
+						"(   entity_id, " +
+						"    field, " +
+						"    value " +
+						") " +
+						"  SELECT *, 'field', 'value' " +
+						"  FROM (SELECT userentity_entity_id " +
+						"                                FROM UserEntity " +
+						"                                WHERE userentity_user_id = 1)");
+		db.executeUpdate(
+				"INSERT INTO Field " +
+						"(   entity_id, " +
+						"    field, " +
+						"    value " +
+						") " +
+						"  SELECT *, 'field', 'value' " +
+						"  FROM (SELECT userentity_entity_id " +
+						"                                FROM UserEntity " +
+						"                                WHERE userentity_user_id = 6)");
+	}
+
+	@Test
+	public void testInsertByUserExtIds() throws Exception {
+		dao.insertByUserExtIds(Lists.newArrayList("1", "2", "3", "4", "5"));
+
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_UserObm")).isEqualTo(5);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_UserEntity")).isEqualTo(5);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_MailboxEntity")).isEqualTo(1);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_EntityRight")).isEqualTo(1);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_CategoryLink")).isEqualTo(1);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_Field")).isEqualTo(1);
+	}
+
+	@Test
+	public void testDeleteByUserExtIds() throws Exception {
+		db.executeUpdate("INSERT INTO P_UserObm " +
+				"(userobm_id, " +
+				"userobm_domain_id, " +
+				"userobm_timecreate, " +
+				"userobm_userupdate, " +
+				"userobm_usercreate, " +
+				"userobm_local, " +
+				"userobm_ext_id, " +
+				"userobm_system, " +
+				"userobm_archive, " +
+				"userobm_timelastaccess, " +
+				"userobm_login, " +
+				"userobm_nb_login_failed, " +
+				"userobm_password_type, " +
+				"userobm_password, " +
+				"userobm_password_dateexp, " +
+				"userobm_account_dateexp, " +
+				"userobm_perms, " +
+				"userobm_delegation_target, " +
+				"userobm_delegation, " +
+				"userobm_calendar_version, " +
+				"userobm_uid, " +
+				"userobm_gid, " +
+				"userobm_datebegin, " +
+				"userobm_hidden, " +
+				"userobm_kind, " +
+				"userobm_commonname, " +
+				"userobm_lastname, " +
+				"userobm_firstname, " +
+				"userobm_title, " +
+				"userobm_sound, " +
+				"userobm_company, " +
+				"userobm_direction, " +
+				"userobm_service, " +
+				"userobm_address1, " +
+				"userobm_address2, " +
+				"userobm_address3, " +
+				"userobm_zipcode, " +
+				"userobm_town, " +
+				"userobm_expresspostal, " +
+				"userobm_country_iso3166, " +
+				"userobm_phone, " +
+				"userobm_phone2, " +
+				"userobm_mobile, " +
+				"userobm_fax, " +
+				"userobm_fax2, " +
+				"userobm_web_perms, " +
+				"userobm_web_list, " +
+				"userobm_web_all, " +
+				"userobm_mail_perms, " +
+				"userobm_mail_ext_perms, " +
+				"userobm_email, " +
+				"userobm_mail_server_id, " +
+				"userobm_mail_quota, " +
+				"userobm_mail_quota_use, " +
+				"userobm_mail_login_date, " +
+				"userobm_nomade_perms, " +
+				"userobm_nomade_enable, " +
+				"userobm_nomade_local_copy, " +
+				"userobm_email_nomade, " +
+				"userobm_vacation_enable, " +
+				"userobm_vacation_datebegin, " +
+				"userobm_vacation_dateend, " +
+				"userobm_vacation_message, " +
+				"userobm_samba_perms, " +
+				"userobm_samba_home, " +
+				"userobm_samba_home_drive, " +
+				"userobm_samba_logon_script, " +
+				"userobm_status, " +
+				"userobm_host_id, " +
+				"userobm_description, " +
+				"userobm_location, " +
+				"userobm_education, " +
+				"userobm_photo_id " +
+				") SELECT    userobm_id, " +
+				"userobm_domain_id, " +
+				"userobm_timecreate, " +
+				"userobm_userupdate, " +
+				"userobm_usercreate, " +
+				"userobm_local, " +
+				"userobm_ext_id, " +
+				"userobm_system, " +
+				"userobm_archive, " +
+				"userobm_timelastaccess, " +
+				"userobm_login, " +
+				"userobm_nb_login_failed, " +
+				"userobm_password_type, " +
+				"userobm_password, " +
+				"userobm_password_dateexp, " +
+				"userobm_account_dateexp, " +
+				"userobm_perms, " +
+				"userobm_delegation_target, " +
+				"userobm_delegation, " +
+				"userobm_calendar_version, " +
+				"userobm_uid, " +
+				"userobm_gid, " +
+				"userobm_datebegin, " +
+				"userobm_hidden, " +
+				"userobm_kind, " +
+				"userobm_commonname, " +
+				"userobm_lastname, " +
+				"userobm_firstname, " +
+				"userobm_title, " +
+				"userobm_sound, " +
+				"userobm_company, " +
+				"userobm_direction, " +
+				"userobm_service, " +
+				"userobm_address1, " +
+				"userobm_address2, " +
+				"userobm_address3, " +
+				"userobm_zipcode, " +
+				"userobm_town, " +
+				"userobm_expresspostal, " +
+				"userobm_country_iso3166, " +
+				"userobm_phone, " +
+				"userobm_phone2, " +
+				"userobm_mobile, " +
+				"userobm_fax, " +
+				"userobm_fax2, " +
+				"userobm_web_perms, " +
+				"userobm_web_list, " +
+				"userobm_web_all, " +
+				"userobm_mail_perms, " +
+				"userobm_mail_ext_perms, " +
+				"userobm_email, " +
+				"userobm_mail_server_id, " +
+				"userobm_mail_quota, " +
+				"userobm_mail_quota_use, " +
+				"userobm_mail_login_date, " +
+				"userobm_nomade_perms, " +
+				"userobm_nomade_enable, " +
+				"userobm_nomade_local_copy, " +
+				"userobm_email_nomade, " +
+				"userobm_vacation_enable, " +
+				"userobm_vacation_datebegin, " +
+				"userobm_vacation_dateend, " +
+				"userobm_vacation_message, " +
+				"userobm_samba_perms, " +
+				"userobm_samba_home, " +
+				"userobm_samba_home_drive, " +
+				"userobm_samba_logon_script, " +
+				"userobm_status, " +
+				"userobm_host_id, " +
+				"userobm_description, " +
+				"userobm_location, " +
+				"userobm_education, " +
+				"userobm_photo_id " +
+				"FROM UserObm");
+		db.executeUpdate("INSERT INTO P_UserEntity SELECT * FROM UserEntity");
+		db.executeUpdate("INSERT INTO P_MailboxEntity SELECT * FROM MailboxEntity");
+		db.executeUpdate("INSERT INTO P_EntityRight SELECT * FROM EntityRight");
+		db.executeUpdate("INSERT INTO P_CategoryLink SELECT * FROM CategoryLink");
+		db.executeUpdate("INSERT INTO P_Field SELECT * FROM Field");
+
+		dao.deleteByUserExtIds(Lists.newArrayList("1", "2", "3", "4", "5"));
+
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_UserObm")).isEqualTo(1);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_UserEntity")).isEqualTo(1);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_MailboxEntity")).isEqualTo(1);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_EntityRight")).isEqualTo(1);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_CategoryLink")).isEqualTo(1);
+		assertThat(getIntFromQuery("SELECT COUNT(*) FROM P_Field")).isEqualTo(1);
+	}
+
+	private int getIntFromQuery(String query, Object... params) throws Exception {
+		ResultSet rs = db.execute(query, params);
+		if (rs.next()) {
+			return rs.getInt(1);
+		}
+		else {
+			throw new RuntimeException("Expected single integer result, got no result instead");
+		}
+	}
+}
