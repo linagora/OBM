d524e5cfd2c8d0fa515109a19b1385073ba4b55a
Antoine Duprat <aduprat@linagora.com>
2013-09-03 16:38:42 +0200

    - [OBMFULL-5341] Introduce an attachment extension mapper

---
+++ java/sync/opush/mail-backend-interfaces/src/main/java/org/obm/push/mail/mime/AttachmentExtensionMapper.java
@@ -0,0 +1,59 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Copyright (C) 2011-2013  Linagora
+ *
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Affero General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version, provided you comply 
+ * with the Additional Terms applicable for OBM connector by Linagora 
+ * pursuant to Section 7 of the GNU Affero General Public License, 
+ * subsections (b), (c), and (e), pursuant to which you must notably (i) retain 
+ * the “Message sent thanks to OBM, Free Communication by Linagora” 
+ * signature notice appended to any and all outbound messages 
+ * (notably e-mail and meeting requests), (ii) retain all hypertext links between 
+ * OBM and obm.org, as well as between Linagora and linagora.com, and (iii) refrain 
+ * from infringing Linagora intellectual property rights over its trademarks 
+ * and commercial brands. Other Additional Terms apply, 
+ * see <http://www.linagora.com/licenses/> for more details. 
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License 
+ * for more details. 
+ *
+ * You should have received a copy of the GNU Affero General Public License 
+ * and its applicable Additional Terms for OBM along with this program. If not, 
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License version 3 
+ * and <http://www.linagora.com/licenses/> for the Additional Terms applicable to 
+ * OBM connectors. 
+ * 
+ * ***** END LICENSE BLOCK ***** */
+package org.obm.push.mail.mime;
+
+import java.util.Map;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+
+public class AttachmentExtensionMapper {
+
+	private static final String DOT_SEPARATOR = ".";
+	
+	private static Map<String, String> subTypeToExtensionMapping = ImmutableMap.<String, String> builder()
+			.put("image/bmp", DOT_SEPARATOR + "bmp")
+			.put("image/gif", DOT_SEPARATOR + "gif")
+			.put("image/jpeg", DOT_SEPARATOR + "jpg")
+			.put("image/png", DOT_SEPARATOR + "png")
+			.put("image/svg+xml", DOT_SEPARATOR + "svg")
+			.put("image/tiff", DOT_SEPARATOR + "tif")
+			.build();
+	
+	public static String extensionFromContentType(ContentType contentType) {
+		String extension = subTypeToExtensionMapping.get(contentType.getFullMimeType());
+		if (!Strings.isNullOrEmpty(extension)) {
+			return extension;
+		}
+		return null;
+	}
+}
