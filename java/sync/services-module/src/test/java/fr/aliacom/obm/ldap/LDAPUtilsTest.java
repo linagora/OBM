01000f15d6be4910e764aeb9fd1a17861e420493
Stéphen Le Maistre <slemaistre@linagora.com>
2013-11-12 15:41:40 +0100

    OBMFULL-5574 close NamingEnumeration when authenticate with Ldap

---
+++ java/sync/services-module/src/test/java/fr/aliacom/obm/ldap/LDAPUtilsTest.java
@@ -0,0 +1,79 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ *
+ * Copyright (C) 2011-2013  Linagora
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version, provided you comply
+ * with the Additional Terms applicable for OBM connector by Linagora
+ * pursuant to Section 7 of the GNU Affero General Public License,
+ * subsections (b), (c), and (e), pursuant to which you must notably (i) retain
+ * the “Message sent thanks to OBM, Free Communication by Linagora”
+ * signature notice appended to any and all outbound messages
+ * (notably e-mail and meeting requests), (ii) retain all hypertext links between
+ * OBM and obm.org, as well as between Linagora and linagora.com, and (iii) refrain
+ * from infringing Linagora intellectual property rights over its trademarks
+ * and commercial brands. Other Additional Terms apply,
+ * see <http://www.linagora.com/licenses/> for more details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and its applicable Additional Terms for OBM along with this program. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License version 3
+ * and <http://www.linagora.com/licenses/> for the Additional Terms applicable to
+ * OBM connectors.
+ *
+ * ***** END LICENSE BLOCK ***** */
+package fr.aliacom.obm.ldap;
+
+import static org.easymock.EasyMock.createControl;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.eq;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+
+import org.easymock.IMocksControl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.obm.filter.SlowFilterRunner;
+
+@RunWith(SlowFilterRunner.class)
+public class LDAPUtilsTest {
+
+	private IMocksControl control;
+	private DirContext ctx;
+	private LDAPUtils ldapUtils;
+	private NamingEnumeration<SearchResult> namingEnumeration;
+
+	@Before
+	public void setUp() {
+		control = createControl();
+		ctx = control.createMock(DirContext.class);
+		namingEnumeration = control.createMock(NamingEnumeration.class);
+		ldapUtils = new LDAPUtils("uri", "rooDN", "rootPW", "baseDN");
+	}
+
+	@Test
+	public void testNamingEnumerationIsClosed() throws NamingException {
+		expect(ctx.search(eq("baseDN"), eq("aFilter"), anyObject(SearchControls.class))).andReturn(namingEnumeration);
+		expect(namingEnumeration.hasMoreElements()).andReturn(false);
+		namingEnumeration.close();
+		expectLastCall().once();
+
+		control.replay();
+		ldapUtils.findResultByFilter("aFilter", ctx);
+		control.verify();
+	}
+}
