d3816f56f71b69d2cd8d28ad4a2f917667af08bd
Emmanuel Surleau <esurleau@linagora.com>
2013-11-27 14:59:31 +0100

    OBMFULL-5611 Hides new_meeting when there is no writable calendar.

--- ui/php/calendar/calendar_index.php
+++ ui/php/calendar/calendar_index.php
@@ -2286,6 +2286,7 @@ function update_calendar_action($writable_calendars) {
   global $actions, $params, $path, $obm;
   if (!$writable_calendars) {
       unset($actions['calendar']['new']);
+      unset($actions['calendar']['new_meeting']);
   }
   $id = $params['calendar_id'];
   if($id) {
