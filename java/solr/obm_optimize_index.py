2025556a41e11230b8edb2840e84ba2fdc44fe96
slemaistre <slemaistre@linagora.com>
2014-01-16 11:48:03 +0100

    OBMFULL-5708 replace (license)2011-2012 and 2011-2013 by 2011-2014 in all obm files.

--- java/solr/obm_optimize_index.py
+++ java/solr/obm_optimize_index.py
@@ -6,7 +6,7 @@
 # Depends: python-psycopg2 for postgresql or python-mysqldb for mysql.
 
 # ##### BEGIN LICENSE BLOCK #####
-# Copyright (C) 2011-2012  Linagora
+# Copyright (C) 2011-2014  Linagora
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Affero General Public License as published by the Free
--- ui/obminclude/of/of_dynamicmethod.php
+++ ui/obminclude/of/of_dynamicmethod.php
@@ -1,7 +1,7 @@
 <?php
 
 /******************************************************************************
- Copyright (C) 2011-2012 Linagora
+ Copyright (C) 2011-2014 Linagora
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU Affero General Public License as published by the Free
