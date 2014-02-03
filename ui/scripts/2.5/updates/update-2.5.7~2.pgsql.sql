2aac5ba2e8b9c7135d259999085d839fc171a638
Emmanuel Surleau <esurleau@linagora.com>
2014-02-03 14:35:20 +0100

    OBMFULL-5765 Adds company phone and fax number to search results.
    Clone of OBMFULL-5741 for next branch.

---
+++ ui/scripts/2.5/updates/update-2.5.7~2.pgsql.sql
@@ -0,0 +1,2 @@
+INSERT INTO DisplayPref (display_entity, display_fieldname, display_fieldorder) VALUES ('company', 'WorkPhone.phone_number', 22);
+INSERT INTO DisplayPref (display_entity, display_fieldname, display_fieldorder) VALUES ('company', 'FaxPhone.phone_number', 23);
--- ui/scripts/creation/install_obmdb_mysql.sh
+++ ui/scripts/creation/install_obmdb_mysql.sh
@@ -67,6 +67,9 @@ mysql --default-character-set='UTF8' -u ${user} -p$pw ${db} \
   < "../2.5/updates/update-2.5.7~1.mysql.sql" >> /tmp/data_insert.log 2>&1
 
 mysql --default-character-set='UTF8' -u ${user} -p$pw ${db} \
+  < "../2.5/updates/update-2.5.7~2.mysql.sql" >> /tmp/data_insert.log 2>&1
+
+mysql --default-character-set='UTF8' -u ${user} -p$pw ${db} \
   < "../2.6/updates/update-2.6.0~1.mysql.sql" >> /tmp/data_insert.log 2>&1
 
 
