f76af40d932848f715d33adab9b224fd759b3b8f
Matthieu Baechler <matthieu.baechler@linagora.com>
2014-02-24 12:38:18 +0100

    OBMFULL-5777 modernize ErrorMailerTest
    Conflicts:
    	java/sync/services/src/test/java/org/obm/sync/server/mailer/ErrorMailerTest.java

--- java/sync/services-module/src/test/java/org/obm/sync/server/mailer/ErrorMailerTest.java
+++ java/sync/services-module/src/test/java/org/obm/sync/server/mailer/ErrorMailerTest.java
@@ -31,6 +31,15 @@
  * ***** END LICENSE BLOCK ***** */
 package org.obm.sync.server.mailer;
 
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.capture;
+import static org.easymock.EasyMock.createControl;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.fest.assertions.api.Assertions.assertThat;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -40,19 +49,13 @@ import java.util.ResourceBundle;
 import java.util.TimeZone;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.easymock.Capture;
-import org.easymock.EasyMock;
-import org.hamcrest.core.IsInstanceOf;
-import org.hamcrest.core.StringContains;
-import org.junit.Assert;
+import org.easymock.IMocksControl;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 import org.obm.sync.auth.AccessToken;
 import org.obm.sync.server.template.ITemplateLoader;
 import org.obm.sync.server.template.TemplateLoaderFreeMarkerImpl;
@@ -62,206 +65,108 @@ import com.google.common.collect.ImmutableList;
 
 import fr.aliacom.obm.ToolBox;
 import fr.aliacom.obm.common.MailService;
-import fr.aliacom.obm.common.calendar.EventNotificationServiceTestTools;
 import fr.aliacom.obm.services.constant.ObmSyncConfigurationService;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 
-@RunWith(Suite.class)
-@SuiteClasses({ErrorMailerTest.Error.class, ErrorMailerTest.Expire.class})
 public class ErrorMailerTest {
 
 	private static final TimeZone TIMEZONE = TimeZone.getTimeZone("Europe/Paris");
-	
+	private AccessToken at;
+	private ITemplateLoader templateLoader;
+	private IMocksControl control;
+
+	@Before
+	public void setup() {
+		control = createControl();
+		at = getMockAccessToken();
+		templateLoader = new ITemplateLoader() {
+			@Override
+			public Template getTemplate(String templateName, Locale locale, TimeZone timezone)
+					throws IOException {
+				Configuration cfg = new Configuration();
+				cfg.setClassForTemplateLoading(getClass(), TemplateLoaderFreeMarkerImpl.getTemplatePathPrefix(locale));
+				return cfg.getTemplate(templateName, locale);
+			}
+		};
+	}
+
 	protected static AccessToken getMockAccessToken(){
 		AccessToken at = new AccessToken(1, "unitTest");
 		at.setDomain(ToolBox.getDefaultObmDomain());
 		at.setUserEmail("adrien@test.tlse.lng");
 		return at;
 	}
-	
-	public abstract static class Common {
-		
-		AccessToken at;
-		ITemplateLoader templateLoader;
-		
-		public Common(){
-			at = getMockAccessToken();
-			
-			templateLoader = new ITemplateLoader() {
-				@Override
-				public Template getTemplate(String templateName, Locale locale, TimeZone timezone)
-						throws IOException {
-					Configuration cfg = new Configuration();
-					cfg.setClassForTemplateLoading(getClass(), TemplateLoaderFreeMarkerImpl.getTemplatePathPrefix(locale));
-					return cfg.getTemplate(templateName, locale);
-				}
-			};
-			
-		}
-		
-		protected ITemplateLoader getMockTemplateLoader(){
-			return templateLoader;
-		}
-		
-		protected MailService defineMailServiceExpectations(
-				List<InternetAddress> expectedRecipients,
-				Capture<MimeMessage> capturedMessage) throws MessagingException {
-			
-			MailService mailService = EasyMock.createMock(MailService.class);
-			mailService.sendMessage(
-					EventNotificationServiceTestTools.compareCollections(expectedRecipients), 
-					EasyMock.capture(capturedMessage),
-					EasyMock.anyObject(AccessToken.class));
-			EasyMock.expectLastCall();
-			EasyMock.replay(mailService);
-			return mailService;
-		}
 
-		protected abstract List<InternetAddress> getExpectedRecipients() throws AddressException;
+	private String getRawMessage(MimeMessage actualMessage)
+			throws IOException, MessagingException,	UnsupportedEncodingException {
 
-		public static class ErrorParts {
-			public String rawMessage;
-			public String plainText;
-		}
-
-		
-		private String getRawMessage(MimeMessage actualMessage)
-		throws IOException, MessagingException,	UnsupportedEncodingException {
-
-			ByteArrayOutputStream output = new ByteArrayOutputStream();
-			actualMessage.writeTo(output);
-			String rawMessage = new String(output.toByteArray(), Charsets.UTF_8.displayName());
-			return rawMessage;
-		}
-
-
-		protected void test() throws UnsupportedEncodingException, IOException, MessagingException {
-			ObmSyncConfigurationService constantService = EasyMock.createMock(ObmSyncConfigurationService.class);
-			EasyMock.expect(constantService.getObmSyncMailer(at)).andReturn("x-obm-sync@test.tlse.lng").once();
-			EasyMock.expect(constantService.getResourceBundle(Locale.FRENCH)).andReturn(
-					ResourceBundle.getBundle("Messages", Locale.FRENCH));
-			Capture<MimeMessage> capturedMessage = new Capture<MimeMessage>();
-			EasyMock.replay(constantService);
-			List<InternetAddress> expectedRecipients = getExpectedRecipients();
-			MailService mailService = defineMailServiceExpectations(expectedRecipients, capturedMessage);
-			ErrorMailer errorMailer = new ErrorMailer(mailService, constantService, getMockTemplateLoader());
-			
-			executeProcess(errorMailer);
-
-			EasyMock.verify(mailService, constantService);
-			MimeMessage mimeMessage = capturedMessage.getValue();
-
-			ErrorParts parts = checkErrorStructure(mimeMessage);
-			checkContent(parts);
-		}
-
-		protected abstract void checkContent(ErrorParts parts) throws IOException, MessagingException;
-
-		protected ErrorParts checkErrorStructure(MimeMessage mimeMessage) throws UnsupportedEncodingException, IOException, MessagingException {
-			ErrorParts parts = new ErrorParts();
-			parts.rawMessage = getRawMessage(mimeMessage);
-			Assert.assertTrue(mimeMessage.getContentType().startsWith("text/plain; charset=UTF-8"));
-			parts.plainText = (String)mimeMessage.getContent();
-			return parts;
-		}
-
-		protected void checkStringContains(String text, String... expected) {
-			for (String s: expected) {
-				Assert.assertThat(text, StringContains.containsString(s));
-			}
-		}
-
-		protected void checkPlainMessage(String plainText) {
-			Assert.assertThat(plainText, IsInstanceOf.instanceOf(String.class));
-			String text = plainText;
-			checkStringContains(text, getExpectedPlainStrings());
-		}
-		
-		protected abstract String[] getExpectedPlainStrings();
-
-
-		protected abstract void executeProcess(ErrorMailer errorMailer);
-	}
-
-	public static class Error extends Common {
-
-		@Override
-		protected void executeProcess(ErrorMailer errorMailer) {
-			errorMailer.notifyConnectorVersionError(at, "1.1.1", Locale.FRENCH, TIMEZONE);
-		}
-		
-		@Test
-		public void errorRequest() throws IOException, MessagingException {
-			super.test();
-		}
-
-		@Override
-		protected void checkContent(ErrorParts parts) throws IOException, MessagingException {
-			checkStringContains(parts.rawMessage, 
-					"From: x-obm-sync@test.tlse.lng",
-					"To: adrien@test.tlse.lng",
-					"Subject: ");
-			checkPlainMessage(parts.plainText);
-		}
-		
-		@Override
-		protected String[] getExpectedPlainStrings() {
-			return new String[] {
-				"La synchronisation de votre agenda et de vos contacts n'a pas pu aboutir car la\nversion de l'extension \"OBM Connector\" que vous utilisez est obsolète.\n",
-				"Merci de contacter votre administrateur afin qu'il vous fournisse une mise à\njour",
-			};
-		}
-		
-		@Override
-		protected List<InternetAddress> getExpectedRecipients() throws AddressException {
-			return ImmutableList.of(new InternetAddress("adrien@test.tlse.lng"));
-		}
-
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		actualMessage.writeTo(output);
+		String rawMessage = new String(output.toByteArray(), Charsets.UTF_8.displayName());
+		return rawMessage;
 	}
 	
-	public static class Expire {
-		
-		@Test
-		public void testExpireAfterWrite() throws MessagingException {
-			AccessToken at = getMockAccessToken();
-			ObmSyncConfigurationService constantService = EasyMock.createMock(ObmSyncConfigurationService.class);
-			EasyMock.expect(constantService.getObmSyncMailer(at)).andReturn("x-obm-sync@test.tlse.lng").once();
-			EasyMock.expect(constantService.getResourceBundle(Locale.FRENCH)).andReturn(
-					ResourceBundle.getBundle("Messages", Locale.FRENCH));
-			Capture<MimeMessage> capturedMessage = new Capture<MimeMessage>();
-			EasyMock.replay(constantService);
-			List<InternetAddress> expectedRecipients = ImmutableList.of(new InternetAddress("adrien@test.tlse.lng"));
-			
-			MailService mailService = EasyMock.createMock(MailService.class);
-			mailService.sendMessage(
-					EventNotificationServiceTestTools.compareCollections(expectedRecipients), 
-					EasyMock.capture(capturedMessage),
-					EasyMock.anyObject(AccessToken.class));
-			EasyMock.expectLastCall();
-			EasyMock.replay(mailService);
+	@Test
+	public void testErrorMailing() throws Exception {
+		ObmSyncConfigurationService constantService = control.createMock(ObmSyncConfigurationService.class);
+		expect(constantService.getObmSyncMailer(at)).andReturn("x-obm-sync@test.tlse.lng").once();
+		expect(constantService.getResourceBundle(Locale.FRENCH)).andReturn(
+				ResourceBundle.getBundle("Messages", Locale.FRENCH));
+		Capture<MimeMessage> capturedMessage = new Capture<MimeMessage>();
 
-			ITemplateLoader templateLoader = new ITemplateLoader() {
-				@Override
-				public Template getTemplate(String templateName, Locale locale, TimeZone timezone)
-						throws IOException {
-					Configuration cfg = new Configuration();
-					cfg.setClassForTemplateLoading(getClass(), TemplateLoaderFreeMarkerImpl.getTemplatePathPrefix(locale));
-					return cfg.getTemplate(templateName, locale);
-				}
-			};
-			
-			ErrorMailer errorMailer = new ErrorMailer(mailService, constantService, templateLoader);
-			
-			errorMailer.notifyConnectorVersionError(at, "1.1.1", Locale.FRENCH, TIMEZONE);
-			errorMailer.notifyConnectorVersionError(at, "1.1.1", Locale.FRENCH, TIMEZONE);
+		List<InternetAddress> expectedRecipients = ImmutableList.of(new InternetAddress("adrien@test.tlse.lng"));
+		MailService mailService = control.createMock(MailService.class);
+		mailService.sendMessage(
+				eq(expectedRecipients),
+				capture(capturedMessage),
+				anyObject(AccessToken.class));
+		expectLastCall();
+		ErrorMailer errorMailer = new ErrorMailer(mailService, constantService, templateLoader);
 
-			EasyMock.verify(mailService, constantService);
-			
+		control.replay();
+		errorMailer.notifyConnectorVersionError(at, "1.1.1", Locale.FRENCH, TIMEZONE);
 
-		}
-		
+		control.verify();
+		MimeMessage mimeMessage = capturedMessage.getValue();
+
+		String rawMessage = getRawMessage(mimeMessage);
+		assertThat(rawMessage)
+			.contains("From: x-obm-sync@test.tlse.lng")
+			.contains("To: adrien@test.tlse.lng")
+			.contains("Subject: ");
+		assertThat(mimeMessage.getContentType()).startsWith("text/plain; charset=UTF-8");
+		assertThat(mimeMessage.getContent()).isInstanceOf(String.class);
+		String plainText = (String)mimeMessage.getContent();
+		assertThat(plainText)
+			.contains("La synchronisation de votre agenda et de vos contacts n'a pas pu aboutir car la\nversion de l'extension \"OBM Connector\" que vous utilisez est obsolète.\n")
+			.contains("Merci de contacter votre administrateur afin qu'il vous fournisse une mise à\njour");
 	}
-	
+
+	@Test
+	public void testNotificationSentOnlyOnce() throws MessagingException {
+		AccessToken at = getMockAccessToken();
+		ObmSyncConfigurationService constantService = control.createMock(ObmSyncConfigurationService.class);
+		expect(constantService.getObmSyncMailer(at)).andReturn("x-obm-sync@test.tlse.lng").once();
+		expect(constantService.getResourceBundle(Locale.FRENCH)).andReturn(
+				ResourceBundle.getBundle("Messages", Locale.FRENCH));
+		
+		List<InternetAddress> expectedRecipients = ImmutableList.of(new InternetAddress("adrien@test.tlse.lng"));
+
+		MailService mailService = createMock(MailService.class);
+		mailService.sendMessage(
+				eq(expectedRecipients), 
+				anyObject(MimeMessage.class),
+				anyObject(AccessToken.class));
+		expectLastCall().once();
+		
+		control.replay();
+
+		ErrorMailer errorMailer = new ErrorMailer(mailService, constantService, templateLoader);
+
+		errorMailer.notifyConnectorVersionError(at, "1.1.1", Locale.FRENCH, TIMEZONE);
+		errorMailer.notifyConnectorVersionError(at, "1.1.1", Locale.FRENCH, TIMEZONE);
+		control.verify();
+	}
 
 }
