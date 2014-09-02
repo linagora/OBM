5a72b3bd7cdf6741806e931719d1ff84e8fa6698
Thomas Hilaire <thilaire@linagora.com>
2014-09-02 16:44:29 +0200

    OBMFULL-6044 Adapt sql file to new schema

--- ui/scripts/3.1/updates/update-3.1.0~1.pgsql.sql
+++ ui/scripts/3.1/updates/update-3.1.0~1.pgsql.sql
@@ -22,9 +22,9 @@ id SERIAL PRIMARY KEY,
 mail_archive_run_uuid character(36) NOT NULL,
 mail_archive_run_domain_uuid character(36) NOT NULL,
 mail_archive_run_status mail_archive_status,
-mail_archive_run_start TIMESTAMP NOT NULL,
+mail_archive_run_schedule TIMESTAMP,
+mail_archive_run_start TIMESTAMP,
 mail_archive_run_end TIMESTAMP,
-mail_archive_run_lower_boundary TIMESTAMP,
 mail_archive_run_higher_boundary TIMESTAMP,
 CONSTRAINT mail_archive_run_uuid_ukey UNIQUE (mail_archive_run_uuid)
 );
