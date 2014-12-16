a872c59d7077cd959e7cb05e6e4bc3b4bfd80ad0
Thomas Hilaire <thilaire@linagora.com>
2014-12-16 18:29:46 +0100

    Revert "OBMFULL-6164 Fix calendar portlet"
    This reverts commit c4feae3de134a507f553f856130ac0b04805601a.

--- ui/php/obm.php
+++ ui/php/obm.php
@@ -232,7 +232,7 @@ function dis_calendar_portal() {
   $end = clone $start;
   $end->addMonth(1)->setWeekday($ccalendar_weekstart)->addWeek(1)->setHour(0)->setMinute(0)->setSecond(0);
   $calendar_entity['user'] = array($obm['uid'] => array('dummy'));
-  calendar_events_model($start,$end, $calendar_entity, Of_date::getOption('timezone'));
+  calendar_events_model($start,$end, $calendar_entity);
   $of = &OccurrenceFactory::getInstance();
 
   // Minicalendar
