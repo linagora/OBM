2534e81fc7a06289f4b22fb60bcede10c88a7ac9
Ba Tuan <btngo@linagora.com>
2015-01-09 18:12:05 +0100

    OBMFULL-6022 Changes _userpattern.pattern to TEXT, improves index.

--- ui/scripts/creation/install_obmdb_pgsql.sh
+++ ui/scripts/creation/install_obmdb_pgsql.sh
@@ -105,4 +105,7 @@ psql -U ${user} -h ${host} ${db} -f \
 psql -U ${user} -h ${host} ${db} -f \
   "../3.1/updates/update-3.1.2~2.pgsql.sql" >> /tmp/data_insert.log 2>&1
 
+psql -U ${user} -h ${host} ${db} -f \
+  "../3.1/updates/update-3.1.2~3.pgsql.sql" >> /tmp/data_insert.log 2>&1
+
 echo "DONE."
