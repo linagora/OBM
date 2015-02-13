950ea1c80c65094ee4e1e40de946b2510503467a
ddolcimascolo <ddolcimascolo@linagora.com>
2015-02-13 10:48:39 +0100

    OBMFULL-6342 Introduced NTLMUtils class with the ASL header.

--- java/sync/ldap-client/src/main/java/org/obm/provisioning/ldap/client/samba/NTLMPasswordGenerator.java
+++ java/sync/ldap-client/src/main/java/org/obm/provisioning/ldap/client/samba/NTLMPasswordGenerator.java
@@ -29,18 +29,12 @@
  * ***** END LICENSE BLOCK ***** */
 package org.obm.provisioning.ldap.client.samba;
 
-import java.security.Key;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
+import static org.obm.provisioning.ldap.client.samba.NTLMUtils.lmHash;
 
 import org.apache.commons.codec.binary.Hex;
 import org.obm.provisioning.ldap.client.bean.NTLMPassword;
 
 public class NTLMPasswordGenerator {
-
-	public static final int NTLM_MAX_PASSWORD_LENGTH = 14;
-	public static final byte[] MAGIC_CONSTANT = {'K', 'G', 'S', '!', '@', '#', '$', '%'};
 
 	public static NTLMPassword computeNTLMPassword(String clearTextPassword) throws Exception {
 		return NTLMPassword
@@ -63,60 +57,4 @@ public class NTLMPasswordGenerator {
 		return md4.engineDigest();
 	}
 
-	/**
-	 * Part of the code below is from Apache HTTP client.
-	 * See {@link org.apache.http.impl.auth.NTLMEngineImpl.CipherGen}
-	 */
-
-	private static byte[] lmHash(String password) throws Exception {
-		Cipher des = Cipher.getInstance("DES/ECB/NoPadding");
-		byte[] oemPassword = password.toUpperCase().getBytes("US-ASCII");
-		int length = Math.min(oemPassword.length, NTLM_MAX_PASSWORD_LENGTH);
-		byte[] keyBytes = new byte[NTLM_MAX_PASSWORD_LENGTH];
-
-		System.arraycopy(oemPassword, 0, keyBytes, 0, length);
-
-		des.init(Cipher.ENCRYPT_MODE, createDESKey(keyBytes, 0));
-		byte[] lowHash = des.doFinal(MAGIC_CONSTANT);
-
-		des.init(Cipher.ENCRYPT_MODE, createDESKey(keyBytes, 7));
-		byte[] highHash = des.doFinal(MAGIC_CONSTANT);
-
-		byte[] lmHash = new byte[16];
-		System.arraycopy(lowHash, 0, lmHash, 0, 8);
-		System.arraycopy(highHash, 0, lmHash, 8, 8);
-
-		return lmHash;
-	}
-
-	private static Key createDESKey(byte[] bytes, int offset) {
-		byte[] keyBytes = new byte[7];
-		System.arraycopy(bytes, offset, keyBytes, 0, 7);
-
-		byte[] material = new byte[8];
-		material[0] = keyBytes[0];
-		material[1] = (byte) (keyBytes[0] << 7 | (keyBytes[1] & 0xff) >>> 1);
-		material[2] = (byte) (keyBytes[1] << 6 | (keyBytes[2] & 0xff) >>> 2);
-		material[3] = (byte) (keyBytes[2] << 5 | (keyBytes[3] & 0xff) >>> 3);
-		material[4] = (byte) (keyBytes[3] << 4 | (keyBytes[4] & 0xff) >>> 4);
-		material[5] = (byte) (keyBytes[4] << 3 | (keyBytes[5] & 0xff) >>> 5);
-		material[6] = (byte) (keyBytes[5] << 2 | (keyBytes[6] & 0xff) >>> 6);
-		material[7] = (byte) (keyBytes[6] << 1);
-		oddParity(material);
-
-		return new SecretKeySpec(material, "DES");
-	}
-
-	private static void oddParity(byte[] bytes) {
-		for (int i = 0; i < bytes.length; i++) {
-			byte b = bytes[i];
-			boolean needsParity = (((b >>> 7) ^ (b >>> 6) ^ (b >>> 5) ^ (b >>> 4) ^ (b >>> 3) ^ (b >>> 2) ^ (b >>> 1)) & 0x01) == 0;
-
-			if (needsParity) {
-				bytes[i] |= (byte) 0x01;
-			} else {
-				bytes[i] &= (byte) 0xfe;
-			}
-		}
-	}
 }
