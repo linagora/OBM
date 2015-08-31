550aeb8a7ecd2d6f52e8662fed64cec317467f8f
ddolcimascolo <ddolcimascolo@linagora.com>
2015-08-31 10:12:53 +0200

    OBMFULL-6423 Ported patch http://trac.roundcube.net/changeset/cb0e0777ce/github

--- ui/php/webmail/program/lib/Roundcube/rcube_ldap.php
+++ ui/php/webmail/program/lib/Roundcube/rcube_ldap.php
@@ -837,7 +837,7 @@ class rcube_ldap extends rcube_addressbook
                 $me = $this;
                 $attributes = array();
                 array_walk($fields, function($field) use ($me, &$attributes) {
-                    if ($this->coltypes[$field] && ($attrs = (array)$this->coltypes[$field]['attributes'])) {
+                    if ($me->coltypes[$field] && ($attrs = (array)$me->coltypes[$field]['attributes'])) { 
                         $attributes = array_merge($attributes, $attrs);
                     }
                 });
